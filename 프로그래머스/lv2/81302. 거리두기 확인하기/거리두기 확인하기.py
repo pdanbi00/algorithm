59ffdc1f65be503b49ea8df4ab1ea84fba0d2b63
박단비 <qkreksqhd@naver.com>
2023-09-28 13:33:09 +0900

    거리두기 확인하기 주석 추가

--- 프로그래머스/lv2/81302. 거리두기 확인하기/거리두기 확인하기.py
+++ 프로그래머스/lv2/81302. 거리두기 확인하기/거리두기 확인하기.py
@@ -1,3 +1,7 @@
+# 아이디어 : 맨해튼 거리 2 초과여야하니깐
+# 맨해튼 거리 1이면 바로 옆에 있는거니깐 거리두기 안된거
+# 맨해튼 거리 2이면 파티션 확인
+
 def solution(places):
     answer = []
     for place in places:
@@ -18,16 +22,16 @@ def checkpartition(place):
             r2, c2 = people[j]
             distance = abs(r1 - r2) + abs(c1 - c2)
             
-            if distance == 1:
+            if distance == 1: # 바로 옆에 있는 상황
                 return 0
             elif distance == 2:
-                if r1 == r2:
+                if r1 == r2: # 같은 행에 있으면 중간 열에 파티션 없으면 거리두기 X
                     if place[r1][c1 + 1] == 'O':
                         return 0
-                elif c1 == c2:
+                elif c1 == c2: # 같은 열에 있으면 중간 행에 파티션 없으면 거리두기ㅣ X
                     if place[r1 + 1][c1] == 'O':
                         return 0
-                else:
+                else: # 대각선에 있는 상황
                     if c1 < c2:
                         if not(place[r1][c2] == 'X' and place[r2][c1] == 'X'):
                             return 0
