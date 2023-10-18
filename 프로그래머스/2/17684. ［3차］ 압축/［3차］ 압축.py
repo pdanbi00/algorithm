a8e3e259d1943e774dbdfa5740af5ad8736188ab
박단비 <qkreksqhd@naver.com>
2023-10-19 01:00:01 +0900

    압축문제  주석 추가

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.py
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.py
@@ -1,51 +1,48 @@
+# 아이디어 : 종이에 적어서 풀어보면 너는 알고리즘을 구현해낼 수 있음. 오늘의 내가 해냈기 때문에
 def solution(msg):
     answer = []
     alpha = {
-        'A':1,
-        'B':2,
-        'C':3,
-        'D':4,
-        'E':5,
-        'F':6,
-        'G':7,
-        'H':8,
-        'I':9,
-        'J':10,
-        'K':11,
-        'L':12,
-        'M':13,
-        'N':14,
-        'O':15,
-        'P':16,
-        'Q':17,
-        'R':18,
-        'S':19,
-        'T':20,
-        'U':21,
-        'V':22,
-        'W':23,
-        'X':24,
-        'Y':25,
-        'Z':26
+        'A': 1,
+        'B': 2,
+        'C': 3,
+        'D': 4,
+        'E': 5,
+        'F': 6,
+        'G': 7,
+        'H': 8,
+        'I': 9,
+        'J': 10,
+        'K': 11,
+        'L': 12,
+        'M': 13,
+        'N': 14,
+        'O': 15,
+        'P': 16,
+        'Q': 17,
+        'R': 18,
+        'S': 19,
+        'T': 20,
+        'U': 21,
+        'V': 22,
+        'W': 23,
+        'X': 24,
+        'Y': 25,
+        'Z': 26
     }
     cnt = 27
-    length = len(msg)
     idx = 0
     word = ''
-    count = 0
-    while idx < length:
+    while idx < len(msg):
         word += msg[idx]
         if word in alpha.keys():
-            count += 1
             idx += 1
-            if idx == length:
-                answer.append(alpha[word])
+
         else:
             answer.append(alpha[word[:-1]])
             alpha[word] = cnt
             cnt += 1
             word = ''
-            count = 0
-        
-    
+
+    answer.append(alpha[word])
+
     return answer
--- 프로그래머스/unrated/150368. 이모티콘 할인행사/이모티콘 할인행사.py
+++ 프로그래머스/unrated/150368. 이모티콘 할인행사/이모티콘 할인행사.py
@@ -6,21 +6,21 @@ def solution(users, emoticons):
     emoticon_plus_user = 0
     emoticon_total = 0
     sale_type = list(product([10, 20, 30, 40], repeat=len(emoticons)))
-    for sale in sale_type:
+    for sale in sale_type: # 조합의 각 경우마다
         plus_user = 0
         sale_total = 0
-        for user in users:
+        for user in users: 
             price = 0
             for i in range(len(emoticons)):
-                if user[0] <= sale[i]:
+                if user[0] <= sale[i]: # 사용자가 사려는 할인율보다 크거나 같다면
                     price += emoticons[i] // 100 * (100 - sale[i])
-            if price >= user[1]:
-                plus_user += 1
-            else:
-                sale_total += price
-        if plus_user > emoticon_plus_user or plus_user == emoticon_plus_user and sale_total > emoticon_total:
-            emoticon_plus_user = plus_user
-            emoticon_total = sale_total
+            if price >= user[1]: # 이모티콘 샀을 때 금액 계산 했을때 플러스 기준 금액보다 크다면
+                plus_user += 1 # 플러스 인원 추가
+            else: # 플러스 기준 금액보다 작으면
+                sale_total += price # 총 금액에 금액 추가
+        if plus_user > emoticon_plus_user or plus_user == emoticon_plus_user and sale_total > emoticon_total: # 모든 이용자 다 계산 해봤을 때 기존까지의 플러스 인원보다 많거나 혹은 플러스 인원은 같지만 총 이모티콘 판매 금액이 더 크다면
+            emoticon_plus_user = plus_user # 답 갱신
+            emoticon_total = sale_total # 답 갱신
             
         
     
