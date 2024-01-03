0c5438ebcb82ea4ba293a0ef97be13d638553a13
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-01-03 21:39:00 +0900

    [Silver IV] Title: 큐, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+list = []
+for i in range(N):
+    line = input().split()
+    if line[0] == "push":
+        list.append(line[1])
+    elif line[0] == "pop":
+        if len(list) == 0:
+            print("-1")
+        else:
+            print(list.pop(0))
+    elif line[0] == "size":
+        print(len(list))
+    elif line[0] == "empty":
+        if len(list) == 0:
+            print("1")
+        else:
+            print("0")
+    elif line[0] == "front":
+        if len(list) == 0:
+            print("-1")
+        else:
+            print(list[0])
+    elif line[0] == "back":
+        if len(list) == 0:
+            print("-1")
+        else:
+            print(list[len(list)-1])
