2744ae343547682cfcc4f0e510dd002118c1e36e
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-01-16 00:25:31 +0900

    [Silver III] Title: 소수 구하기, Time: 596 ms, Memory: 38932 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,26 +1,17 @@
-M, N = map(int, input().split())
-# for i in range(M, N+1):
-#     for j in range(2, i-1):
-#         if i % j == 0:
-#             break
-#     else:
-#         print(i)
-#
-# arr = list(range(M, N+1))
-# for i in range(2, N):
-#     for j in range(N-M + 1):
-#         if arr[j] != i and arr[j] % i == 0:
-#             arr[j] = 0
-#         if arr[j] == i:
-#             print(arr[j])
-for i in range(M, N+1):
-    is_prime = True
-    if i == 1:
-        continue
-    for j in range(2, int(i**(0.5)) + 1):
-        if i % j == 0:
-            is_prime = False
-            break
-    if is_prime == True:
-        print(i)
+# 에라토스테네스의 체 이용
+MAX = 1000000
+check = [0] * (MAX + 1)
+check[0] = True # True가 소수가 아니라서 지운다는 의미
+check[1] = True
 
+for i in range(2, MAX + 1):
+    if not check[i]: # 안 지워졌다는건 소수라는거임. 그래서 그 수의 배수들을 싹 지운다.
+        j = i + i # i * 2임 i의 배수들 싹 다 지우겠다는거임
+        while j <= MAX:
+            check[j] = True
+            j += i # i 배수를 계속 만든다잉
+
+M, N = map(int, input().split())
+for i in range(M, N+1):
+    if not check[i]:
+        print(i)
