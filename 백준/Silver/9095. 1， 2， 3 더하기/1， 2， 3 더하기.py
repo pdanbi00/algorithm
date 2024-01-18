3ef5490c6e44513ebc5ebadcd19763e88a0bd22e
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-01-18 16:29:56 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,10 +1,16 @@
 T = int(input())
-DP = [0] * 12
-DP[1] = 1 # 1
-DP[2] = 2 # 1 + 1 , 2
-DP[3] = 4 # 1 + 1 + 1, 2 + 1, 1 + 2, 3
-for i in range(4, 12):
-    DP[i] = DP[i-3] + DP[i-2] + DP[i-1]
 for _ in range(T):
-    n = int(input())
-    print(DP[n])
+    N = int(input())
+    dp = [0] * (N+1)
+
+    for i in range(1, N+1):
+        if i == 1:
+            dp[i] = 1
+        elif i == 2:
+            dp[i] = 2
+        elif i == 3:
+            dp[i] = 4
+        else:
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+    print(dp[N])
+            
