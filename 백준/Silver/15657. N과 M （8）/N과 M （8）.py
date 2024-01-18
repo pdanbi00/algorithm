888eefde52e998324a35ad882dd310c6fb21bdbe
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-01-18 23:31:05 +0900

    [Silver III] Title: N과 M (8), Time: 56 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.py
@@ -0,0 +1,15 @@
+N, M = map(int, input().split())
+nums = list(map(int, input().split()))
+nums.sort()
+
+ans = [0] * M
+
+def func(index, start, M):
+    if index == M:
+        print(' '.join(map(str, ans)))
+        return
+    for i in range(start, N):
+        ans[index] = nums[i]
+        func(index+1, i, M)
+
+func(0, 0, M)
