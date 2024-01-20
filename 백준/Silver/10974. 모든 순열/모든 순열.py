cad190a4ef87da3f2bdebf771fe80dd633c5b2a0
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-01-20 21:32:25 +0900

    [Silver III] Title: 모든 순열, Time: 116 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10974. 모든 순열/모든 순열.py
+++ 백준/Silver/10974. 모든 순열/모든 순열.py
@@ -1,12 +1,6 @@
-def perm(index, p):
-    if index == N:
-        print(' '.join(map(str, p)))
-    for i in range(1, N+1):
-        if not used[i]:
-            used[i] = True
-            perm(index+1, p+[i])
-            used[i] = False
 
+from itertools import permutations
 N = int(input())
-used = [0] * (N+1)
-perm(0, [])
+nums = [i for i in range(1, N+1)]
+for perm in permutations(nums, N):
+    print(' '.join(map(str, list(perm))))
