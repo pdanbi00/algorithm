9f548df7d080413cf3f58160d2533cbf8f29c092
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-01-20 20:43:48 +0900

    [Silver III] Title: 다음 순열, Time: 44 ms, Memory: 32140 KB -BaekjoonHub

---
+++ 백준/Silver/10972. 다음 순열/다음 순열.py
@@ -0,0 +1,24 @@
+def next_perm(a):
+    i = len(a) - 1
+    while i > 0 and a[i-1] >= a[i]:
+        i -= 1
+    if i <= 0:
+        return False
+    j = len(a) - 1
+    while a[j] <= a[i-1]:
+        j -= 1
+    a[i-1], a[j] = a[j], a[i-1]
+
+    j = len(a) -1
+    while i < j:
+        a[i], a[j] = a[j], a[i]
+        i += 1
+        j -= 1
+    return True
+
+N = int(input())
+perm = list(map(int, input().split()))
+if next_perm(perm):
+    print(' '.join(map(str, perm)))
+else:
+    print(-1)
