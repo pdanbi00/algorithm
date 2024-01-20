c3278a05f27c5c5ad8132d0019f64635220c36f3
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-01-20 23:05:30 +0900

    [Silver II] Title: 차이를 최대로, Time: 108 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10819. 차이를 최대로/차이를 최대로.py
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.py
@@ -1,25 +1,34 @@
-def check(num):
+def next_perm(nums):
+    i = len(nums) - 1
+    while i > 0 and nums[i] <= nums[i-1]:
+        i -= 1
+    if i <= 0:
+        return False
+    j = len(nums) - 1
+    while nums[i-1] >= nums[j]:
+        j -= 1
+    nums[i-1], nums[j] = nums[j], nums[i-1]
+    j = len(nums) - 1
+    while i < j:
+        nums[i], nums[j] =  nums[j], nums[i]
+        i += 1
+        j -= 1
+    return True
+
+def calc(nums):
     total = 0
     for i in range(N-1):
-        total += abs(num[i] - num[i+1])
+        total += abs(nums[i] - nums[i+1])
     return total
-
-def func(index, num):
-    global ans
-    if index == N:
-        temp = check(num)
-        ans = max(ans, temp)
-    for i in range(N):
-        if not used[i]:
-            used[i] = True
-            func(index+1, num+[nums[i]])
-            used[i] = False
-
 
 N = int(input())
 nums = list(map(int, input().split()))
-used = [False] * N
-ans = -100000
 nums.sort()
-func(0, [])
+ans = 0
+
+while True:
+    temp = calc(nums)
+    ans = max(ans, temp)
+    if not next_perm(nums):
+        break
 print(ans)
