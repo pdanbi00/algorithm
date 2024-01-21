28e51d066e4574dc3d99eed7213df0b388a1fc89
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-01-22 00:15:00 +0900

    [Silver V] Title: 집합, Time: 1856 ms, Memory: 128204 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -1,24 +1,29 @@
 import sys
-n = 20
-m = int(sys.stdin.readline())
-s = 0
-for _ in range(m):
-    op, *num = sys.stdin.readline().split()
-    if len(num) > 0:
-        x = int(num[0])-1
-    if op == 'add':
-        s = (s | (1 << x))
-    elif op == 'remove':
-        s = (s & ~(1 << x))
-    elif op == 'check':
-        res = (s & (1 << x))
-        if res > 0:
-            sys.stdout.write('1\n')
+input = sys.stdin.readline
+
+M = int(input())
+ans = set()
+for i in range(M):
+    line = list(input().split())
+    command = line[0]
+
+    if command == 'add':
+        ans.add(int(line[1]))
+    elif command == 'remove':
+        if int(line[1]) in ans:
+            ans.remove(int(line[1]))
+    elif command == 'check':
+        if int(line[1]) in ans:
+            print(1)
         else:
-            sys.stdout.write('0\n')
-    elif op == 'toggle':
-        s = (s ^ (1 << x))
-    elif op == 'all':
-        s = (1 << n) - 1
+            print(0)
+    elif command == 'toggle':
+        if int(line[1]) in ans:
+            ans.remove(int(line[1]))
+        else:
+            ans.add(int(line[1]))
+    elif command == 'all':
+        ans = set([i for i in range(1, 21)])
     else:
-        s = 0
+        ans = set()
+
