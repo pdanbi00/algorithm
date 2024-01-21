80deeabb90408dd5d3a2712f1dd4dd057c75f2d2
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-01-22 00:31:59 +0900

    [Silver V] Title: 집합, Time: 1268 ms, Memory: 131880 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -1,29 +1,25 @@
 import sys
 input = sys.stdin.readline
-
-M = int(input())
-ans = set()
-for i in range(M):
-    line = list(input().split())
-    command = line[0]
-
-    if command == 'add':
-        ans.add(int(line[1]))
-    elif command == 'remove':
-        if int(line[1]) in ans:
-            ans.remove(int(line[1]))
-    elif command == 'check':
-        if int(line[1]) in ans:
+n = 20
+m = int(input())
+s = 0
+for _ in range(m):
+    op, *num = input().split() # 이렇게 하면 띄워쓰기 뒤에 뭐가 더 있으면 ['10'] 이런식으로 되고 없으면 [] 이렇게 됨.
+    if len(num) > 0:
+        x = int(num[0]) - 1 # 숫자는 1부터 20까지 있으니깐 비트마스크로는 0부터 확인하니깐 1 빼기
+    if op == 'add':
+        s = (s | (1 << x))
+    elif op == 'remove':
+        s = (s & ~(1 << x))
+    elif op == 'check':
+        res = (s & (1 << x))
+        if res > 0:
             print(1)
         else:
             print(0)
-    elif command == 'toggle':
-        if int(line[1]) in ans:
-            ans.remove(int(line[1]))
-        else:
-            ans.add(int(line[1]))
-    elif command == 'all':
-        ans = set([i for i in range(1, 21)])
+    elif op == 'toggle':
+        s = (s ^ (1 << x))
+    elif op == 'all':
+        s = (1 << 20) - 1
     else:
-        ans = set()
-
+        s = 0
