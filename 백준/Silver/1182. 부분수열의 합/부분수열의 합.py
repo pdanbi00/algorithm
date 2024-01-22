bc18c3e95d875816d3845b2e12a9abc068412425
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-01-22 18:17:44 +0900

    [Silver II] Title: 부분수열의 합, Time: 3516 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,11 +1,11 @@
-from itertools import combinations
-
 N, S = map(int, input().split())
 nums = list(map(int, input().split()))
-cnt = 0
-for i in range(1, N+1):
-    for com in combinations(nums, i):
-        if sum(com) == S:
-            cnt += 1
-
-print(cnt)
+ans = 0
+for i in range(1, (1 << N)): # N에 해당하는 값은 (1 << N) - 1 인데 range는 마지막 값-1이니깐 구간 (1<<N). 그리고 크기가 양수라고 했으니깐 공집합 제외하기 위해서 1부터
+    sum = 0
+    for j in range(N):
+        if i & (1<<j):
+            sum += nums[j]
+    if sum == S:
+        ans += 1
+print(ans)
