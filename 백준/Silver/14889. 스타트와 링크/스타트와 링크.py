0f866e4a6e1a6c3585818188ed62c28059b7c6f8
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-01-22 22:29:50 +0900

    [Silver I] Title: 스타트와 링크, Time: 468 ms, Memory: 117996 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -1,32 +1,40 @@
+# 조합으로 팀 나누기
+# 각 팀별로 능력치 구하기
+# 능력치 차이 최소 답으로
+def solve(n, r, s):
+    global min_v
+
+    if r == 0:
+        start_total = 0
+        link_total = 0
+        idx = 0
+        for n in num:
+            if n not in team_start:
+                team_link[idx] = n
+                idx += 1
+
+        for i in range((N // 2) - 1):
+            for j in range(i + 1, N // 2):
+                start_total += board[team_start[i]][team_start[j]]
+                start_total += board[team_start[j]][team_start[i]]
+                link_total += board[team_link[i]][team_link[j]]
+                link_total += board[team_link[j]][team_link[i]]
+
+        ans = abs(start_total - link_total)
+        if ans < min_v:
+            min_v = ans
+        return
+    else:
+        for i in range(s, n-r+1):
+            team_start[N//2 - r] = num[i]
+            solve(n, r-1, i+1)
+
 N = int(input())
-powers = [list(map(int, input().split())) for _ in range(N)]
+board = [[0]*(N+1)] + [[0] + list(map(int, input().split())) for _ in range(N)]
+min_v = 100 * N
+num = [x for x in range(1, N+1)]
+team_start = [0] * (N//2)
+team_link = [0] * (N//2)
+solve(N, N//2, 0)
 
-def func(index, start, link):
-    if index == N:
-        if len(start) != N//2:
-            return -1
-        if len(link) != N//2:
-            return -1
-        s_total = 0
-        l_total = 0
-        for i in range(N//2):
-            for j in range(N//2):
-                if i == j:
-                    continue
-                s_total += powers[start[i]][start[j]]
-                l_total += powers[link[i]][link[j]]
-        return abs(s_total - l_total)
-
-    if len(start) > (N//2) or len(link) > (N//2):
-        return -1
-    ans = -1
-    s_total = func(index+1, start+[index], link)
-    if ans == -1 or (s_total != -1 and ans > s_total):
-        ans = s_total
-    l_total = func(index+1, start, link+[index])
-    if ans == -1 or (l_total != -1 and ans > l_total):
-        ans = l_total
-    return ans
-
-print(func(0, [], []))
-
+print(min_v)
