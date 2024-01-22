5df51a7dba3c3e722193a499c0a7f69b963f2021
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-01-23 02:20:43 +0900

    [Gold III] Title: 종이 조각, Time: 192 ms, Memory: 111128 KB -BaekjoonHub

--- 백준/Gold/14391. 종이 조각/종이 조각.py
+++ 백준/Gold/14391. 종이 조각/종이 조각.py
@@ -5,33 +5,28 @@ ans = -1
 for i in range(1<<(N*M)):
     visited = [False] * (N*M)
     nums = 0
-    for j in range(N*M-1, -1, -1):
-        ga = 0
-        sero = 0
-        if i & (1 << j) == 0 and not visited[(N*M - 1)-j]: # 방문하지 않은 가로라면
-            visited[(N*M - 1)-j] = True
-            ga = board[((N*M - 1)-j)//M][((N*M - 1)-j)%M]
+    for j in range(N*M):
+        if i & (1 << j) == 0 and not visited[j]: # 방문하지 않은 가로라면
+            visited[j] = True
+            ga = board[j//M][j%M]
             for k in range(1, M):
-                if j - k >= 0:
-                    if i & (1 << (j-k)) == 0 and (j-k) % M != (M-1) and not visited[(N*M - 1)-(j-k)]:
-                        visited[(N*M - 1)-(j-k)] = True
-                        ga += board[((N*M - 1)-(j-k))//M][((N*M - 1)-(j-k))%M]
-                    else:
-                        break
+                if i & (1 << (j+k)) == 0 and (j+k) % M != 0 and not visited[j+k]:
+                    visited[j+k] = True
+                    ga += board[(j+k)//M][(j+k)%M]
+                else:
+                    break
 
             nums += int(ga)
-        elif i & (1 << j) >0  and not visited[(N*M - 1)-j]: # 방문하지 않은 세로라면
-            visited[(N*M - 1)-j] = True
-            sero = board[((N*M - 1)-j)//M][((N*M - 1)-j)%M]
+        elif i & (1 << j) != 0 and not visited[j]: # 방문하지 않은 tpfh라면
+            visited[j] = True
+            se = board[j//M][j%M]
             for k in range(1, N):
-                if j - (k*M) >= 0:
-                    if i & (1 << (j-(k*M))) > 0 and (j-(k*M)) >= 0 and not visited[(N*M - 1)-(j-(k*M))]:
-                        visited[(N*M - 1)-(j-(k*M))] = True
-                        sero += board[((N*M - 1)-(j-(k*M)))//M][((N*M - 1)-(j-(k*M)))%M]
-                    else:
-                        break
-            nums += int(sero)
-
+                if i & (1 << (j+(k*M))) != 0 and (j+(k*M)) < (N*M) and not visited[j+(k*M)]:
+                    visited[j+(k*M)] = True
+                    se += board[(j+(k*M))//M][(j+(k*M))%M]
+                else:
+                    break
+            nums += int(se)
     if nums > ans:
         ans = nums
 print(ans)
