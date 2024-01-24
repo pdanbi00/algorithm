ecd5aacbb8530299dc82138a66f384f368e433ce
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-01-24 22:54:11 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 208 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.py
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.py
@@ -1,16 +1,8 @@
-# 아이디어 : DP임
-# n 개를 사는 방법 : (1개 사는 방법 + n-1개 사는 방법) + (2개 사는 방법 + n-2개 사는 방법) + ... + (n개 사는 방법)
-# dp[i]를 i개를 사는 최소 비용
-# dp[i] = dp[i-j] + card[j]
 N = int(input())
-card = list(map(int, input().split()))
-dp = [0 for _ in range(N+1)] # dp[1] : 1개의 카드를 살 수 있는 최소 비용
+p = [0] + list(map(int, input().split()))
+dp = [10000000] * (N+1)
+dp[0] = 0
 for i in range(1, N+1):
     for j in range(1, i+1):
-        if dp[i] == 0:
-            dp[i] = dp[i-j] + card[j-1]
-        else:
-            dp[i] = min(dp[i], dp[i-j] + card[j-1])
+        dp[i] = min(dp[i], dp[i-j]+p[j])
 print(dp[N])
-
-
