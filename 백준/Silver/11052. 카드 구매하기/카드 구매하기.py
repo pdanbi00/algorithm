dbc00e9454978f6576e01ce716bc9073133cb243
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-01-24 22:37:46 +0900

    [Silver I] Title: 카드 구매하기, Time: 204 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -0,0 +1,7 @@
+N = int(input())
+card = [0] + list(map(int, input().split()))
+dp = [0] * (N+1)
+for i in range(1, N+1):
+    for j in range(1, i+1):
+        dp[i] = max(dp[i], dp[i-j]+card[j])
+print(dp[N])
