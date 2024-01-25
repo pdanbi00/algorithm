96946055dfcea7a65a95797f7faf2aaaf0340068
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-01-26 01:13:07 +0900

    [Silver III] Title: 퇴사, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -1,22 +1,15 @@
-n = int(input())
-arr = [(0,0)]
-dp = [0 for _ in range(n+1)]
+N = int(input())
+T = [0] * (N)
+P = [0] * (N)
+dp = [0] * (N+1)
+for i in range(N):
+    T[i], P[i] = map(int, input().split())
 
-for _ in range(1, n+1) :
-  t,p = map(int, input().split())
-  arr.append((t,p))
-
-for i in range(1, n+1) :
-  time = arr[i][0]
-  money = arr[i][1]
-  if i+time > n+1 : # 상담이 n일전에 끝나지 않는다면 
-    continue
-  else :
-    dp[i] = max(dp[i], money)
-    for j in range(i+time, n+1) :
-      next_time = arr[j][0]
-      if j + next_time <= n+1 :
-        dp[j] = max(dp[j], arr[j][1]+dp[i])
-    
-
-print(max(dp))
+for i in range(N-1, -1, -1):
+    # i일에 상담을 하는게 퇴사일을 넘기면 상담 안한다.
+    if i + T[i] > N:
+        dp[i] = dp[i+1]
+    else:
+        # i일에 상담을 하는거랑 상담 안하는거 중에 큰거 선택
+        dp[i] = max(dp[i+1], dp[i+T[i]] + P[i])
+print(dp[0])
