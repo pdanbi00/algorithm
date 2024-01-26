d7bf080ef27eccb17a3d03591a1cbf9e703b0026
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-01-26 22:37:40 +0900

    [Gold V] Title: 합분해, Time: 744 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.py
+++ 백준/Gold/2225. 합분해/합분해.py
@@ -4,9 +4,6 @@ dp[0][0] = 1
 for i in range(1, K+1):
     for j in range(N+1):
         for l in range(j+1):
-            if i == 1:
-                dp[i][j] = 1
-            else:
-                dp[i][j] += dp[i-1][j-l]
-                dp[i][j] %= 1000000000
-print(dp[K][N] % 1000000000)
+            dp[i][j] += dp[i-1][j-l]
+        dp[i][j] %= 1000000000
+print(dp[K][N])
