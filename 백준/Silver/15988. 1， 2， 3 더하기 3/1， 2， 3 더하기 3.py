c41318dd92139b8ad07965ea638f9d832916966d
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-01-27 02:22:50 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 708 ms, Memory: 70676 KB -BaekjoonHub

--- 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.py
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.py
@@ -4,13 +4,11 @@ dp[0] = 1
 for i in range(1, MAX+1):
     if i - 1 >= 0:
         dp[i] += dp[i-1]
-        dp[i] %= 1000000009
     if i - 2 >= 0:
         dp[i] += dp[i-2]
-        dp[i] %= 1000000009
     if i - 3 >= 0:
         dp[i] += dp[i-3]
-        dp[i] %= 1000000009
+    dp[i] %= 1000000009
 
 T = int(input())
 for tc in range(T):
