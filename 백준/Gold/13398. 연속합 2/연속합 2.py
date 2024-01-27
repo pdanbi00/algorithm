689bfdfe1f404d8e8e4a4c86bfaed770408686b2
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-01-27 23:42:22 +0900

    [Gold V] Title: 연속합 2, Time: 140 ms, Memory: 38828 KB -BaekjoonHub

--- 백준/Gold/13398. 연속합 2/연속합 2.py
+++ 백준/Gold/13398. 연속합 2/연속합 2.py
@@ -2,12 +2,18 @@ N = int(input())
 nums = list(map(int, input().split()))
 dp_1 = [0] * N # dp_1[i]는 nums[i]로 끝나는 연속된 수의 가장 큰 합
 dp_2 = [0] * N # dp_2[i]는 nums[i]로 시작하는 연속된 수의 가장 큰 합
-dp_1[0] = nums[0]
-for i in range(1, N):
-    dp_1[i] = max(dp_1[i-1] + nums[i], nums[i])
-dp_2[N-1] = nums[-1]
-for i in range(N-2, -1, -1):
-    dp_2[i] = max(dp_2[i+1] + nums[i], nums[i])
+for i in range(N):
+    dp_1[i] = nums[i]
+    if i == 0:
+        continue
+    if dp_1[i] < dp_1[i-1] + nums[i]:
+        dp_1[i] = dp_1[i-1] + nums[i]
+for i in range(N-1, -1, -1):
+    dp_2[i] = nums[i]
+    if i == N-1:
+        continue
+    if dp_2[i] < dp_2[i+1] + nums[i]:
+        dp_2[i] = dp_2[i+1] + nums[i]
 ans = max(dp_1)
 for i in range(1, N-1):
     if dp_1[i-1] + dp_2[i+1] > ans:
