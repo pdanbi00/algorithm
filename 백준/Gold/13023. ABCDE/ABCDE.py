cdc3adb827c5d21f8362280d68d5bddec1de8a4f
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-01-29 00:03:03 +0900

    [Gold V] Title: ABCDE, Time: 1752 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.py
+++ 백준/Gold/13023. ABCDE/ABCDE.py
@@ -1,29 +1,41 @@
 import sys
+input = sys.stdin.readline
 N, M = map(int, input().split())
-edges = [] # 간선리스트
-a = [[False] * N for _ in range(N)] # 인접행렬 - a[i][j] : i번 노드랑 j번 노드랑 연결되어 있다.
-g = [[] for _ in range(N)] # 인접 리스트 - a[i] : i번 노드랑 연결된 모든 노드들 정보가 들어있음
-for _ in range(M):
-    v1, v2 = map(int, input().split())
-    edges.append((v1, v2))
-    edges.append((v2, v1))
-    a[v1][v2] = True
-    a[v2][v1] = True
-    g[v1].append(v2)
-    g[v2].append(v1)
+relations = [[] for _ in range(N)]
+# 방문 표시
+visited = [False] * N
+# 정답인지 판단
+ans = False
 
-M *= 2 # 양쪽 노드 모두에 간선정보 담았으니깐 2배해줌
 for i in range(M):
-    for j in range(M):
-        A, B = edges[i]
-        C, D = edges[j]
-        if A == B or A == C or A == D or B == C or B == D or C == D:
-            continue
-        if not a[B][C]:
-            continue
-        for E in g[D]:
-            if A == E or B == E or C == E or D == E:
-                continue
-            print(1)
-            sys.exit(0)
-print(0)
+    v1, v2 = map(int, input().split())
+    relations[v1].append(v2)
+    relations[v2].append(v1)
+
+# dfs
+def dfs(index, depth):
+    global ans
+    visited[index] = True
+    # 친구관계가 4번 이상 연결되어 있으면
+    if depth == 4:
+        ans = True
+        return
+    # 친구관계가 존재하는지 확인
+    for i in relations[index]:
+        # 아직 방문 안했으면
+        if not visited[i]:
+            visited[i] = True
+            # 재귀적으로 수행
+            dfs(i, depth+1)
+            visited[i] = False
+
+# 0번부터 N-1번까지 확인
+for i in range(N):
+    dfs(i, 0)
+    visited[i] = False
+    if ans:
+        break
+if ans:
+    print(1)
+else:
+    print(0)
