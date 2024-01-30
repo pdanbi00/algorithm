87d3749f364480d8e5f0df7ab4df9469d96b0c4f
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-01-30 21:48:16 +0900

    [Silver I] Title: 숨바꼭질, Time: 244 ms, Memory: 41520 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,45 +1,22 @@
-# 이건 시간 터짐
-
-# def solve(n, k, cnt):
-#     global min_v
-#     if cnt > min_v:
-#         return
-#     if n == k:
-#         min_v = min(min_v, cnt)
-#         return
-#     else:
-#         if n + 1 <= 100000:
-#             solve(n+1, k, cnt+1)
-#         if n - 1 >= 0:
-#             solve(n-1, k, cnt+1)
-#         if 0 <= n * 2 <= 100000:
-#             solve(n*2, k, cnt+1)
-#
-# N, K = map(int, input().split())
-# min_v = 100000
-# solve(N, K, 0)
-# print(min_v)
-
-def bfs(n, k):
-    stack = []
-    visited = [0] * 100001
-    cnt = 0
-    stack.append((n, cnt))
-    visited[n] = 1
-    while stack:
-        x, xcnt = stack.pop(0)
-        xcnt += 1
-        for nx in ((x+1), (x-1), (x*2)):
-            if 0 <= nx <= 100000 and not visited[nx]:
-                if nx == k:
-                    return print(xcnt)
-                else:
-                    stack.append((nx, xcnt))
-                    visited[nx] = 1
-
-
+from collections import deque
+MAX = 200000
 
 N, K = map(int, input().split())
-bfs(N, K)
-if N == K:
-    print(0)
+
+board = [-1] * (MAX+1)
+checked = [False] * (MAX+1)
+
+q = deque()
+q.append(N)
+board[N] = 0
+checked[N] = True
+while q:
+    idx = q.popleft()
+    if idx == K:
+        print(board[idx])
+        break
+    for n_idx in (idx-1, idx+1, idx*2):
+        if 0 <= n_idx <= MAX and checked[n_idx] == False:
+            q.append(n_idx)
+            checked[n_idx] = True
+            board[n_idx] = board[idx] + 1
