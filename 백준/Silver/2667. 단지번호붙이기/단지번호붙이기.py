14f2d4c201d05610fa5eae376984b1844b52d69b
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-01-30 15:13:32 +0900

    [Silver I] Title: 단지번호붙이기, Time: 40 ms, Memory: 31196 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,40 +1,31 @@
-def dfs(r,c):
-    stack = []
-    stack.append((i, j))
-    visited[i][j] = 1
-    cnt = 0
-    while stack:
-        r, c = stack[-1]
+N  = int(input())
+house = [list(map(int, input())) for _ in range(N)]
+num = []
 
-        # 상하좌우
-        for dr, dc in ((-1, 0), (1, 0), (0, -1), (0, 1)):
-            nr = r + dr
-            nc = c + dc
-            if 0 <= nr < N and 0 <= nc < N and visited[nr][nc] == 0 and board[nr][nc] == 1:
-                stack.append((nr, nc))
-                visited[nr][nc] = 1
-                r = nr
-                c = nc
-                break
-        else:
-            stack.pop()
-            cnt += 1
-    return cnt
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
 
+def dfs(x, y):
+    global count
+    count += 1
+    house[x][y] = 0
+    for k in range(4):
+        nx = x + dx[k]
+        ny = y + dy[k]
+        if 0 <= nx < N and 0 <= ny < N:
+            if house[nx][ny] != 0:
+                dfs(nx, ny)
 
-
-N = int(input())
-board = [list(map(int, input())) for _ in range(N)]
-visited = [list([0] * N) for _ in range(N)]
+count = 0
 total = 0
-total_list = []
 for i in range(N):
     for j in range(N):
-        if board[i][j] == 1 and visited[i][j] == 0:
+        if house[i][j] != 0:
+            dfs(i, j)
+            num.append(count)
             total += 1
-            total_list.append(dfs(i,j))
-# 오름차순으로 출력하라 했으니까 정렬
-total_list.sort()
+            count = 0
+num.sort()
 print(total)
-for i in range(total):
-    print(total_list[i])
+for i in num:
+    print(i)
