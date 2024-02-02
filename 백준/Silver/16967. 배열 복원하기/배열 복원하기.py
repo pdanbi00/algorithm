3bd7b57bf1d12665a08689e87e2ca6ff64f6501d
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-02-02 23:42:47 +0900

    [Silver III] Title: 배열 복원하기, Time: 168 ms, Memory: 37608 KB -BaekjoonHub

--- 백준/Silver/16967. 배열 복원하기/배열 복원하기.py
+++ 백준/Silver/16967. 배열 복원하기/배열 복원하기.py
@@ -1,12 +1,8 @@
 H, W, X, Y = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(H+X)]
 
-arr = [[0] * W for _ in range(H)]
 for i in range(H):
     for j in range(W):
-        if i >= X and j >= Y:
-            arr[i][j] = board[i][j] - arr[i-X][j-Y]
-        else:
-            arr[i][j] = board[i][j]
+        board[i+X][j+Y] -= board[i][j]
 for i in range(H):
-    print(*arr[i])
+    print(*board[i][:W])
