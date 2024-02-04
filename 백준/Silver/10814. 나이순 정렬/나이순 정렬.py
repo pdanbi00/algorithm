a5eeb9a22998ce4f501d48595793ac1e6afe7ffd
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-02-05 00:04:16 +0900

    [Silver V] Title: 나이순 정렬, Time: 4052 ms, Memory: 48188 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -3,7 +3,11 @@ people = []
 for i in range(N):
     age, name = input().split()
     age = int(age)
-    people.append([i, age, name])
-people.sort(key=lambda x: (x[1], x[0]))
+    people.append([age, name])
+# 파이썬은 기본적으로 stable 정렬을 함.
+# 그래서 기준을 주는대로 정렬하되 처음 들어온 순서를 유지하면서 함.
+# 예를 들어, [1, 2, 3(X), 4, 5, 3(Y)] 을 오름차순 정렬한다면,
+# [1, 2, 3(X), 3(Y), 4, 5]순으로 세 번째 위치한 3의 위치와 여섯 번째 위치한 3의 위치가 바뀌지 않는다.
+people.sort(key=lambda x: x[0])
 for person in people:
-    print(person[1], person[2])
+    print(person[0], person[1])
