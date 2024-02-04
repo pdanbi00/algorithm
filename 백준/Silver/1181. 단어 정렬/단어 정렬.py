e949b224cfe0bee810126fd18a39159f60c64965
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-02-04 23:17:59 +0900

    [Silver V] Title: 단어 정렬, Time: 780 ms, Memory: 35732 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -4,6 +4,8 @@ words = set(words)
 words = list(words)
 
 words.sort()
-words.sort(key=lambda x: len(x))
+# words.sort(key=lambda x: len(x))
+# sort의 key는 함수를 받기 때문에
+words.sort(key=len) # 이렇게 하면 됨
 for w in words:
     print(w)
