4872b839575fab295054f7aeefc5fedc9ec69b87
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-02-05 10:03:42 +0900

    [Bronze I] Title: 이항 계수 1, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -1,11 +1,9 @@
 N, K = map(int, input().split())
-total = 1
-k_total = 1
-n_k_total = 1
-for i in range(1, N+1):
-    total *= i
-for i in range(1, K+1):
-    k_total *= i
-for i in range(1, N-K+1):
-    n_k_total *= i
-print(total // (k_total*n_k_total))
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+print(factorial(N) // (factorial(K) * factorial(N-K)))
