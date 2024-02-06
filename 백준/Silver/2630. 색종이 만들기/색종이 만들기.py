2308790ccde2ffbaeb4ed4ca6d832d5890a9b449
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-02-07 01:31:01 +0900

    [Silver II] Title: 색종이 만들기, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -1,37 +1,23 @@
 N = int(input())
 board = [list(map(int, input().split())) for _ in range(N)]
-w_count = 0
-b_count = 0
-paper_list = []
-l_size = N
-l_count = N // l_size
+blue = 0
+white = 0
 
-def check(arr):
-    global w_count, b_count
-    l = len(arr)
-    c = arr[0][0]
-    b = [[0]*(l//2) for _ in range(l//2)]
-    possible = True
-    for i in range(l):
-        for j in range(l):
-            if arr[i][j] != c:
-                possible = False
-                break
-        if not possible:
-            break
+def solution(x, y, N):
+    global white, blue
+    color = board[x][y]
+    for i in range(x, x+N):
+        for j in range(y, y+N):
+            if board[i][j] != color:
+                solution(x, y, N//2)
+                solution(x+N//2, y, N//2)
+                solution(x, y+N//2, N//2)
+                solution(x+N//2, y+N//2, N//2)
+                return
+    if color == 1:
+        blue += 1
     else:
-        if c == 1:
-            b_count += 1
-        else:
-            w_count += 1
-    if not possible:
-        for i in range(2):
-            for j in range(2):
-                for k in range(l//2):
-                    for p in range(l//2):
-                        b[k][p] = arr[i*(l//2)+k][j*(l//2)+p]
-                check(b)
-
-check(board)
-print(w_count)
-print(b_count)
+        white += 1
+solution(0, 0, N)
+print(white)
+print(blue)
