bff10f3ac19d107ed5ab3ab004d85d386c9d8c54
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-02-11 23:42:17 +0900

    [Silver III] Title: 파도반 수열, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.py
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.py
@@ -1,16 +1,13 @@
-trangles = [0] * 100
-trangles[0] = 1
+trangles = [0] * 101
+
 trangles[1] = 1
 trangles[2] = 1
-trangles[3] = 2
-trangles[4] = 2
-start = 0
-end = 4
-for i in range(5, 100):
-    trangles[i] = trangles[start] + trangles[end]
-    start += 1
-    end += 1
+trangles[3] = 1
+
+for i in range(4, 101):
+    trangles[i] = trangles[i-3] + trangles[i-2]
+
 T = int(input())
 for _ in range(T):
     N = int(input())
-    print(trangles[N-1])
+    print(trangles[N])
