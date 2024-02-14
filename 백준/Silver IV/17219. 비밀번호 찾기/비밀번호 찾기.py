62a314c25f9f68c4a54a8f3bd7e5563be4bfe6ce
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-02-14 23:54:23 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 220 ms, Memory: 49208 KB -BaekjoonHub

--- 백준/Silver IV/17219. 비밀번호 찾기/비밀번호 찾기.py
+++ 백준/Silver IV/17219. 비밀번호 찾기/비밀번호 찾기.py
@@ -1,8 +1,10 @@
+import sys
+input = sys.stdin.readline
 N, M = map(int, input().split())
 dict = {}
 for i in range(N):
     site, password = input().split()
     dict[site] = password
 for i in range(M):
-    site = input()
+    site = input().rstrip()
     print(dict[site])
