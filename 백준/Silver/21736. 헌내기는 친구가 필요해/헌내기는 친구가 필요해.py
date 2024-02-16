a9a1317c20bbafac0d238f7da53cf67c3767f25a
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-02-16 23:47:09 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 888 ms, Memory: 37080 KB -BaekjoonHub

---
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
@@ -0,0 +1,30 @@
+# 이것은 누가봐도 bfs로 풀 수 있음
+from collections import deque
+N, M = map(int, input().split())
+board = [input() for _ in range(N)]
+checked= [[False] * M for _ in range(N)]
+dr = [-1, 1, 0, 0]
+dc = [0, 0, -1, 1]
+ans = 0
+q = deque()
+for i in range(N):
+    for j in range(M):
+        if board[i][j] == 'I':
+            q.append((i, j))
+            checked[i][j] = True
+            while q:
+                r, c = q.popleft()
+                for k in range(4):
+                    nr, nc = r + dr[k], c + dc[k]
+                    if 0 <= nr < N and 0 <= nc < M:
+                        if board[nr][nc] != 'X' and not checked[nr][nc]:
+                            q.append((nr, nc))
+                            checked[nr][nc] = True
+                            if board[nr][nc] == 'P':
+                                ans += 1
+if ans == 0:
+    print('TT')
+else:
+    print(ans)
+
+
