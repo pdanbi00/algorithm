77aa8fc7660a908a93b60c26d45b7f397e567f08
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-02-18 22:33:01 +0900

    [Bronze II] Title: 분해합, Time: 1416 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -1,16 +1,11 @@
+import sys
+input = sys.stdin.readline
 N = int(input())
-num = [i for i in range(N+1)]
-find = False
 for i in range(1, N+1):
-    n = i
-    while n >= 10:
-        num[i] += n // 10**(len(str(n))-1)
-        n %= 10**(len(str(n))-1)
-    num[i] += n
-for i in range(N+1):
-    if num[i] == N:
-        find = True
+    nums = list(map(int, str(i)))
+    result = sum(nums) + i
+    if result == N:
         print(i)
         break
-if find == False:
-    print(0)
+    if i == N:
+        print(0)
