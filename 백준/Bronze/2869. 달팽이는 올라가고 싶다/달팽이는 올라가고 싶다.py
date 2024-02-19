9dd3b7b39d84781ae5fca3a1d80c7551f845cdee
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-02-19 15:25:25 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
@@ -1,5 +1,12 @@
-from math import ceil
+# 달팽이는 낮에 나무 막대에 다 올라갈거임
+# k일에 도달한다고 하면 올라가는 횟수는 k번, 내려오는 횟수는 k-1번
+# 총 올라간거는 V = Ak - B(k-1)
+# k = (V-B) / (A-B)
+# 이 값이 정수라면 k일에 다 올라간거임.
+# 이 값이 정수가 아니면 하루 더 지나서 올라갈거임
 A, B, V = map(int, input().split())
-height = V - A
-ans= ceil(height/(A-B)) + 1
-print(ans)
+x = (V-B) / (A-B)
+if x == int(x):
+    print(int(x))
+else:
+    print(int(x)+1)
