ef0f6b1c2130d958e4b37d86e9890642521d638f
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-02-27 14:44:25 +0900

    [Bronze I] Title: 더하기 사이클, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
@@ -1,18 +1,13 @@
 N = int(input())
-cnt = 1
-n = 0
-if N >= 10:
-    n = N // 10 + N % 10
-    n = (N % 10) * 10 + (n % 10)
-else:
-    n = N * 11
-
-while N != n:
-    if n >= 10:
-        temp = n
-        n = temp // 10 + temp % 10
-        n = (temp % 10) * 10 + (n % 10)
-    else:
-        n = n * 11
+num = N
+cnt = 0
+while True:
+    a = num // 10
+    b = num % 10
+    c = (a + b) % 10
+    num = b * 10 + c
     cnt += 1
+    if num == N:
+        break
+
 print(cnt)
