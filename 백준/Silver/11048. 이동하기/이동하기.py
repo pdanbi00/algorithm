71651fe40ee27f907e7e31dab43cc41b70a81771
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-02-28 23:53:12 +0900

    [Silver II] Title: 이동하기, Time: 1012 ms, Memory: 78664 KB -BaekjoonHub

---
+++ 백준/Silver/11048. 이동하기/이동하기.py
@@ -0,0 +1,17 @@
+N, M = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(N)]
+dp = [[0]*M for _ in range(N)]
+for i in range(N):
+    for j in range(M):
+        if i == 0 and j > 0:
+            dp[i][j] = dp[i][j-1] + board[i][j]
+        elif i > 0 and j == 0:
+            dp[i][j] = dp[i-1][j] + board[i][j]
+        else:
+            dp[i][j] = board[i][j]
+if N >= 2 and M >= 2:
+    for i in range(1, N):
+        for j in range(1, M):
+            dp[i][j] = max(dp[i-1][j-1], dp[i-1][j], dp[i][j-1]) + board[i][j]
+
+print(dp[N-1][M-1])
