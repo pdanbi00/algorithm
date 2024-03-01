459091884f7c73dda92c474f422a4306e6188c02
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-03-01 09:50:02 +0900

    [Silver IV] Title: 한수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.py
+++ 백준/Silver/1065. 한수/한수.py
@@ -1,14 +1,9 @@
 N = int(input())
 cnt = 0
 for i in range(1, N+1):
-    num = str(i)
-    not_answer = False
-    if len(num) >= 2:
-        diff = int(num[0]) - int(num[1])
-        for j in range(1, len(num)-1):
-            if int(num[j]) - int(num[j+1]) != diff:
-                not_answer = True
-                break
-    if not not_answer:
+    num_list = list(map(int, str(i)))
+    if i < 100:
+        cnt += 1 # 100보다 작으면 다 한수...
+    elif num_list[0] - num_list[1] == num_list[1] - num_list[2]:
         cnt += 1
 print(cnt)
