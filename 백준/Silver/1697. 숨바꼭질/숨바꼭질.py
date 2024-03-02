7cf6d42430817c46f1bd98f6ebd4166d3be2ef30
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-03-02 16:39:48 +0900

    [Silver I] Title: 숨바꼭질, Time: 208 ms, Memory: 39148 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,22 +1,21 @@
+# 가장 빠른 시간? BFS
 from collections import deque
-MAX = 200000
-
 N, K = map(int, input().split())
-
-board = [-1] * (MAX+1)
-checked = [False] * (MAX+1)
-
+visited = [0] * 200001
 q = deque()
 q.append(N)
-board[N] = 0
-checked[N] = True
+visited[N] = 0
 while q:
-    idx = q.popleft()
-    if idx == K:
-        print(board[idx])
+    now = q.popleft()
+    if now == K:
+        print(visited[now])
         break
-    for n_idx in (idx-1, idx+1, idx*2):
-        if 0 <= n_idx <= MAX and checked[n_idx] == False:
-            q.append(n_idx)
-            checked[n_idx] = True
-            board[n_idx] = board[idx] + 1
+    if now - 1 >= 0 and visited[now-1] == 0:
+        q.append(now-1)
+        visited[now-1] = visited[now] + 1
+    if now + 1 <= 200000 and visited[now+1] == 0:
+        q.append(now+1)
+        visited[now+1] = visited[now] + 1
+    if now * 2 <= 200000 and visited[now*2] == 0:
+        q.append((now*2))
+        visited[now*2] = visited[now] + 1
