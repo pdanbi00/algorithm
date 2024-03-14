62d293c59165be999371c9d3d69acc31fe4aad25
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-03-14 22:35:16 +0900

    [Gold IV] Title: 알파벳, Time: 2256 ms, Memory: 52620 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,24 +1,22 @@
+from collections import deque
 import sys
 input = sys.stdin.readline
 R, C = map(int, input().split())
 board = [input() for _ in range(R)]
-visited = [[0] * C for _ in range(R)]
+
 dr = [-1, 1, 0, 0]
 dc = [0, 0, -1, 1]
 ans = 1
-def dfs(r, c, alpha):
-    global ans
-    visited[r][c] = 1
-    alpha += board[r][c]
+q = set()
+q.add((0, 0, board[0][0]))
+while q:
+    r, c, line = q.pop()
     for i in range(4):
         nr = r + dr[i]
         nc = c + dc[i]
         if 0 <= nr < R and 0 <= nc < C:
-            if visited[nr][nc] == 0 and board[nr][nc] not in alpha:
-                dfs(nr, nc, alpha)
-                visited[nr][nc] = 0
-    if len(alpha) > ans:
-        ans = len(alpha)
+            if board[nr][nc] not in line:
+                q.add((nr, nc, line+board[nr][nc]))
+                ans = max(ans, len(line)+1)
 
-dfs(0, 0, '')
 print(ans)
