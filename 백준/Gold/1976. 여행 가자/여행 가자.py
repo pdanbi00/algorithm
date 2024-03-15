83adf77636d15e16560ceb4a36039a39723aaa5c
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-03-15 13:24:13 +0900

    [Gold IV] Title: 여행 가자, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.py
+++ 백준/Gold/1976. 여행 가자/여행 가자.py
@@ -1,37 +1,3 @@
-# # bfs
-# # 각 여행 계획지별로 A, B, C 순서라면 A, B까지 갈 수 있는지 bfs, B에서 C까지 갈 수 있는지 각각 bfs실행
-# from collections import deque
-# N = int(input())
-# M = int(input())
-# board = [list(map(int, input().split())) for _ in range(N)]
-# route = list(map(int, input().split()))
-#
-#
-# def bfs(start, end):
-#     visitied = [0] * N
-#     q = deque()
-#     q.append(start)
-#     visitied[start] = 1
-#     while q:
-#         idx = q.popleft()
-#         if idx == end:
-#             return True
-#
-#         for k in range(N):
-#             if board[idx][k] == 1 and visitied[k] == 0:
-#                 q.append(k)
-#                 visitied[k] = 1
-#     # 다음 여행지로 못가는 경우
-#     return False
-#
-# ans = "YES"
-# for i in range(M-1):
-#     possible = bfs(route[i]-1, route[i+1]-1)
-#     if not possible:
-#         ans = "NO"
-#         break
-# print(ans)
-#
 # Union Find
 # 특정 정점부터 특정 정점까지 이어져있는지 확인할 수 있다.
 
