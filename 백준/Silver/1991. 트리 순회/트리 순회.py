cee12b84939e4f1f86760e7761169901d0f06e77
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-03-15 16:34:05 +0900

    [Silver I] Title: 트리 순회, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.py
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -1,51 +1,33 @@
+# 딕셔너리로 하면 문자 숫자로 변환하고 이거 안해도 됨~~
 N = int(input())
-front = []
-middle = []
-last = []
-graph = [[-1]*2 for _ in range(N)]
-
+dict = {}
 for i in range(N):
     node, left, right = input().split()
-    if left != '.':
-        graph[ord(node)-65][0] = ord(left)-65
-    if right != '.':
-        graph[ord(node)-65][1] = ord(right)-65
+    dict[node] = [left, right]
 
 # 전위순회
-def f(idx):
-    front.append(idx)
-    if graph[idx][0] > -1:
-        f(graph[idx][0])
-    if graph[idx][1] > -1:
-        f(graph[idx][1])
+def preorder(root):
+    if root != '.':
+        print(root, end='')
+        preorder(dict[root][0])
+        preorder(dict[root][1])
 
 # 중위순회
-def m(idx):
-    if graph[idx][0] > -1:
-        m(graph[idx][0])
-    middle.append(idx)
-    if graph[idx][1] > -1:
-        m(graph[idx][1])
+def inorder(root):
+    if root != '.':
+        inorder(dict[root][0])
+        print(root, end='')
+        inorder(dict[root][1])
 
 # 후위순회
-def l(idx):
-    if graph[idx][0] > -1:
-        l(graph[idx][0])
-    if graph[idx][1] > -1:
-        l(graph[idx][1])
-    last.append(idx)
-f(0)
-m(0)
-l(0)
-for i in range(len(front)):
-    c = front[i]
-    print(chr(c+65), end='')
+def postorder(root):
+    if root != '.':
+        postorder(dict[root][0])
+        postorder(dict[root][1])
+        print(root, end='')
+
+preorder('A')
 print()
-for i in range(len(middle)):
-    c = middle[i]
-    print(chr(c+65), end='')
+inorder('A')
 print()
-for i in range(len(last)):
-    c = last[i]
-    print(chr(c+65), end='')
-print()
+postorder('A')
