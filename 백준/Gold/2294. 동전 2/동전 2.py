1e324fa4776d1f46f34d7ac70c761677a55fa17c
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-03-19 00:00:18 +0900

    [Gold V] Title: 동전 2, Time: 412 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.py
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -1,17 +1,16 @@
 n, k = map(int, input().split())
 coin = set()
 for i in range(n):
-    cost = int(input())
-    coin.add(cost)
-dp = [100001] * 10001
+    coin.add(int(input()))
+dp = [10001] * (k+1)
 for c in coin:
-    if c <= 10000:
+    if c <= k:
         dp[c] = 1
-for i in range(1, k+1):
+for i in range(k+1):
     for c in coin:
         if i-c >= 0:
             dp[i] = min(dp[i], dp[i-c]+1)
-if dp[k] != 100001:
+if dp[k] != 10001:
     print(dp[k])
 else:
     print(-1)
