3489b74a7152128f4aab513af40ec354fe71ca92
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-03-23 19:57:51 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -1,16 +1,11 @@
-from collections import deque
 N, K = map(int, input().split())
+arr = [i for i in range(1, N+1)]
 
-q = deque()
-for i in range(1, N+1):
-    q.append(str(i))
-ans = []
-print("<", end="")
-while N > 0:
-    for i in range(K-1):
-        n = q.popleft()
-        q.append(n)
-    ans.append(q.popleft())
-    N -= 1
-print(', '.join(ans), end="")
-print('>')
+answer = [] # 제거된 사람들 넣을 배열
+num = 0 # 제거될 사람의 인덱스 번호
+for i in range(N):
+    num += K-1
+    if num >= len(arr):
+        num %= len(arr)
+    answer.append(str(arr.pop(num)))
+print("<", ", ".join(answer)[:], ">", sep="")
