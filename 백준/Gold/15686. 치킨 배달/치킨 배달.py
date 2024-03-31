9d94a06d5a450b1ff7c4b461511f7d1011210f85
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-03-31 16:03:44 +0900

    [Gold V] Title: 치킨 배달, Time: 604 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -1,51 +1,27 @@
-# 전체 치킨 집 개수 중에 M개 고르기. -> 조합으로
-# 골라서 큐에 다 집어넣고 bfs로 거리 다 구하기
-# 거리 다 합한거랑 ans랑 비교해서 갱신
-from collections import deque
-from itertools import combinations
 import sys
+from itertools import combinations
+
 input = sys.stdin.readline
 N, M = map(int, input().split())
 city = [list(map(int, input().split())) for _ in range(N)]
-chicken = []
-dr = [-1, 1, 0, 0]
-dc = [0, 0, -1, 1]
 ans = 1e9
+house = [] # 집 좌표
+chick = [] # 치킨집 좌표
 
 for i in range(N):
     for j in range(N):
-        if city[i][j] == 2:
-            chicken.append((i, j))
+        if city[i][j] == 1:
+            house.append((i, j))
+        elif city[i][j] == 2:
+            chick.append((i, j))
 
-def bfs(arr, board):
-    ans = 0
-    visited = [[0] * N for _ in range(N)]
-    q = deque()
-    for a in arr:
-        q.append((a[0], a[1], a[0], a[1]))
-        visited[a[0]][a[1]] = 1
-    while q:
-        r, c, p_r, p_c = q.popleft()
-        if board[r][c] == 1:
-            ans += abs(p_r - r) + abs(p_c - c)
-        for i in range(4):
-            nr = r + dr[i]
-            nc = c + dc[i]
-            if 0 <= nr < N and 0 <= nc < N and board[nr][nc] != 2 and visited[nr][nc] == 0:
-                q.append((nr, nc, p_r, p_c))
-                visited[nr][nc] = 1
-    return ans
-
-for com in combinations(chicken, M):
-    arr = list(com)
-    board = [[0] * N for _ in range(N)]
-    for i in range(N):
-        for j in range(N):
-            board[i][j] = city[i][j]
-            if (i, j) not in arr and city[i][j] == 2:
-                board[i][j] = 0
-
-    cnt = bfs(arr, board)
-    if cnt < ans:
-        ans = cnt
+for chi in combinations(chick, M): # m개의 치킨집 선택
+    temp = 0
+    for h in house:
+        chi_len = 1e9 # 각 집에서 치킨집까지의 거리
+        for c in chi:
+            chi_len = min(chi_len, abs(c[0]-h[0])+abs(c[1]-h[1]))
+        temp += chi_len
+    if temp < ans:
+        ans = temp
 print(ans)
