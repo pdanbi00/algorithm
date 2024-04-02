b36529cc6c534b533e0ff8f26f2db57c08d197e9
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-04-02 21:28:49 +0900

    [Gold V] Title: 물병, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1052. 물병/물병.py
+++ 백준/Gold/1052. 물병/물병.py
@@ -2,6 +2,8 @@ N, K = map(int, input().split())
 cnt = 0
 
 while bin(N).count('1') > K:
-    N += 1
-    cnt += 1
+    idx = bin(N)[::-1].index('1') # 1이 오른쪽에서 몇번째에 있는지 찾기
+    cnt += 2**idx
+    N += 2 ** idx
+
 print(cnt)
