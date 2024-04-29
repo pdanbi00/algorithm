f50bdd3496ed3c4745b9dcad55ae9fafa7f6b5e3
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-04-29 23:52:28 +0900

    [Silver V] Title: 숫자 카드, Time: 1948 ms, Memory: 113332 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,27 +1,28 @@
+import sys
+input = sys.stdin.readline
 N = int(input())
 card = list(map(int, input().split()))
 card.sort()
 M = int(input())
 nums = list(map(int, input().split()))
 
-for n in nums:
+answer = []
+
+def check(num):
     l = 0
-    r = N-1
-    find = False
+    r = len(card) - 1
+
     while l <= r:
         mid = (l+r) // 2
 
-        if card[mid] > n:
-            r = mid - 1
-
-        elif card[mid] < n:
-            l = mid + 1
-
+        if card[mid] == num:
+            return 1
         else:
-            find = True
-            break
-    if find:
-        print(1, end = ' ')
-    else:
-        print(0, end = ' ')
+            if card[mid]>= num:
+                r = mid - 1
+            else:
+                l = mid + 1
+    return 0
 
+for n in nums:
+    print(check(n), end=" ")
