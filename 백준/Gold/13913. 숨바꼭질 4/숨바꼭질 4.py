9afad4b98dfa027e54b72c3e4fa11ecc04ff61c6
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-05-15 21:17:11 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 168 ms, Memory: 48656 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.py
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.py
@@ -1,37 +1,36 @@
+# bfs
+def check(now):
+    ans = []
+    # 뒤에서부터 경로 찾기
+    temp = now
+    for _ in range(visited[now]):
+        ans.append(temp)
+        temp = move[temp]
+    # 뒤집어서 출력
+    print(' '.join(map(str, ans[::-1])))
 from collections import deque
-import sys
-MAX = 200000
-sys.setrecursionlimit(MAX)
-checked = [False] * MAX
-board = [-1] * MAX
-via = [-1] * MAX
-
 N, K = map(int, input().split())
-checked[N] = True
-board[N] = 0
-
+visited = [0]*100001 # 방문 여부 표시. 걸린 시간 표시
+move = [0] * 100001 # 이전에 어디서부터 온 건지 표시
 q = deque()
 q.append(N)
+visited[N] = 1
+
 while q:
     now = q.popleft()
-    if now-1 >= 0 and not checked[now-1]:
-        q.append(now-1)
-        checked[now-1] = True
-        board[now-1]  = board[now] + 1
-        via[now-1] = now
-    if now+1 < MAX and not checked[now+1]:
-        q.append(now+1)
-        checked[now+1] = True
-        board[now+1] = board[now] + 1
-        via[now+1] = now
-    if now*2 < MAX and not checked[now*2]:
+    if now == K:
+        print(visited[now]-1)
+        check(now)
+        break
+    if now * 2 <= 100000 and visited[now * 2] == 0:
         q.append(now*2)
-        checked[now*2] = True
-        board[now*2] = board[now] + 1
-        via[now*2] = now
-print(board[K])
-def go(n, k):
-    if n != k:
-        go(n, via[k])
-    print(k, end = ' ')
-go(N, K)
+        visited[now*2] = visited[now] + 1
+        move[now*2] = now
+    if now + 1 <= 100000 and visited[now + 1] == 0:
+        q.append(now+1)
+        visited[now+1] = visited[now] + 1
+        move[now + 1] = now
+    if now - 1 >= 0 and visited[now - 1] == 0:
+        q.append(now-1)
+        visited[now-1] = visited[now] + 1
+        move[now - 1] = now
