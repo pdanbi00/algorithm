b8e06c91178466fd8dbb15b0cb610a5b9b1cf77c
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-05-17 20:40:31 +0900

    [Silver IV] Title: 수들의 합 2, Time: 220 ms, Memory: 110776 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
@@ -1,10 +1,12 @@
+# 방법 2 : 부분합
 N, M = map(int, input().split())
 nums = list(map(int, input().split()))
+for i in range(1, N):
+    nums[i] += nums[i-1]
+nums = [0] + nums
 cnt = 0
-for i in range(N):
-    sum = 0
-    for j in range(i, N):
-        sum += nums[j]
-        if sum == M:
+for i in range(N+1):
+    for j in range(i, N+1):
+        if nums[j] - nums[i] == M:
             cnt += 1
 print(cnt)
