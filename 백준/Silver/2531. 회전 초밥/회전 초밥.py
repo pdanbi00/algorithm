da5bce66e893fadcc039ea00d075ff6dfe8eaa70
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-05-21 01:20:12 +0900

    [Silver I] Title: 회전 초밥, Time: 84 ms, Memory: 32288 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.py
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.py
@@ -1,4 +1,4 @@
-# 2. 투 포인터
+# 3. 슬라이딩 윈도우
 import sys
 input = sys.stdin.readline
 
@@ -6,17 +6,39 @@ N, d, k, c = map(int, input().split())
 sushi = []
 for _ in range(N):
     sushi.append(int(input()))
-    
-left, right = 0, k
+left = 0
+right = 0
+
+dict = {}
 answer = 0
-while left < N:
-    s = set()
-    for i in range(left, right):
-        s.add(sushi[i % N])
-    if c not in s:
-        s.add(c)
+# k만큼 먹기
+while right < k:
+    if sushi[right] not in dict.keys():
+        dict[sushi[right]] = 1
+    else:
+        dict[sushi[right]] += 1
+    right += 1
+
+if c not in dict.keys():
+    dict[c] = 1
+else:
+    dict[c] += 1
     
-    answer = max(answer, len(s))
+# 슬라이딩 윈도우
+while left < N:
+    answer = max(answer, len(dict))
+    # 1. 제일 왼쪽 초밥 제거
+    dict[sushi[left]] -= 1
+    # 삭제된 왼쪽 초밥이 0이면 dict에서 삭제
+    if dict[sushi[left]] == 0:
+        del dict[sushi[left]]
+    # 위에서 k개 만큼 먹고 k가 1 증가한 상태라서 바로 right % N 하면 됨.
+    if sushi[right % N] not in dict.keys():
+        dict[sushi[right % N]] = 1
+    else:
+        dict[sushi[right % N]] += 1
+    
     left += 1
     right += 1
-print(answer)
+# 처음에 k개 넣고 쿠폰에 해당하는 초밥도 추가 해서 더 만질거 없음.    
+print(answer)  
