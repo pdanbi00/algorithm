19ac3754a8196e8c7ed568f88e3a6882a954d295
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-05-21 01:09:41 +0900

    [Silver I] Title: 회전 초밥, Time: 2280 ms, Memory: 145944 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.py
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.py
@@ -1,18 +1,22 @@
-# 브루트포스
-# 그냥 초밥 k개씩 잘라서 종류 개수 세고, 쿠폰에 있는 초밥은 그냥 무조건 공짜로 먹을 수 있으니깐 그거 추가해서 종류 개수 세기
+# 2. 투 포인터
 import sys
 input = sys.stdin.readline
 
-sushi = []
 N, d, k, c = map(int, input().split())
-for i in range(N):
-    n = int(input())
-    sushi.append(n)
-max_type = 0
-for i in range(N):
-    if i + k > N:
-        number_type = len(set(sushi[i:N] + sushi[:(i+k)%N] + [c]))
-    else:
-        number_type = len(set(sushi[i:(i+k)] + [c]))
-    max_type = max(max_type, number_type)
-print(max_type)
+sushi = []
+for _ in range(N):
+    sushi.append(int(input()))
+    
+left, right = 0, k
+answer = 0
+while left < N:
+    s = set()
+    for i in range(left, right):
+        s.add(sushi[i % N])
+    if c not in s:
+        s.add(c)
+    
+    answer = max(answer, len(s))
+    left += 1
+    right += 1
+print(answer)
