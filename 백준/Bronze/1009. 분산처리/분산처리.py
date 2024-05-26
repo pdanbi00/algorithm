5ec790cb448f1fb3286468a3e0f8a7797ae8eee5
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-05-27 00:11:03 +0900

    [Bronze II] Title: 분산처리, Time: 80 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1009. 분산처리/분산처리.py
@@ -0,0 +1,18 @@
+T = int(input())
+for _ in range(T):
+    a, b = map(int, input().split())
+    base = a % 10
+    if base == 0: # 1의 자리가 0인 경우(10의 배수)
+        print(10)
+    elif base == 1 or base == 5 or base == 6:
+        print(base)
+    elif base == 4 or base == 9:
+        if b % 2 == 0:
+            print((base**2) % 10)
+        else:
+            print(base)
+    else:
+        if b % 4 == 0:
+            print(base**4 % 10)
+        else:
+            print(base**(b%4) % 10)
