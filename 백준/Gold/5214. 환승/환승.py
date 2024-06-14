ca42115e1ec538d1a5341b0afe26e4da64499397
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-06-15 01:15:41 +0900

    [Gold II] Title: 환승, Time: 1136 ms, Memory: 124456 KB -BaekjoonHub

--- 백준/Gold/5214. 환승/환승.py
+++ 백준/Gold/5214. 환승/환승.py
@@ -8,6 +8,26 @@ input = sys.stdin.readline
 
 from collections import deque
 
+def bfs():
+    q = deque()
+    q.append(1)
+    visited = [-1] * (N+M+1)
+    visited[1] = 1
+    while q:
+        now = q.popleft()
+        if now == N:
+            print(visited[now])
+            return
+        for next in graph[now]:
+            if visited[next] == -1:
+                if next > N: # 더미 스테이션일 경우 횟수 추가 안함.
+                    q.append(next)
+                    visited[next] = visited[now]
+                else: # 일반 역일 경우 횟수 추가
+                    q.append(next)
+                    visited[next] = visited[now] + 1
+    print(-1)
+
 N, K, M = map(int, input().split())
 graph = [[] for _ in range(N+M+1)] # 역 개수 N+1개 + 튜브개수 M개
 
@@ -16,19 +36,4 @@ for i in range(1, M+1):
     for j in range(K):
         graph[tube[j]].append(N+i)
         graph[N+i].append(tube[j])
-
-q = deque()
-q.append(1)
-visited = [-1] * (N+M+1)
-visited[1] = 1
-while q:
-    now = q.popleft()
-    for next in graph[now]:
-        if visited[next] == -1:
-            if next > N: # 더미 스테이션일 경우 횟수 추가 안함.
-                q.append(next)
-                visited[next] = visited[now]
-            else: # 일반 역일 경우 횟수 추가
-                q.append(next)
-                visited[next] = visited[now] + 1
-print(visited[N])
+bfs()
