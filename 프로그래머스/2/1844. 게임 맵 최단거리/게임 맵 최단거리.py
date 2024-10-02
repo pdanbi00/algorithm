5cce87cb26de87f8ee011d945054e02b4e32bb19
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-10-02 11:05:23 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 11.54 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,29 +1,28 @@
+# 최대한 빨리? bfs
 from collections import deque
+
 def solution(maps):
-    answer = 0
-    n = len(maps) # 세로
-    m = len(maps[0]) # 가로
+    answer = -1
+    R = len(maps)
+    C = len(maps[0])
+    
     dr = [-1, 1, 0, 0]
     dc = [0, 0, -1, 1]
     
-    find = False
+    visited = [[0] * C for _ in range(R)]
     q = deque()
-    q.append((0, 0, 1))
-    visited = [[False] * m for _ in range(n)]
-    visited[0][0] = True
-    cnt = 0
+    q.append((0, 0))
+    visited[0][0] = 1
     while q:
-        r, c, cnt = q.popleft()
-        if r == n-1 and c == m-1:
-            answer = cnt
-            find = True
+        r, c = q.popleft()
+        if r == R-1 and c == C-1:
+            answer = visited[r][c]
+            break
         for k in range(4):
             nr = r + dr[k]
             nc = c + dc[k]
-            if 0 <= nr < n and 0 <= nc < m:
-                if visited[nr][nc] == False and maps[nr][nc] == 1:
-                    q.append((nr, nc, cnt+1))
-                    visited[nr][nc] = True
-    if not find:
-        answer = -1
+            if 0 <= nr < R and 0 <= nc < C:
+                if visited[nr][nc] == 0 and maps[nr][nc] == 1:
+                    q.append((nr, nc))
+                    visited[nr][nc] = visited[r][c] + 1
     return answer
