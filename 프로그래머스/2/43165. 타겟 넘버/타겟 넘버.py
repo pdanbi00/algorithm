be6b653ed41d95d34ce7384c1ea0d8c0df84fd13
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-10-02 10:51:17 +0900

    [level 2] Title: 타겟 넘버, Time: 351.55 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,13 +1,15 @@
+def dfs(idx, total, numbers, target):
+    global answer
+    if idx == len(numbers):
+        if total == target:
+            answer += 1
+        return
+    dfs(idx+1, total + numbers[idx], numbers, target)
+    dfs(idx+1, total - numbers[idx], numbers, target)
+    
 answer = 0
 def solution(numbers, target):
-    def dfs(idx, num):
-        global answer
-        if idx == len(numbers):
-            if num == target:
-                answer += 1
-            return
-        dfs(idx+1, num+numbers[idx])
-        dfs(idx+1, num-numbers[idx])
-        return
-    dfs(0, 0)
+    
+    visited = [0] * len(numbers)
+    dfs(0, 0, numbers, target)
     return answer
