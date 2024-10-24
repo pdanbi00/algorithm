841e06d798b7c34ec51f28424b3521f6a682e034
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-10-24 15:04:43 +0900

    [Gold II] Title: 동전 분배, Time: 480 ms, Memory: 19352 KB -BaekjoonHub

--- 백준/Gold/1943. 동전 분배/동전 분배.java
+++ 백준/Gold/1943. 동전 분배/동전 분배.java
@@ -1,45 +1,61 @@
 import java.util.*;
 import java.io.*;
 
+class Coin {
+    int value, quantity;
+    Coin(int value, int quantity) {
+        this.value = value;
+        this.quantity = quantity;
+    }
+}
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
         for (int t = 0; t < 3; t++) {
-            boolean[] dp = new boolean[100001];
-
-            ArrayDeque<int[]> coins = new ArrayDeque<>();
-            dp[0] = true;
+//            boolean[] dp = new boolean[100001];
+//            dp[0] = true;
             int sum = 0;
             int N = Integer.parseInt(br.readLine());
+            Coin[] coins = new Coin[N+1];
             for (int i = 0; i < N; i++) {
                 StringTokenizer st = new StringTokenizer(br.readLine(), " ");
                 int coin = Integer.parseInt(st.nextToken());
                 int cnt = Integer.parseInt(st.nextToken());
+                coins[i+1] = new Coin(coin, cnt);
                 sum += coin * cnt;
-                for (int k = 1; k <= cnt; k++) {
-                    dp[k * coin] = true;
-                }
-                coins.offer(new int[]{coin, cnt, sum});
+//                for (int k = 1; k <= cnt; k++) {
+//                    dp[k * coin] = true;
+//                }
             }
-            if (sum % 2 == 1) sb.append(0).append("\n");
-            else if (dp[sum/2]) sb.append(1).append("\n");
+            if (sum % 2 == 1) System.out.println(0);
             else {
-                while (!coins.isEmpty()) {
-                    int[] now = coins.poll();
-                    if (now[2] > 50000) now[2] = 50000;
-                    for (int p = 0; p < now[1]; p++) {
-                        for (int j = now[2]; j > now[0]; j--) {
-                            if ((j - now[0]) % now[0] == 0 && (j - now[0]) <= now[0] * now[1]) continue;
-                            dp[j] |= dp[j - now[0]];
+                boolean[] dp = new boolean[100001];
+                dp[0] = true;
+                for (int i = 1; i <= N; i++) {
+                    int coin = coins[i].value;
+                    int cnt = coins[i].quantity;
+
+                    for (int j = sum/2; j >= coin; j--) {
+                        if (dp[j - coin]) {
+                            for (int p = 0; p < cnt; p++) {
+                                if (j + coin * p <= sum/2){
+                                    dp[j + coin * p] = true;
+                                }
+                                else {
+                                    break;
+                                }
+                            }
                         }
                     }
                 }
-                if (dp[sum / 2]) sb.append(1).append("\n");
-                else sb.append(0).append("\n");
+                if (dp[sum/2]) {
+                    System.out.println(1);
+                } else {
+                    System.out.println(0);
                 }
+            }
         }
-        System.out.println(sb);
     }
 }
