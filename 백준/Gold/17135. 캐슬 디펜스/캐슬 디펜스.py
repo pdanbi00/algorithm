e7fc0d786f657221b4ad7b2a8067001d7bedb210
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-10-26 17:52:16 +0900

    [Gold III] Title: 캐슬 디펜스, Time: 180 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.py
+++ 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.py
@@ -1,81 +1,52 @@
-# board = M열, N+1행
-# 궁수는 3명. N+1 행에 위치
-# 적은 거리가 D 이하인 적 중에 가장 가까운 적, 거리가 같으면 가장 왼쪽에 있는 적
-# 공격받으면 적은 제거됨. 공격 이후 적은 한칸씩 아래로 이동#
-# 이동해서 N+1로 오게 되면 제거됨
+from collections import deque
 from copy import deepcopy
 
 N, M, D = map(int, input().split())
-board = []
-juck = 0 # 적의 수. while 문 break 조건이 될거임.
-for _ in range(N):
-    arr = list(map(int, input().split()))
-    for j in range(M):
-        if arr[j] == 1:
-            juck += 1
-    board.append(arr)
 
-def attack(i, j, k, new_board):
-    global cnt
-    global tong
-    while cnt + tong < juck:
-        gung1 = []
-        gung2 = []
-        gung3 = []
-        # 공격 대상 찾기
-        for r in range(N):
-            for c in range(M):
+board = [list(map(int, input().split())) for _ in range(N)]
+
+# 우선순위를 위해서 왼쪽, 위쪽, 오른쪽
+dr = [0, -1, 0]
+dc = [-1, 0, 1]
+
+def kill(gungs):
+    new_board = deepcopy(board)
+    visited = [[0] * M for _ in range(N)]
+
+    cnt = 0
+    for i in range(N-1, -1, -1):
+        this_turn = [] # 이번 턴에 죽는 애들
+        # 궁수별로 bfs 돌리면서 사정거리 안에서 가장 가까운 적군 찾기
+        for gung in gungs:
+            # 첫번째 값은 궁수 바로 위
+            q = deque()
+            q.append((1, i, gung))
+            while q:
+                distance, r, c = q.popleft()
+                # 그 칸에 적군이 있으면
                 if new_board[r][c] == 1:
-                    dis1 = abs(r - N) + abs(c - i)
-                    dis2 = abs(r - N) + abs(c - j)
-                    dis3 = abs(r - N) + abs(c - k)
-                    if dis1 <= D:
-                        gung1.append((dis1, r, c))
-                    if dis2 <= D:
-                        gung2.append((dis2, r, c))
-                    if dis3 <= D:
-                        gung3.append((dis3, r, c))
-        if len(gung1) >= 1:
-            gung1.sort(key=lambda x : (x[0], x[2]))
-            # 공격
-            new_board[gung1[0][1]][gung1[0][2]] = 0
-            cnt += 1
-        if len(gung2) >= 1:
-            gung2.sort(key=lambda x: (x[0], x[2]))
-            if new_board[gung2[0][1]][gung2[0][2]] == 1:
-                # 공격
-                new_board[gung2[0][1]][gung2[0][2]] = 0
-                cnt += 1
-        if len(gung3) >= 1:
-            gung3.sort(key=lambda x: (x[0], x[2]))
-            if new_board[gung3[0][1]][gung3[0][2]] == 1:
-                # 공격
-                new_board[gung3[0][1]][gung3[0][2]] = 0
-                cnt += 1
+                    this_turn.append((r, c))
+                    if visited[r][c] == 0:
+                        visited[r][c] = 1
+                        cnt += 1
+                    break
 
-        # 공격 이후 적들은 아래로 이동
-        # N-1행에 남아있는 적들은 제거시키기
-        for c in range(M):
-            if new_board[N-1][c] == 1:
-                tong += 1
-                new_board[N-1][c] = 0
-
-        # 나머지 적들은 아래로 한칸씩 이동
-        for r in range(N-2, -1, -1):
-            for c in range(M):
-                new_board[r+1][c] = new_board[r][c]
-        # 0 행은 0으로 다 채우기
-        for c in range(M):
-            new_board[0][c] = 0
+                if distance < D:
+                    for p in range(3):
+                        nr = r + dr[p]
+                        nc = c + dc[p]
+                        if 0 <= nr < N and 0 <= nc < M:
+                            q.append((distance+1, nr, nc))
+        # 공격할 애들 동시에 죽이기
+        for r, c in this_turn:
+            new_board[r][c] = 0
     return cnt
 
 ans = -1
-for i in range(M-2): # 궁수 1 위치
-    for j in range(i+1, M-1): # 궁수 2 위치
-        for k in range(j+1, M): # 궁수 3 위치
-            new_board = deepcopy(board)
-            cnt = 0 # 공격 당해서 제거된 적의 수
-            tong = 0 # 성에 도착해서 제거된 적의 수
-            tmp = attack(i, j, k, new_board)
+for i in range(M-2):
+    for j in range(i+1, M-1):
+        for k in range(j+1, M):
+            gungs = [i, j, k]
+            tmp = kill(gungs)
             ans = max(ans, tmp)
 print(ans)
