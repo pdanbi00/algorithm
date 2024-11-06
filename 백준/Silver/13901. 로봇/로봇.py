8a89d3a6bd71f42347066b226e1bc801af46bdf5
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-11-06 14:45:57 +0900

    [Silver I] Title: 로봇, Time: 80 ms, Memory: 34432 KB -BaekjoonHub

--- 백준/Silver/13901. 로봇/로봇.py
+++ 백준/Silver/13901. 로봇/로봇.py
@@ -1,9 +1,9 @@
 R, C = map(int, input().split())
 k = int(input())
-obstacle = []
+board = [[0] * C for _ in range(R)]
 for _ in range(k):
     br, bc = map(int, input().split())
-    obstacle.append((br, bc))
+    board[br][bc] = 1
 rr, rc = map(int, input().split()) # 로봇 위치
 directions = list(map(int, input().split()))
 
@@ -11,33 +11,19 @@ dr = [-1, 1, 0, 0]
 dc = [0, 0, -1, 1]
 
 dir = 0
-visited = [[0] * C for _ in range(R)]
-visited[rr][rc] = 1
-board = [[0] * C for _ in range(R)]
-for i in range(k):
-    br, bc = obstacle[i][0], obstacle[i][1]
-    board[br][bc] = 'X'
+board[rr][rc] = 1
 
 while True:
     possible = False
     for k in range(4):
         n_dir = (dir + k) % 4
-        if directions[n_dir] == 1: # 상
-            nr = rr + dr[0]
-            nc = rc + dc[0]
-        elif directions[n_dir] == 2: # 하
-            nr = rr + dr[1]
-            nc = rc + dc[1]
-        elif directions[n_dir] == 3: # 좌
-            nr = rr + dr[2]
-            nc = rc + dc[2]
-        elif directions[n_dir] == 4: # 우
-            nr = rr + dr[3]
-            nc = rc + dc[3]
+        d = directions[n_dir] - 1
+        nr = rr + dr[d]
+        nc = rc + dc[d]
 
         if 0 <= nr < R and 0 <= nc < C:
-            if visited[nr][nc] == 0 and board[nr][nc] != 'X':
-                visited[nr][nc] = 1
+            if board[nr][nc] == 0:
+                board[nr][nc] = 1
                 rr = nr
                 rc = nc
                 dir = n_dir
