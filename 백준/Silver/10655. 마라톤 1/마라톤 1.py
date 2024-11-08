2c76e5f23b09136cd8aa43466ee8a43d20422fa5
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-11-09 00:18:35 +0900

    [Silver III] Title: 마라톤 1, Time: 252 ms, Memory: 117580 KB -BaekjoonHub

--- 백준/Silver/10655. 마라톤 1/마라톤 1.py
+++ 백준/Silver/10655. 마라톤 1/마라톤 1.py
@@ -1,16 +1,18 @@
 N = int(input())
-checkpoint = [list(map(int, input().split())) for _ in range(N)]
+total = 0
+p_r, p_c = map(int, input().split())
+arr = [(p_r, p_c)]
+for _ in range(N-1):
+    r, c = map(int, input().split())
+    total += abs(p_r - r) + abs(p_c - c)
+    arr.append((r, c))
+    p_r, p_c = r, c
+ans = 1e9
 
-distance = [0]
-for i in range(N-1):
-    dist = abs(checkpoint[i+1][0] - checkpoint[i][0]) + abs(checkpoint[i+1][1] - checkpoint[i][1])
-    distance.append(dist)
-    
-totalDist = sum(distance)
-
-dist = 0
-min_dist = 1e9
 for i in range(1, N-1):
-    dist = totalDist - (distance[i] + distance[i+1]) + abs(checkpoint[i+1][0] - checkpoint[i-1][0]) + abs(checkpoint[i+1][1] - checkpoint[i-1][1])
-    min_dist = min(min_dist, dist)
-print(min_dist)
+    tmp = total
+    tmp -= (abs(arr[i][0] - arr[i-1][0]) + abs(arr[i][1] - arr[i-1][1]))
+    tmp -= (abs(arr[i][0] - arr[i + 1][0]) + abs(arr[i][1] - arr[i + 1][1]))
+    tmp += (abs(arr[i+1][0] - arr[i-1][0]) + abs(arr[i+1][1] - arr[i-1][1]))
+    ans = min(ans, tmp)
+print(ans)
