4b85de35470b044fdbe4b8498cb8152e83f39c9e
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-11-14 16:10:40 +0900

    [Gold V] Title: 마법사 상어와 비바라기, Time: 176 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.py
+++ 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.py
@@ -1,42 +1,54 @@
+# 시간 초과
+# from collections import deque
 import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(N)]
-directions = [list(map(int, input().split())) for _ in range(M)]
 
 dr = [0, -1, -1, -1, 0, 1, 1, 1]
 dc = [-1, -1, 0, 1, 1, 1, 0, -1]
-clouds = [(N-2, 0), (N-2, 1), (N-1, 0), (N-1, 1)]
-for direction in directions:
-    d, m = direction[0] - 1, direction[1] % N
-    not_cloud = set()
-    while clouds:
-        r, c = clouds.pop()
-        nr = (r + (dr[d] * m)) % N
-        nc = (c + (dc[d] * m)) % N
-        board[nr][nc] += 1
-        # 구름 있던 자리는 저장했다가 나중에 제외시키기
-        not_cloud.add((nr, nc))
 
-    # 물 뿌리기
-    for nr, nc in not_cloud:
-        count = 0
+dae_r = [-1, -1, 1, 1] # 단계 4에서 사용할 대각선들 방향
+dae_c = [-1, 1, -1, 1] # 단계 4에서 사용할 대각선들 방향
+
+clouds = [(N-2, 0), (N-2, 1), (N-1, 0), (N-1, 1)]
+
+
+answer = 0
+
+def rain(direction, distance):
+    new_clouds = set()
+    while clouds:
+        # 구름 이동시키기
+        r, c = clouds.pop()
+        nr = (r + (dr[direction] * distance)) % N
+        nc = (c + (dc[direction] * distance)) % N
+
+        board[nr][nc] += 1
+        new_clouds.add((nr, nc))
+
+    # 단계 4
+    for nr, nc in new_clouds:
+        tmp = 0
         for k in [1, 3, 5, 7]:
             nnr = nr + dr[k]
             nnc = nc + dc[k]
             if 0 <= nnr < N and 0 <= nnc < N:
                 if board[nnr][nnc] > 0:
-                    count += 1
-        board[nr][nc] += count
+                    tmp += 1
+        board[nr][nc] += tmp
 
-    # 새로운 구름 만들기
     for i in range(N):
         for j in range(N):
-            if (i, j) not in not_cloud:
-                if board[i][j] >= 2:
-                    clouds.append((i, j))
-                    board[i][j] -= 2
+            if board[i][j] >= 2 and (i, j) not in new_clouds:
+                clouds.append((i, j))
+                board[i][j] -= 2
+
+for _ in range(M):
+    direction, distance = map(int, input().split())
+    rain(direction-1, distance % N)
+
 answer = 0
 for i in range(N):
     for j in range(N):
