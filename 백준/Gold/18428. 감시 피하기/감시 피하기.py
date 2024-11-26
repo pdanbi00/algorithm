f14a1b511a0a86674671092c60083b7a0a9011c9
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-11-26 23:44:43 +0900

    [Gold V] Title: 감시 피하기, Time: 60 ms, Memory: 34068 KB -BaekjoonHub

---
+++ 백준/Gold/18428. 감시 피하기/감시 피하기.py
@@ -0,0 +1,49 @@
+from collections import deque
+from itertools import combinations
+
+N = int(input())
+board = []
+empty = []
+teacher = []
+
+dr = [-1, 1, 0, 0]
+dc = [0, 0, -1, 1]
+
+for i in range(N):
+    arr = list(input().split())
+    for j in range(N):
+        if arr[j] == 'X':
+            empty.append((i, j))
+        elif arr[j] == 'T':
+            teacher.append((i, j))
+    board.append(arr)
+
+def check():
+    for t_r, t_c in teacher:
+        for k in range(4):
+            nr = t_r
+            nc = t_c
+            while True:
+                nr += dr[k]
+                nc += dc[k]
+                if 0 <= nr < N and 0 <= nc < N:
+                    if board[nr][nc] == 'S':
+                        return False
+                    elif board[nr][nc] == 'O':
+                        break
+                else:
+                    break
+    return True
+possible = False
+for com in combinations(empty, 3):
+    for c in com:
+        board[c[0]][c[1]] = 'O'
+    if check():
+        possible = True
+        break
+    for c in com:
+        board[c[0]][c[1]] = 'X'
+if possible:
+    print("YES")
+else:
+    print("NO")
