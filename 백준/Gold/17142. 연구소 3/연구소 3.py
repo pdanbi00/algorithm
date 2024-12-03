7f915a9f5437a3f075f66f3424b1a9d3f1bae123
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-12-03 16:44:19 +0900

    [Gold III] Title: 연구소 3, Time: 544 ms, Memory: 34136 KB -BaekjoonHub

--- 백준/Gold/17142. 연구소 3/연구소 3.py
+++ 백준/Gold/17142. 연구소 3/연구소 3.py
@@ -5,28 +5,17 @@ from itertools import combinations
 from collections import deque
 
 N, M = map(int, input().split())
-board = [list(map(int, input().split())) for _ in range(N)]
+board = []
+virus = []
+
+for i in range(N):
+    arr = list(map(int, input().split()))
+    for j in range(N):
+        if arr[j] == 2:
+            virus.append((i, j))
+    board.append(arr)
 
 ans = 1000000000000
-
-
-# 벽은 '-', 바이러스 위치는 '*'(비활성화), 나머지는 -1로 초기화
-# 바이러스 위치가 담긴 큐를 return
-def init():
-    virus = deque()
-    for i in range(N):
-        for j in range(N):
-            # 벽일 경우
-            if board[i][j] == 1:
-                board[i][j] = '-'
-            elif board[i][j] == 2:
-                board[i][j] = '*'
-                virus.append((i, j))
-            else:
-                board[i][j] = -1
-    return virus
-
-viruses = init()
 
 def bfs(vir):
     global ans
@@ -50,28 +39,25 @@ def bfs(vir):
             nr = r + dr[k]
             nc = c + dc[k]
             if 0 <= nr < N and 0 <= nc < N:
-                if board[nr][nc] != '-' and visited[nr][nc] == -1:
-                    if board[nr][nc] == -1:
+                if board[nr][nc] != 1 and visited[nr][nc] == -1:
+                    if board[nr][nc] == 0:
                         visited[nr][nc] = visited[r][c] + 1
                         time = max(time, visited[nr][nc])
                     # 비활성화 상태의 바이러스를 만나면 통과해야 하기 때문에 시간은 1초 늘리지만 갱신은 안함
-                    elif board[nr][nc] == '*':
+                    elif board[nr][nc] == 2:
                         visited[nr][nc] = visited[r][c] + 1
                     q.append((nr, nc))
 
     for i in range(N):
         for j in range(N):
-            if board[i][j] == -1 and visited[i][j] == -1:
+            if board[i][j] == 0 and visited[i][j] == -1:
                 time = 1000000000000
                 return time
     return time
 
-for combi in combinations(viruses, M):
-    # print(combi)
+for combi in combinations(virus, M):
     ans = min(bfs(combi), ans)
-    # for i in range(N):
-    #     print(board[i])
-    # print('---------')
+
 if ans == 1000000000000:
     print(-1)
 else:
