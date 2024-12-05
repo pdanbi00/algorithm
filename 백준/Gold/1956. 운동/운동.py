5342738fb58d2920777c0649bbd8cf4f4c89d2e4
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-12-05 09:41:56 +0900

    [Gold IV] Title: 운동, Time: 768 ms, Memory: 112736 KB -BaekjoonHub

---
+++ 백준/Gold/1956. 운동/운동.py
@@ -0,0 +1,30 @@
+# 플로이드 워샬 : 경유지 기준으로 경유지를 들렀을 경우와 아닌 경우를 비교하면서 최소 비용 루트를 찾는 방법
+# 자기 자신에서 자기 자신으로 돌아오는 거리 중에 최솟값 구하기
+
+# A에서 B로 가는 최단거리랑, A에서 k를 거쳐서 B로 가는 거리를 비교하는 방식
+
+V, E = map(int, input().split())
+# 거리를 담는 그래프
+graph = [[int(1e9)] * (V+1) for _ in range(V+1)]
+for _ in range(E):
+    v1, v2, dis = map(int, input().split())
+    graph[v1][v2] = dis
+
+# 1. 모든 정덤에서 모든 정점으로 가는 최소 거리 구하기기
+# 경유지 k, 출발지 i, 도착지 j로 3중 for문 돌기
+for k in range(1, V+1):
+    for i in range(1, V+1):
+        for j in range(1, V+1):
+            # i -> j가 빠른지 i -> k -> j가 빠른지 검사
+            if graph[i][k] + graph[k][j] < graph[i][j]:
+                graph[i][j] = graph[i][k] + graph[k][j]
+
+# 2. 자기자신에서 자기자신으로 돌아오는 거리 중 최소 값 구하기
+ans = 1e9
+for i in range(1, V+1):
+    ans = min(ans, graph[i][i])
+
+if ans == 1e9:
+    print(-1)
+else:
+    print(ans)
