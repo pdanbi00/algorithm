aef8569adeb21dfe1f882e820d8e4e9534e13e90
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-12-09 23:52:16 +0900

    [Gold III] Title: BFS 스페셜 저지, Time: 3072 ms, Memory: 88980 KB -BaekjoonHub

---
+++ 백준/Gold/16940. BFS 스페셜 저지/BFS 스페셜 저지.py
@@ -0,0 +1,40 @@
+from collections import deque
+N = int(input())
+
+graph = [[] for _ in range(N+1)]
+visited = [-1] * (N+1)
+children = [set() for _ in range(N+1)] # 트리의 부모자식 관계를 알기 위ㅎ해서
+for _ in range(N-1):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+path = list(map(int, input().split()))
+
+# BFS
+q = deque()
+q.append(1)
+visited[1] = 1
+while q:
+    now = q.popleft()
+    for node in graph[now]:
+        if visited[node] == -1:
+            q.append(node)
+            visited[node] = visited[now] + 1
+            children[now].add(node)
+
+next_idx = 1
+for i in path:
+    # print(i)
+    if next_idx == N:
+        break
+    length = len(children[i]) # 자식 길이
+    # print('children : ', children[i])
+    c1 = set(path[next_idx:next_idx + length])
+    # print('c1 : ', c1)
+    c2 = children[i]
+    # print('c2 : ', c2)
+    if c1 != c2:
+        print(0)
+        exit()
+    next_idx += length
+print(1)
