fa355d4a639a6df769436c92bf6051ab36800d66
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-12-13 10:44:53 +0900

    [Gold V] Title: 강의실 배정, Time: 332 ms, Memory: 74744 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.py
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -12,6 +12,7 @@ heappush(room, lecture[0][1]) # 첫번째 강의 끝나는 시간
 
 # 지금 사용중인 강의실 중에서도 제일 먼저 시작한 강의의 종료시간이랑 비교하면서 진행해야하기 때문에 우선순위 큐 사용
 answer = 0
+# print(room)
 for i in range(1, N):
     # 강의 시작시간이 현재 강의나는 시간 보다 이르면
     if lecture[i][0] < room[0]:
@@ -19,5 +20,18 @@ for i in range(1, N):
     else:
         heappop(room) # 기존 강의 종료.
         heappush(room, lecture[i][1])
-    answer = max(answer, len(room))
-print(answer)
+    # print(room)
+    # answer = max(answer, len(room))
+# print(answer)
+print(len(room)) # 하나가 빠지고 하나가 추가되는거라서 늘어난 길이가 계속 유지됨.
+
+'''
+7
+1 5
+2 4
+3 6
+4 5
+5 6
+6 7
+7 8
+'''
