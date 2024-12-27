c533beffe67ddfb27ee20b662715e2030132c31b
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-12-27 16:49:19 +0900

    [Gold III] Title: 같이 눈사람 만들래?, Time: 3332 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/20366. 같이 눈사람 만들래？/같이 눈사람 만들래？.py
+++ 백준/Gold/20366. 같이 눈사람 만들래？/같이 눈사람 만들래？.py
@@ -6,24 +6,33 @@ snow = list(map(int, input().split()))
 snow.sort()
 
 answer = 1e9
-for i in range(N):
+for i in range(N-3):
     for j in range(i+3, N):
+        anna = snow[i] + snow[j]
+
         left = i + 1
         right = j - 1
         while left < right:
-            tmp = (snow[i] + snow[j]) - (snow[left] + snow[right])
-            if answer > abs(tmp):
-                answer = abs(tmp)
-            # 정렬 된 상태이기 때문에 tmp < 0 인 경우는
-            # snow[left] + snow[right] > snow[i] + snow[j]
-            # tmp를 늘려야하기 때문에
-            if tmp < 0:
-                right -= 1
-            # tmp > 0인 경우는
-            # snow[left] + snow[right] < snow[i] + snow[j]
-            # tmp를 줄여야하기 때문에
-            # (right + 1을 하지 않는 이유는 그러면 right의 범위가 j를 넘어갈 수 있어서)
-            else:
+            elsa = snow[left] + snow[right]
+            diff = abs(anna - elsa)
+            answer = min(answer, diff)
+            if answer == 0:
+                print(0)
+                exit()
+            if anna > elsa:
                 left += 1
+            else:
+                right -= 1
+            # # 정렬 된 상태이기 때문에 tmp < 0 인 경우는
+            # # snow[left] + snow[right] > snow[i] + snow[j]
+            # # tmp를 늘려야하기 때문에
+            # if tmp < 0:
+            #     right -= 1
+            # # tmp > 0인 경우는
+            # # snow[left] + snow[right] < snow[i] + snow[j]
+            # # tmp를 줄여야하기 때문에
+            # # (right + 1을 하지 않는 이유는 그러면 right의 범위가 j를 넘어갈 수 있어서)
+            # else:
+            #     left += 1
 
 print(answer)
