695c96c973fb12481db9b6baeb84d5aed2c76033
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2024-12-31 18:03:41 +0900

    [Gold IV] Title: 두 동전, Time: 744 ms, Memory: 124120 KB -BaekjoonHub

--- 백준/Gold/16197. 두 동전/두 동전.py
+++ 백준/Gold/16197. 두 동전/두 동전.py
@@ -1,54 +1,41 @@
-# 다음 칸이 벽이면 이동 안함
-# 다음 칸이 범위 밖이면 동전은 바닥으로 떨어짐.(board에서 없애면 됨)
-# 이동하는 칸에 동전 있어도 감.
-
-# 두 동전 중 하나만 보드에서 떨어뜨리기 위해서는 버튼 최소 몇 번 눌러야하는지 구해야 됨.
+# BFS
+from collections import deque
 
 N, M = map(int, input().split())
 board = [list(input()) for _ in range(N)]
+coins = []
 
 dr = [-1, 1, 0, 0]
 dc = [0, 0, -1, 1]
 
-coins = []
-ans = [1e9, N, M] # 이동한 횟수, 행, 열
-
-def move(coins, idx): # 동전들 위치, 몇번째 이동인지,
-    global ans
-    if idx == 10:
-        if len(coins) == 1:
-            if idx < ans[0]:
-                ans = [idx, coins[0][0], coins[0][1]]
-        return
-    elif idx < 10:
-        if len(coins) == 1:
-            if idx < ans[0]:
-                ans = [idx, coins[0][0], coins[0][1]]
-            return
+def bfs():
+    while q:
+        r1, c1, r2, c2, cnt = q.popleft()
+        if cnt >= 10:
+            return -1
         for k in range(4):
-            tmp = []
-            for coin in coins:
-                nr = coin[0] + dr[k]
-                nc = coin[1] + dc[k]
-                if 0 <= nr < N and 0 <= nc < M:
-                    if board[nr][nc] != '#':
-                        tmp.append((nr, nc))
-                    else:
-                        tmp.append((coin[0], coin[1]))
-            if len(tmp) >= 1:
-                move(tmp, idx+1)
-        return
-
-
+            nr1 = r1 + dr[k]
+            nc1 = c1 + dc[k]
+            nr2 = r2 + dr[k]
+            nc2 = c2 + dc[k]
+            if 0 <= nr1 < N and 0 <= nc1 < M and 0 <= nr2 < N and 0 <= nc2 < M:
+                if board[nr1][nc1] == '#':
+                    nr1, nc1 = r1, c1
+                if board[nr2][nc2] == '#':
+                    nr2, nc2 = r2, c2
+                q.append((nr1, nc1, nr2, nc2, cnt+1))
+            elif not (0 <= nr1 < N and 0 <= nc1 < M) and not (0 <= nr2 < N and 0 <= nc2 < M):
+                continue
+            else: # 하나만 범위 안에 있는 경우
+                return cnt + 1
+    return -1
 
 for i in range(N):
     for j in range(M):
         if board[i][j] == 'o':
-            coins.append((i, j))
+            coins.append(i)
+            coins.append(j)
+q = deque()
+q.append([*coins, 0])
 
-move(coins, 0)
-
-if ans[0] == 1e9:
-    print(-1)
-else:
-    print(ans[0])
+print(bfs())
