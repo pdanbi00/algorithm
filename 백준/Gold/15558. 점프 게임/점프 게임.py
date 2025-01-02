f3882e9bba8bd71d9569647dc67fb6c08de4c5b1
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-01-02 23:57:01 +0900

    [Gold V] Title: 점프 게임, Time: 328 ms, Memory: 43828 KB -BaekjoonHub

--- 백준/Gold/15558. 점프 게임/점프 게임.py
+++ 백준/Gold/15558. 점프 게임/점프 게임.py
@@ -7,18 +7,19 @@ visited = [[-1] * (N+K) for _ in range(2)]
 q = deque()
 l_r = 0
 q.append((l_r, 0, 0)) # 방향, 몇 번째 칸인지, 시간
+visited[l_r][0] = 0
 d = [1, -1, K]
 answer = 0
 while q:
     directions, idx, time = q.popleft()
-    if idx >= N-1:
+    if idx > N-1:
         answer = 1
         break
     for k in range(2):
         next_idx = idx + d[k]
         if next_idx > time:
             if 0 <= next_idx and board[directions][next_idx] == 1 and visited[directions][next_idx] == -1:
-                visited[directions][next_idx] = time
+                visited[directions][next_idx] = time + 1
                 q.append((directions, next_idx, time+1))
     # 반대편 줄로 이동하는 경우
     next_idx = idx + d[2]
@@ -26,6 +27,7 @@ while q:
     directions %= 2
     if next_idx > time:
         if 0 <= next_idx and board[directions][next_idx] == 1 and visited[directions][next_idx] == -1:
-            visited[directions][next_idx] = time
+            visited[directions][next_idx] = time + 1
             q.append((directions, next_idx, time + 1))
+# print(visited)
 print(answer)
