2f18f278e0212a9a930106d3f07981b7b2bae021
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-01-11 23:26:34 +0900

    [Gold III] Title: 서울 지하철 2호선, Time: 1480 ms, Memory: 68144 KB -BaekjoonHub

--- 백준/Gold/16947. 서울 지하철 2호선/서울 지하철 2호선.py
+++ 백준/Gold/16947. 서울 지하철 2호선/서울 지하철 2호선.py
@@ -10,60 +10,54 @@ for _ in range(N):
     graph[a].append(b)
     graph[b].append(a)
 
-isCycle = [False] * (N+1) # 순환선에 해당하는 역인지 표시
+# isCycle = [False] * (N+1) # 순환선에 해당하는 역인지 표시
 findCycle = False # 순환선 찾았는지 확인
+cycle = [] # 순환선에 해당하는 역들
 
 # 사이클 확인하기
-def dfs(node, start, idx):
+def dfs(node, start, tmp):
     global findCycle
     if findCycle:
         return
     for next_node in graph[node]:
         if not visited[next_node]:
             visited[next_node] = True
-            dfs(next_node, start, idx+1)
+            dfs(next_node, start, tmp+[next_node])
             visited[next_node] = False
         # 순환선일 경우
-        elif visited[next_node] and idx >= 3:
+        elif visited[next_node] and len(tmp) >= 3:
             # 시작한 번호랑 같은 번호가 나오면
             if next_node == start:
                 findCycle = True
                 # 순환선에 해당하는 역들 다 표시
-                for i in range(1, N+1):
-                    if visited[i]:
-                        isCycle[i] = True
+                for t in tmp:
+                    cycle.append(t)
                 return
 
-def bfs(idx):
+ans = [-1] * (N+1)
+
+def bfs():
     q = deque()
-    visited = [-1] * (N+1)
-    q.append(idx)
-    visited[idx] = 0
+
+    for c in cycle:
+        q.append(c)
+        ans[c] = 0
+
     while q:
         node = q.popleft()
         for next_node in graph[node]:
-            if visited[next_node] == -1:
-                if isCycle[next_node]:
-                    return visited[node] + 1
-                else:
-                    q.append(next_node)
-                    visited[next_node] = visited[node] + 1
+            if ans[next_node] == -1:
+                q.append(next_node)
+                ans[next_node] = ans[node] + 1
 
 for i in range(1, N+1):
     visited = [False] * (N+1)
     visited[i] = True
-    dfs(i, i, 1)
+    dfs(i, i, [i])
+    if findCycle:
+        break
     visited[i] = False
 
-ans = []
+bfs()
 
-for i in range(1, N+1):
-    # 순환선에 해당하는 역인지 확인
-    if isCycle[i]:
-        # 맞으면 0
-        ans.append(0)
-    else:
-        # 아니면 그 역에서부터 bfs로 가장 가까운 순환선 역 찾기
-        ans.append(bfs(i))
-
-print(*ans)
+print(" ".join(map(str, ans[1:])))
