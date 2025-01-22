c6a44f920cb13c3eacf3c1111c5cf5ff762add5c
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-01-22 23:55:45 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 133.34 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
@@ -1,36 +1,37 @@
-dr = [0, 1, 0, -1] # 방향 전환을 계산하기 위해서 우, 하, 좌, 상
-dc = [1, 0, -1, 0] # 방향 전환을 계산하기 위해서 우, 하, 좌, 상
-ans = 25 * 25 * 600
+from collections import deque
 
+dr = [-1, 1, 0, 0]
+dc = [0, 0, -1, 1]
+
+def bfs(board, d):
+    n = len(board)
+    visited = [[1e9] * n for _ in range(n)]
+    visited[0][0] = 0
+    
+    q = deque()
+    q.append((0, 0, 0, d))  # r, c, cost, 방향
+
+    while q:
+        r, c, cost, direction = q.popleft()
+
+        if r == n - 1 and c == n - 1:
+            continue
+
+        for k in range(4):
+            nr = r + dr[k]
+            nc = c + dc[k]
+            if 0 <= nr < n and 0 <= nc < n:
+                if board[nr][nc] == 0:
+                    if k == direction:
+                        next_cost = cost + 100
+                    else:
+                        next_cost = cost + 600
+
+                    if next_cost <= visited[nr][nc]:
+                        visited[nr][nc] = next_cost
+                        q.append((nr, nc, next_cost, k))
+    return visited[n-1][n-1]
 def solution(board):
-    # dfs + dp, 방향 바뀌면 600원 추가, 아니면 100원 추가
-    length = len(board)
-    dp = [[1e9] * length for _ in range(length)]
-    def dfs(r, c, d, cost, n):
-        global ans
-        # 현재 위치가 최소 비용인지 확인
-        if dp[r][c] < cost:
-            return
-        else:
-            dp[r][c] = cost
-            
-        # 종료조건
-        if r == n-1 and c == n-1:
-            if cost < ans:
-                ans = cost
-            return
-        
-        # 지나온 곳 체크
-        board[r][c] = -1
-            
-        for i in range(4):
-            nr = r + dr[i]
-            nc = c + dc[i]
-            if 0 <= nr < n and 0 <= nc < n and board[nr][nc] == 0:
-                if (r == 0 and c == 0) or ((d + i) % 2 == 0):
-                    dfs(nr, nc, i, cost+100, n)
-                else:
-                    dfs(nr, nc, i, cost+600, n)
-        board[r][c] = 0
-    dfs(0, 0, 0, 0, len(board))
-    return ans
+    answer = min(bfs(board, 1), bfs(board, 3))
+
+    return answer
