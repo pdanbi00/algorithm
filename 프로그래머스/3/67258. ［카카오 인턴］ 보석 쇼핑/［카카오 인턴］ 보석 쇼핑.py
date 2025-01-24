5e43c1dbdacc40ce821bd28c4271d49eadcd3051
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-01-24 23:52:59 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 83.60 ms, Memory: 17 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
@@ -1,24 +1,31 @@
 def solution(gems):
     N = len(gems)
-    kind = len(set(gems))
-    dict = {gems[0] : 1}
-    ans = [0, N-1]
-    # 투포인터
-    s, e = 0, 0
-    while s < N and e < N:
-        if len(dict) < kind: # 보석 종류가 부족할 경우
-            e += 1
-            if e == N:
-                break
-            dict[gems[e]] = dict.get(gems[e], 0) + 1
-        else: # 보석 종류 다 포함되어 있으면 정답 구간이랑 비교
-            if ans[1] - ans[0] + 1 > e - s + 1:
-                ans = [s, e]
-            if dict[gems[s]] == 1:
-                del dict[gems[s]]
+    answer = [0, N-1]
+    kind = len(set(gems)) # 보석 종류 수
+    dict = {gems[0] : 1} # 보석 종류 체크 딕셔너리
+    left, right = 0, 0
+    
+    while left < len(gems) and right < len(gems):
+        # 딕셔너리에 보석 종류가 다 있는 경우
+        if len(dict) == kind:
+            if right - left < answer[1] - answer[0]:
+                answer = [left, right]
             else:
-                dict[gems[s]] -= 1
-            s += 1
-    ans[0] += 1
-    ans[1] += 1
-    return ans
+                dict[gems[left]] -= 1
+                if dict[gems[left]] == 0:
+                    del dict[gems[left]] # 개수 0이면 key도 없애줘야됨
+                left += 1
+        else:
+            right += 1
+            
+            if right == len(gems):
+                break
+            
+            if gems[right] in dict:
+                dict[gems[right]] += 1
+                
+            else:
+                dict[gems[right]] = 1
+    
+    
+    return answer[0] + 1, answer[1] + 1
