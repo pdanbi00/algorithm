eedd79a2f4b70c9469193f7497f64ef6d2ccfc41
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-01-27 18:22:36 +0900

    [Gold IV] Title: 십자가 2개 놓기, Time: 92 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/17085. 십자가 2개 놓기/십자가 2개 놓기.py
+++ 백준/Gold/17085. 십자가 2개 놓기/십자가 2개 놓기.py
@@ -28,17 +28,17 @@ def search(row, col):
     while 0 <= row - size and row + size < N and 0 <= col - size and col + size < M and board[row-size][col] == board[row+size][col] == board[row][col - size] == board[row][col + size] == '#':
         size += 1
     while size: # 작은 경우끼리 곱하는 경우가 더 클 수도 있으니 사이즈 줄여봐야 됨.
-        dotInfo.append((row, col, size))
+        dotInfo.append((row, col, size-1))
         size -= 1
 
 # 각 십자가가 서로 안 겹치는지 유효성 검사
 def test(dot1, dot2):
     testBoard = [[0] * M for _ in range(N)]
-    for i in range(dot1[2]):
+    for i in range(dot1[2]+1): # size가 1일 경우 i는 0밖에 안되는데 그럼 한칸씩 확장해서 살펴볼 수가 없음.
         for k in range(4):
             testBoard[dot1[0] + dr[k] * i][dot1[1] + dc[k] * i] = 1
 
-    for i in range(dot2[2]):
+    for i in range(dot2[2]+1): # size가 1일 경우 i는 0밖에 안되는데 그럼 한칸씩 확장해서 살펴볼 수가 없음.
         for k in range(4):
             if testBoard[dot2[0] + dr[k] * i][dot2[1] + dc[k] * i] == 1:
                 return False # 첫번째 십자가랑 겹치면 False 반환
@@ -52,7 +52,7 @@ for i in range(N):
 
 for i in range(len(dotInfo) - 1):
     for j in range(i+1, len(dotInfo)):
-        tmp = (1 + (dotInfo[i][2] - 1) * 4) * (1 + (dotInfo[j][2] - 1) * 4)
+        tmp = (1 + (dotInfo[i][2] * 4)) * (1 + (dotInfo[j][2] * 4))
         # 현재 값보다 큰 경우만 유호성 검사
         if tmp > answer and test(dotInfo[i], dotInfo[j]):
             answer = tmp
