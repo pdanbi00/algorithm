3ece5df0fa6d00407feb290de17bef1cee97f701
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-02-07 17:43:54 +0900

    [Gold V] Title: 동전, Time: 76 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/9084. 동전/동전.py
+++ 백준/Gold/9084. 동전/동전.py
@@ -2,15 +2,12 @@ T = int(input())
 for _ in range(T):
     N = int(input())
     coins = list(map(int, input().split()))
-    money = int(input())
+    M = int(input())
 
-    # dp[i] : i원을 만들 수 있는 가지수
-
-    dp = [0] * (money+1)
-    dp[0] = 1
-
+    dp = [0] * (M+1)
+    dp[0] = 1 # 0원을 만드는 경우의 수는 1개
     for coin in coins:
-        for i in range(1, money+1):
-            if i >= coin:
-                dp[i] += dp[i-coin]
-    print(dp[money])
+        for money in range(1, M+1):
+            if money >= coin:
+                dp[money] += dp[money-coin]
+    print(dp[M])
