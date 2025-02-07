adb27524c8d48bc901dabf37eb0dcf4b2266619e
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-02-07 17:31:17 +0900

    [Gold V] Title: 평범한 배낭, Time: 5084 ms, Memory: 281576 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,15 +1,13 @@
-import sys
-input = sys.stdin.readline
 N, K = map(int, input().split())
-bags = []
-dp = [[0] * (K+1) for _ in range(N+1)] # 열은 0부터 K까지의 가방 무게를 의미. 행은 각 가방들을 의미
-for i in range(N):
-    W, V = map(int, input().split())
-    bags.append((W, V))
+arr = [list(map(int, input().split())) for _ in range(N)]
+dp = [[0] * (K+1) for _ in range(N+1)]
+
 for i in range(1, N+1):
     for j in range(1, K+1):
-        if j >= bags[i-1][0]: # 지금 무게가 물건 무게보다 크거나 같은 경우
-            dp[i][j] = max(dp[i-1][j], dp[i-1][j-bags[i-1][0]]+bags[i-1][1]) # i가 1부터 시작하니깐 bags[i-1]로 봐야됨.
+        if j >= arr[i-1][0]:
+            # i-1번째까지의 물건들을 고려한 값이랑 현재 물건의 가치 + 이전 물건들의 가치 중 큰 값
+            dp[i][j] = max(arr[i-1][1] + dp[i-1][j-arr[i-1][0]], dp[i-1][j])
         else:
             dp[i][j] = dp[i-1][j]
+
 print(dp[N][K])
