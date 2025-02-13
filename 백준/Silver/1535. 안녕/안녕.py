4c2e10b70cbaa451a33875cbf20bdcd80e5047aa
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-02-14 00:01:42 +0900

    [Silver II] Title: 안녕, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1535. 안녕/안녕.py
+++ 백준/Silver/1535. 안녕/안녕.py
@@ -2,13 +2,13 @@ N = int(input())
 power = list(map(int, input().split()))
 happy = list(map(int, input().split()))
 
-# dp[i][j] : 체력이 j인 상태에서 i번째 사람에게 인사를 했을 경우 얻을 수 있는 최대 기쁨
+# dp[i][j] : 체력이 i인 상태에서 j번째 사람에게 인사를 했을 경우 얻을 수 있는 최대 기쁨
 
 dp = [[0] * 101 for _ in range(N+1)]
 for i in range(1, N+1):
     for j in range(1, 101):
-        if power[i-1] <= j:
+        if j - power[i-1] > 0:
             dp[i][j] = max(dp[i-1][j], dp[i-1][j-power[i-1]] + happy[i-1])
         else:
             dp[i][j] = dp[i-1][j]
-print(dp[N][99])
+print(dp[N][100])
