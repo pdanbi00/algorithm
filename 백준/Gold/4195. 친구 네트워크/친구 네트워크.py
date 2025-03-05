b0a1c9951f068cdc0574bfcbe03b8adc29f717c1
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-03-06 00:08:12 +0900

    [Gold II] Title: 친구 네트워크, Time: 264 ms, Memory: 62452 KB -BaekjoonHub

--- 백준/Gold/4195. 친구 네트워크/친구 네트워크.py
+++ 백준/Gold/4195. 친구 네트워크/친구 네트워크.py
@@ -1,11 +1,15 @@
 import sys
+sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
+
 def find(a):
     if parents[a] != a:
         parents[a] = find(parents[a])
     return parents[a]
 
+# 문자열이라서 부모 대소 비교가 안되니깐 무조건 a가 부모라고 생각하기
 def union(a, b):
+    # 각 부모노드 찾기
     a = find(a)
     b = find(b)
     if a != b:
@@ -16,15 +20,17 @@ def union(a, b):
 T = int(input())
 for _ in range(T):
     F = int(input())
-    idx = 0
     parents = {}
     count = {}
     for i in range(F):
         f1, f2 = input().split()
+        # 친구 관계의 없는 친구면 추가
         if f1 not in parents:
             parents[f1] = f1
             count[f1] = 1
+
         if f2 not in parents:
             parents[f2] = f2
             count[f2] = 1
+
         union(f1, f2)
