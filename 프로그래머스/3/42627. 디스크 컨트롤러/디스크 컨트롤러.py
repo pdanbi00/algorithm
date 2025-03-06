a3c834cb0188dca72e8bad299d94f5e4ca5588a6
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-03-06 21:56:35 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.57 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -3,25 +3,24 @@ from heapq import heappush, heappop
 
 def solution(jobs):
     answer = 0
-    now = 0 # 현재시간
-    i = 0 # 처리 개수
-    start = -1 # 직전 완료 작업의 시작시간
-    heap = []
+    jobs.sort()
     
-    while i < len(jobs):
-        # 현재 시점에서 처리할 수 있는 작업 판단
-        # 작업 요청 시간이 마지막 완료 작업의 시작시간보다 크고
-        # 현재시점보다 작거나 같아야 됨.
-        for job in jobs:
-            if start < job[0] <= now:
-                heappush(heap, (job[1], job[0]))
-        
+    N = len(jobs)
+    heap = []
+    idx = 0
+    now = 0 # 현재시간
+    
+    while idx < N or heap:
+        while idx < N and now >= jobs[idx][0]:
+            heappush(heap, (jobs[idx][1], jobs[idx][0], idx))
+            idx += 1
+            
         if heap:
-            current = heappop(heap)
-            start = now
-            now += current[0]
-            answer += (now - current[1])
-            i += 1
+            time, request, index = heappop(heap)
+            now += time
+            answer += (now - request)
+            
         else:
-            now += 1
-    return answer // len(jobs)
+            now = jobs[idx][0]
+            
+    return answer // N
