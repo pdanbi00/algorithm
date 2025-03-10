8b168fd71e4b53f428ce9da148fd66c8aabb5151
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-03-10 21:50:56 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 931.54 ms, Memory: 143 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.py
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.py
@@ -1,35 +1,27 @@
-def solution(board, skill):
-    # 누적합
+def solution(board, skills):
     answer = 0
-    tmp = [[0] * (len(board[0])+1) for _ in range(len(board)+1)]
-    
-    for type, r1, c1, r2, c2, degree in skill:
+    N = len(board)
+    M = len(board[0])
+    new_board = [[0] * (M+1) for _ in range(N+1)]
+    for skill in skills:
+        type, r1, c1, r2, c2, n = skill
         if type == 1:
-            tmp[r1][c1] -= degree
-            tmp[r1][c2+1] += degree
-            tmp[r2+1][c1] += degree
-            tmp[r2+1][c2+1] -= degree
-        else:
-            tmp[r1][c1] += degree
-            tmp[r1][c2+1] -= degree
-            tmp[r2+1][c1] -= degree
-            tmp[r2+1][c2+1] += degree
+            n = -n
+        new_board[r1][c1] += n
+        new_board[r1][c2+1] += -n
+        new_board[r2+1][c1] += -n
+        new_board[r2+1][c2+1] += n
+    for i in range(N):
+        for j in range(1, M):
+            new_board[i][j] += new_board[i][j-1]
             
-    # 누적합 구하기
-    # 행 기준 누적합
-    for i in range(len(tmp)-1):
-        for j in range(len(tmp[0])-1):
-            tmp[i][j+1] += tmp[i][j]
+    for j in range(M):
+        for i in range(1, N):
+            new_board[i][j] += new_board[i-1][j]
             
-    # 열 기준 누적합
-    for j in range(len(tmp[0])-1):
-        for i in range(len(tmp)-1):
-            tmp[i+1][j] += tmp[i][j]
-            
-    # 최종 계산
-    for i in range(len(board)):
-        for j in range(len(board[0])):
-            board[i][j] += tmp[i][j]
-            if board[i][j] > 0:
+    for i in range(N):
+        for j in range(M):
+            board[i][j] += new_board[i][j]
+            if board[i][j] >= 1:
                 answer += 1
     return answer
