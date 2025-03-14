858760a9ab6641fe03e06dc0c9b06981aba1558f
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-03-14 23:12:32 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 3.60 ms, Memory: 9.83 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -1,33 +1,29 @@
+# def solution(files):
+#     answer = []
+#     head, number, tail = '', '', ''
+    
+#     for file in files:
+#         for i in range(len(file)):
+#             if file[i].isdigit(): # 숫자가 나오면 그 이전은 무조건 HEAD
+#                 head = file[:i]
+#                 number = file[i:]
+                
+#                 for j in range(len(number)): # NUMBER와 TAIL구분
+#                     if not number[j].isdigit():
+#                         tail = number[j:]
+#                         number = number[:j]
+#                         break
+#                 answer.append([head, number, tail])
+#                 head, number, tail = '', '', ''
+#                 break
+                
+#     answer.sort(key=lambda x : (x[0].lower(), int(x[1])))
+    
+#     return [''.join(ans) for ans in answer]
+
+# 정규식
+import re
 def solution(files):
-    answer = []
-    N = len(files)
-    new_file = []
-    num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    for idx in range(N):
-        tmp = [files[idx]]
-        find_num1 = -1
-        find_num2 = -1
-        head = []
-        number = ''
-        for i in range(len(files[idx])):
-            if files[idx][i] in num and find_num1 == -1:
-                find_num1 = i
-                find_num2 = i
-            elif find_num1 == -1 and files[idx][i] not in num:
-                head.append(files[idx][i].upper())    
-            elif find_num1 != -1 and files[idx][i] not in num:
-                find_num2 = i
-                break
-        for i in range(find_num1, find_num2):
-            number += files[idx][i]
-        number = int(number)
-        tmp.append(head)
-        tmp.append(number)  
-        tmp.append(idx)
-        new_file.append(tmp)
-        
-    new_file.sort(key=lambda x : (x[1], x[2], x[3]))
-    for i in range(N):
-        answer.append(new_file[i][0])
-            
-    return answer
+    answer = [re.split(r"([0-9]+)", f) for f in files]
+    answer.sort(key=lambda x : (x[0].lower(), int(x[1])))
+    return[''.join(ans) for ans in answer]
