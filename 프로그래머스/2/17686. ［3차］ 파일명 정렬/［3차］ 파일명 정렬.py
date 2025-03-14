317f7a77d8b973dcb24cd94257ec682b380970fb
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-03-14 23:03:02 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 2.74 ms, Memory: 9.77 MB -BaekjoonHub

---
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -0,0 +1,24 @@
+def solution(files):
+    answer = []
+    head, number, tail = '', '', ''
+    
+    for file in files:
+        for i in range(len(file)):
+            if file[i].isdigit(): # 숫자가 나오면 그 이전은 무조건 HEAD
+                head = file[:i]
+                number = file[i:]
+                
+                for j in range(len(number)): # NUMBER와 TAIL구분
+                    if not number[j].isdigit():
+                        tail = number[j:]
+                        number = number[:j]
+                        break
+                answer.append([head, number, tail])
+                head, number, tail = '', '', ''
+                break
+                
+    answer.sort(key=lambda x : (x[0].lower(), int(x[1])))
+    
+                
+            
+    return [''.join(ans) for ans in answer]
