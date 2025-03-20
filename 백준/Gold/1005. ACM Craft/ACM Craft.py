d5f13fb261ef44c09f64a379b86fcb078bec147d
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-03-20 16:49:19 +0900

    [Gold III] Title: ACM Craft, Time: 1164 ms, Memory: 37620 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.py
+++ 백준/Gold/1005. ACM Craft/ACM Craft.py
@@ -2,31 +2,33 @@ from collections import deque
 import sys
 input = sys.stdin.readline
 T = int(input())
+
 for _ in range(T):
     N, K = map(int, input().split())
     buildings = [0] + list(map(int, input().split()))
     graph = [[] for _ in range(N+1)] # 각 건물을 짓게 위해 미리 지어져야하는 건물들 정보
+    inDegree = [0] * (N+1)
+    dp = [0] * (N + 1)
+    q = deque()
+
     for i in range(K):
         a, b = map(int, input().split())
-        graph[b].append(a)
+        graph[a].append(b)
+        inDegree[b] += 1
     W = int(input())
 
-    q = deque()
     for i in range(1, N+1):
-        q.append(i)
-    dp = [-1] * (N+1)
+        if inDegree[i] == 0:
+            q.append(i)
+            dp[i] = buildings[i]
 
     while q:
-        now = q.popleft()
-        possible = True
-        for next in graph[now]:
-            if dp[next] == -1:
-                possible = False
-                q.append(now)
-                break
-        if possible:
-            dp[now] = buildings[now]
-            for next in graph[now]:
-                dp[now] = max(dp[now], dp[next] + buildings[now])
+        tmp = q.popleft()
+
+        for next in graph[tmp]:
+            inDegree[next] -= 1
+            dp[next] = max(dp[next], dp[tmp] + buildings[next])
+            if inDegree[next] == 0:
+                q.append(next)
 
     print(dp[W])
