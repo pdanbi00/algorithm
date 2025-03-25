7d886cd7666204c356631bf2d46e878cd3366c81
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-03-25 16:21:22 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 84 ms, Memory: 36872 KB -BaekjoonHub

--- 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.py
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.py
@@ -1,14 +1,17 @@
-# 아이디어 : 내가 볼 수 있는 옥상의 수가 아니라 나를 볼 수 있는 관리인의 수를 구해야 됨.
+# 핵심은 stack 안의 원소 개수 활용
+# 내가 볼 수 있는 옥상의 수가 아니라 나를 볼 수 있는
+import sys
+input = sys.stdin.readline
+
 N = int(input())
 ans = 0
-stack = []
 buildings = [int(input()) for _ in range(N)]
+stack = []
+for height in buildings:
+    while stack and stack[-1] <= height:
+        stack.pop() # 나를 볼 수 없는 관리인 제거
 
-for building in buildings:
-    while stack and stack[-1] <= building:
-        stack.pop() # 옥상을 볼 수 없는 관리인은 전부 제거
-
-    ans += len(stack) # 옥상을 볼 수 있는 관리인 수
-    stack.append(building) # 지금 빌딩 스택에 추가
+    ans += len(stack) # 나를 볼 수 있는 관리인 수 추가
+    stack.append(height) # 현재 빌딩 높이 추가
 
 print(ans)
