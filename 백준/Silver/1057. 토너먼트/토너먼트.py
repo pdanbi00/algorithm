3412f67d9c8cf9492f13ec641cc0eac7c73736be
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-04-03 23:46:33 +0900

    [Silver IV] Title: 토너먼트, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1057. 토너먼트/토너먼트.py
+++ 백준/Silver/1057. 토너먼트/토너먼트.py
@@ -1,32 +1,7 @@
-N, J, H = map(int, input().split())
-arr = [i for i in range(1, N+1)]
-cnt = 1
-while len(arr) >= 2:
-    new_arr = []
-    if len(arr) % 2 == 1:
-        for i in range(0, len(arr)-1, 2):
-            if arr[i] != J and arr[i] != H and arr[i+1] != J and arr[i+1] != H:
-                new_arr.append(arr[i])
-            elif (arr[i] == J and arr[i+1] == H) or (arr[i] == H and arr[i+1] == J):
-                print(cnt)
-                exit()
-            elif arr[i] == J or arr[i] == H:
-                new_arr.append(arr[i])
-            elif arr[i+1] == J or arr[i+1] == H:
-                new_arr.append(arr[i+1])
-        new_arr.append(arr[-1])
-    else:
-        for i in range(0, len(arr), 2):
-            if arr[i] != J and arr[i] != H and arr[i+1] != J and arr[i+1] != H:
-                new_arr.append(arr[i])
-            elif (arr[i] == J and arr[i+1] == H) or (arr[i] == H and arr[i+1] == J):
-                print(cnt)
-                exit()
-            elif arr[i] == J or arr[i] == H:
-                new_arr.append(arr[i])
-            elif arr[i+1] == J or arr[i+1] == H:
-                new_arr.append(arr[i+1])
+n, start, end = map(int, input().split())
+cnt = 0
+while start != end:
+    start -= start // 2
+    end -= end // 2
     cnt += 1
-    arr = new_arr
-
-print(-1)
+print(cnt)
