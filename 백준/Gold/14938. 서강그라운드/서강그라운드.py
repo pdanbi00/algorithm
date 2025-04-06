587b54dd84f2702ad7eafaa034fb8dd2eeb6c3e4
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-04-06 16:30:01 +0900

    [Gold IV] Title: 서강그라운드, Time: 44 ms, Memory: 35508 KB -BaekjoonHub

--- 백준/Gold/14938. 서강그라운드/서강그라운드.py
+++ 백준/Gold/14938. 서강그라운드/서강그라운드.py
@@ -1,6 +1,5 @@
 # 각 점에서의 최단 거리를 모두 구하고
 # 최단거리가 수색 범위 이내인 경우 아이템 개수 다 더해서 갱신학
-from collections import deque
 from heapq import heappush, heappop
 N, M, R = map(int, input().split())
 items = [0] + list(map(int, input().split()))
@@ -12,19 +11,20 @@ for _ in range(R):
 
 def dijkstra(i):
     distance = [1e9] * (N + 1)
-    visited = [False] * (N + 1)
     distance[i] = 0
     heap = []
     heappush(heap, (0, i))
     while heap:
         cur_dis, cur_r = heappop(heap)
-        visited[cur_r] = True
+        if distance[cur_r] < cur_dis:
+            continue
+            
         for next_r, next_dis in graph[cur_r]:
-            if cur_dis + next_dis <= distance[next_r] and not visited[next_r]:
-                heappush(heap,(cur_dis + next_dis, next_r))
-                distance[next_r] = cur_dis + next_dis
+            dis = cur_dis + next_dis
+            if dis < distance[next_r]:
+                heappush(heap, (dis, next_r))
+                distance[next_r] = dis
     return distance
-
 
 ans = 0
 # i에서 각 점까지의 최단거리 구하기
