e1726bac31d35e35d16bc5d376f3235bc5f0ca8b
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-04-08 17:50:24 +0900

    [Gold II] Title: 통나무 옮기기, Time: 80 ms, Memory: 35872 KB -BaekjoonHub

--- 백준/Gold/1938. 통나무 옮기기/통나무 옮기기.py
+++ 백준/Gold/1938. 통나무 옮기기/통나무 옮기기.py
@@ -1,177 +1,162 @@
+import sys
 from collections import deque
-from copy import deepcopy
+input = sys.stdin.readline
+
+def checkDir(temp):
+    if temp[0][1] == temp[1][1]:
+        return 0 # 세로 방향 : 0
+    return 1 # 가로 방향 : 1
+
+def checkVisited(temp, d, cnt):
+    if temp not in visited:
+        visited.add(temp)
+        q.append((temp, d, cnt+1))
+
+def bfs():
+    while q:
+        tmp, d, cnt = q.popleft()
+
+        if tmp == target:
+            return cnt
+
+        if d == 0: # 세로 뱡향일 경우
+            for cmd in range(5):
+                # U
+                if cmd == 0:
+                    new_tmp = deque(tmp)
+                    r, c = new_tmp[0]
+                    if (0 <= r-1 < N) and board[r-1][c] == '0':
+                        new_tmp.pop()
+                        new_tmp.appendleft((r-1, c))
+                        checkVisited(tuple(new_tmp), 0, cnt)
+                # D
+                elif cmd == 1:
+                    new_tmp = deque(tmp)
+                    r, c = new_tmp[2]
+                    if (0 <= r + 1 < N) and board[r+1][c] == '0':
+                        new_tmp.popleft()
+                        new_tmp.append((r+1, c))
+                        checkVisited(tuple(new_tmp), 0, cnt)
+
+                # L
+                elif cmd == 2:
+                    flag = 1
+                    for r, c in tmp:
+                        if not (0 <= c - 1 < N) or board[r][c-1] == '1':
+                            flag = 0
+                            break
+                    if flag:
+                        new_tmp = tuple([(r, c-1) for r, c in tmp])
+                        checkVisited(new_tmp, 0, cnt)
+
+                # R
+                elif cmd == 3:
+                    flag = 1
+                    for r, c in tmp:
+                        if not (0 <= c + 1 < N) or board[r][c + 1] == '1':
+                            flag = 0
+                            break
+                    if flag:
+                        new_tmp = tuple([(r, c + 1) for r, c in tmp])
+                        checkVisited(new_tmp, 0, cnt)
+
+                # T
+                else:
+                    flag = 1
+                    for r, c in tmp:
+                        if not (1 <= c < N-1):
+                            flag = 0
+                            break
+
+                        if board[r][c-1] == '1' or board[r][c+1] == '1':
+                            flag = 0
+                            break
+                    if flag:
+                        cr, cc = tmp[1]
+                        new_tmp = ((cr, cc-1), (cr, cc), (cr, cc+1))
+                        checkVisited(new_tmp, 1, cnt)
+
+        else: # 가로 방향일 경우
+            for cmd in range(5):
+                # U
+                if cmd == 0:
+                    flag = 1
+                    for r, c in tmp:
+                        if not (0 <= r - 1 < N) or board[r-1][c] == '1':
+                            flag = 0
+                            break
+                    if flag:
+                        new_tmp = tuple([(r-1, c) for r, c in tmp])
+                        checkVisited(new_tmp, 1, cnt)
+
+                # D
+                elif cmd == 1:
+                    flag = 1
+                    for r, c in tmp:
+                        if not (0 <= r + 1 < N) or board[r + 1][c] == '1':
+                            flag = 0
+                            break
+                    if flag:
+                        new_tmp = tuple([(r + 1, c) for r, c in tmp])
+                        checkVisited(new_tmp, 1, cnt)
+
+                # L
+                elif cmd == 2:
+                    new_tmp = deque(tmp)
+                    r, c = new_tmp[0]
+                    if (0 <= c-1 < N) and board[r][c-1] == '0':
+                        new_tmp.pop()
+                        new_tmp.appendleft((r, c-1))
+                        checkVisited(tuple(new_tmp), 1, cnt)
+
+                # R
+                elif cmd == 3:
+                    new_tmp = deque(tmp)
+                    r, c = new_tmp[2]
+                    if (0 <= c + 1 < N) and board[r][c + 1] == '0':
+                        new_tmp.popleft()
+                        new_tmp.append((r, c + 1))
+                        checkVisited(tuple(new_tmp), 1, cnt)
+                # T
+                else:
+                    flag = 1
+                    for r, c in tmp:
+                        if not (1 <= r < N-1):
+                            flag = 0
+                            break
+
+                        if board[r-1][c] == '1' or board[r+1][c] == '1':
+                            flag = 0
+                            break
+
+                    if flag:
+                        cr, cc = tmp[1]
+                        new_tmp = ((cr-1, cc), (cr, cc), (cr+1, cc))
+                        checkVisited(new_tmp, 0, cnt)
+
+    return 0
+
 N = int(input())
 board = []
 tong = []
 target = []
 for i in range(N):
-    arr = list(input())
+    arr = list(input().rstrip())
     for j in range(N):
         if arr[j] == 'B':
-            tong.append([i, j])
+            tong.append((i, j))
+            arr[j] = '0'
         elif arr[j] == 'E':
-            target.append([i, j])
+            target.append((i, j))
+            arr[j] = '0'
     board.append(arr)
 
-# 통나무 방향 판단하기
-if tong[0][0] == tong[1][0]: # 가로
-    tong.append(0)
-else: # 세로
-    tong.append(1)
+startDir = checkDir(tong)
+target = tuple(target)
 
-# 상하좌우 움직이기 가능
-# 이동하려는 곳이 1이 아니어야 됨
-# visited에 통나무 위치 배열 계속 담기
-# 이동할 때마다 방향도 기록해놓기
-# 가로 방향일 경우 회전시키려면 상하에 1이 없어야 됨
-# 세로 방향일 경우 회전시키려면 좌우에 1이 없어야 됨
-
-# 통나무가 가로일 경우 상, 하, 좌, 우, 회전 확인
-def garo(tree, cnt):
-    global q, visited
-
-    # 상
-    possible = True
-    t = deepcopy(tree[:3])
-    for k in range(3):
-        if t[k][0] - 1 < 0 or board[t[k][0] - 1][t[k][1]] == '1':
-            possible = False
-    if possible:
-        for k in range(3):
-            t[k][0] -= 1
-        if t not in visited:
-            q.append([t + [0], cnt + 1])
-            visited.append(t)
-
-    # 하
-    possible = True
-    t = deepcopy(tree[:3])
-    for k in range(3):
-        if t[k][0] + 1 >= N or board[t[k][0] + 1][t[k][1]] == '1':
-            possible = False
-    if possible:
-        for k in range(3):
-            t[k][0] += 1
-        if t not in visited:
-            q.append([t + [0], cnt + 1])
-            visited.append(t)
-
-    # 좌
-    possible = True
-    t = deepcopy(tree[:3])
-    if t[0][1] - 1 < 0 or board[t[0][0]][t[0][1] - 1] == '1':
-        possible = False
-    if possible:
-        for k in range(3):
-            t[k][1] -= 1
-        if t not in visited:
-            q.append([t + [0], cnt + 1])
-            visited.append(t)
-    # 우
-    possible = True
-    t = deepcopy(tree[:3])
-    if t[2][1] + 1 >= N or board[t[2][0]][t[2][1] + 1] == '1':
-        possible = False
-    if possible:
-        for k in range(3):
-            t[k][1] += 1
-        if t not in visited:
-            q.append([t + [0], cnt + 1])
-            visited.append(t)
-    # 회전
-    possible = True
-    t = deepcopy(tree[:3])
-    for k in range(3):
-        if t[k][0] + 1 >= N or board[t[k][0] + 1][t[k][1]] == '1' or t[k][0] - 1 < 0 or board[t[k][0] - 1][t[k][1]] == '1':
-            possible = False
-    if possible:
-        t = [[t[1][0] - 1 , t[1][1]], [t[1][0] , t[1][1]], [t[1][0] + 1 , t[1][1]]]
-        if t not in visited:
-            q.append([t + [1], cnt + 1])
-            visited.append(t)
-
-# 통나무가 세로일 경우 상, 하, 좌, 우, 회전 확인
-def sero(tree, cnt):
-    global q, visited
-    # 상
-    possible = True
-    t = deepcopy(tree[:3])
-    if t[0][0] - 1 < 0 or board[t[0][0]-1][t[0][1]] == '1':
-        possible = False
-    if possible:
-        for k in range(3):
-            t[k][0] -= 1
-        if t not in visited:
-            q.append([t + [1], cnt + 1])
-            visited.append(t)
-
-    # 하
-    possible = True
-    t = deepcopy(tree[:3])
-    if t[2][0] + 1 >= N or board[t[2][0] + 1][t[2][1]] == '1':
-        possible = False
-    if possible:
-        for k in range(3):
-            t[k][0] += 1
-        if t not in visited:
-            q.append([t + [1], cnt + 1])
-            visited.append(t)
-
-    # 좌
-    possible = True
-    t = deepcopy(tree[:3])
-    for k in range(3):
-        if t[k][1] - 1 < 0 or board[t[k][0]][t[k][1] - 1] == '1':
-            possible = False
-    if possible:
-        for k in range(3):
-            t[k][1] -= 1
-        if t not in visited:
-            q.append([t + [1], cnt + 1])
-            visited.append(t)
-
-    # 우
-    possible = True
-    t = deepcopy(tree[:3])
-    for k in range(3):
-        if t[k][1] + 1 >= N or board[t[k][0]][t[k][1] + 1] == '1':
-            possible = False
-    if possible:
-        for k in range(3):
-            t[k][1] += 1
-        if t not in visited:
-            q.append([t + [1], cnt + 1])
-            visited.append(t)
-
-    # 회전
-    possible = True
-    t = deepcopy(tree[:3])
-    for k in range(3):
-        if t[k][1] + 1 >= N or board[t[k][0]][t[k][1] + 1] == '1' or t[k][1] - 1 < 0 or board[t[k][0]][t[k][1] - 1] == '1':
-            possible = False
-    if possible:
-        t = [[t[1][0], t[1][1] - 1], [t[1][0], t[1][1]], [t[1][0], t[1][1] + 1]]
-        if t not in visited:
-            q.append([t + [0], cnt + 1])
-            visited.append(t)
-
-answer = 1e9
-visited = []
+visited = set()
+visited.add(tuple(tong))
 q = deque()
-q.append([tong, 0]) # [나무 위치들, 방향], 움직인 횟수
-visited.append(tong[:-1])
-while q:
-    tree, cnt = q.popleft()
-    # print(tree)
-    if tree[0] == target[0] and tree[1] == target[1] and tree[2] == target[2]:
-        answer = cnt
-        break
-    if tree[-1] == 0:
-        garo(tree, cnt)
-    else:
-        sero(tree, cnt)
+q.append((tong, startDir, 0))
 
-if answer == 1e9:
-    print(0)
-else:
-    print(answer)
+print(bfs())
