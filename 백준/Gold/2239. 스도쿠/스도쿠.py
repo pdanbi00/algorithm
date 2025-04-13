ce9bfb3c2fd44547361ec4deed48ae3ce3cc01a5
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-04-13 16:08:05 +0900

    [Gold IV] Title: 스도쿠, Time: 2000 ms, Memory: 122300 KB -BaekjoonHub

---
+++ 백준/Gold/2239. 스도쿠/스도쿠.py
@@ -0,0 +1,45 @@
+board = []
+empty = []
+
+for i in range(9):
+    arr = list(input())
+    new_arr = []
+    for j in range(9):
+        new_arr.append(int(arr[j]))
+        if arr[j] == '0':
+            empty.append((i, j))
+    board.append(new_arr)
+
+def func(idx):
+    if idx == len(empty):
+        for i in range(9):
+            for j in range(9):
+                print(board[i][j], end='')
+            print()
+        exit()
+    r, c = empty[idx][0], empty[idx][1]
+    for n in range(1, 10):
+        possible = True
+        # 가로, 세로 확인하기
+        for i in range(9):
+            if board[r][i] == n:
+                possible = False
+                break
+
+            if board[i][c] == n:
+                possible = False
+                break
+        if possible:
+            # 9칸 확인하기
+            for i in range(3):
+                for j in range(3):
+                    if board[3 * (r//3) + i][3 * (c//3) + j] == n:
+                        possible = False
+                        break
+        if possible:
+            board[r][c] = n
+            func(idx+1)
+            board[r][c] = 0
+
+
+func(0)
