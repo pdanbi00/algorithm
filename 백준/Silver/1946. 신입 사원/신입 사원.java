f5e83e92a391084c9f54e7abacdd0b8a1dde7665
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-05-05 00:11:04 +0900

    [Silver I] Title: 신입 사원, Time: 780 ms, Memory: 301744 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.java
+++ 백준/Silver/1946. 신입 사원/신입 사원.java
@@ -3,35 +3,27 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
         int T = Integer.parseInt(br.readLine());
-
         for (int t = 0; t < T; t++) {
             int N = Integer.parseInt(br.readLine());
-            int[][] scores = new int[N][2];
+            int[] arr = new int[N+1];
 
             for (int i = 0; i < N; i++) {
-                String[] arr = br.readLine().split(" ");
-                scores[i][0] = Integer.parseInt(arr[0]);
-                scores[i][1] = Integer.parseInt(arr[1]);
+                st = new StringTokenizer(br.readLine());
+                int rank = Integer.parseInt(st.nextToken());
+                arr[rank] = Integer.parseInt(st.nextToken());
             }
 
-            // 서류 등수 순으로 정렬
-            Arrays.sort(scores, new Comparator<int[]>() {
-                @Override
-                public int compare(int[] o1, int[] o2) {
-                    return Integer.compare(o1[0], o2[0]);
-                }
-            });
-
-            int top = scores[0][1];
-            int cnt = 1;
-            for (int i = 1; i < N; i++) {
-                if (top > scores[i][1]) {
-                    cnt++;
-                    top = scores[i][1];
+            int ans = 1;
+            int min = arr[1];
+            for (int i = 2; i <= N; i++) {
+                if (arr[i] < min) {
+                    ans++;
+                    min = arr[i];
                 }
             }
-            System.out.println(cnt);
+            System.out.println(ans);
         }
 
     }
