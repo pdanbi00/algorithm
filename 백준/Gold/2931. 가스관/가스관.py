9d39119df817b6a980c88ded02277096725fbb01
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-05-06 21:36:08 +0900

    [Gold II] Title: 가스관, Time: 92 ms, Memory: 35204 KB -BaekjoonHub

--- 백준/Gold/2931. 가스관/가스관.py
+++ 백준/Gold/2931. 가스관/가스관.py
@@ -46,202 +46,71 @@ def bfs():
     while q:
         r, c, d = q.popleft()
 
-        if board[r][c] == '|':
-            # 아래쪽에서 온 경우
+        if board[r][c] in ('1', '2', '3', '4'):
+            if board[r][c] == '1':
+                # 오른쪽에서 온 경우
+                if d == 2:
+                    d = 1
+
+                # 아래쪽에서 온 경우
+                elif d == 0:
+                    d = 3
+            elif board[r][c] == '2':
+                # 오른쪽에서 온 경우
+                if d == 2:
+                    d = 0
+
+                # 위쪽에서 온 경우
+                elif d == 1:
+                    d = 3
+            elif board[r][c] == '3':
+                # 왼쪽에서 온 경우
+                if d == 3:
+                    d = 0
+
+                # 위쪽에서 온 경우
+                elif d == 1:
+                    d = 2
+
+            elif board[r][c] == '4':
+                # 왼쪽에서 온 경우
+                if d == 3:
+                    d = 1
+
+                # 아래쪽에서 온 경우
+                elif d == 0:
+                    d = 2
+
+        nr = r + dr[d]
+        nc = c + dc[d]
+
+        if 0 <= nr < R and 0 <= nc < C:
+            if nr == z_r and nc == z_c:
+                return True
+
+            # 위쪽으로 가는 경우
             if d == 0:
-                nr = r + dr[d]
-                nc = c + dc[d]
-                if 0 <= nr < R and 0 <= nc < C:
-                    if nr == z_r and nc == z_c:
-                        return True
+                if board[nr][nc] in ('|', '+', '1', '4') and not visited[nr][nc][d]:
+                    q.append((nr, nc, d))
+                    visited[nr][nc][d] = True
 
-                    if board[nr][nc] in ('|', '+', '1', '4') and not visited[nr][nc][d]:
-                        q.append((nr, nc, d))
-                        visited[nr][nc][d] = True
-
-            # 위쪽에서 온 경우
+            # 아래쪽으로 가는 경우
             elif d == 1:
-                nr = r + dr[d]
-                nc = c + dc[d]
-                if 0 <= nr < R and 0 <= nc < C:
-                    if nr == z_r and nc == z_c:
-                        return True
+                if board[nr][nc] in ('|', '+', '2', '3') and not visited[nr][nc][d]:
+                    q.append((nr, nc, d))
+                    visited[nr][nc][d] = True
 
-                    if board[nr][nc] in ('|', '+', '2', '3') and not visited[nr][nc][d]:
-                        q.append((nr, nc, d))
-                        visited[nr][nc][d] = True
-
-        elif board[r][c] == '-':
-            # 오른쪽에서 온 경우
-            if d == 2:
-                nr = r + dr[d]
-                nc = c + dc[d]
-                if 0 <= nr < R and 0 <= nc < C:
-                    if nr == z_r and nc == z_c:
-                        return True
-                    if board[nr][nc] in ('-', '+', '1', '2') and not visited[nr][nc][d]:
-                        q.append((nr, nc, d))
-                        visited[nr][nc][d] = True
-
-            # 왼쪽에서 온 경우
-            elif d == 3:
-                nr = r + dr[d]
-                nc = c + dc[d]
-                if 0 <= nr < R and 0 <= nc < C:
-                    if nr == z_r and nc == z_c:
-                        return True
-
-                    if board[nr][nc] in ('-', '+', '3', '4') and not visited[nr][nc][d]:
-                        q.append((nr, nc, d))
-                        visited[nr][nc][d] = True
-
-        elif board[r][c] == '+':
-            # 아래쪽에서 온 경우
-            if d == 0:
-                nr = r + dr[d]
-                nc = c + dc[d]
-                if 0 <= nr < R and 0 <= nc < C:
-                    if nr == z_r and nc == z_c:
-                        return True
-
-                    if board[nr][nc] in ('|', '+', '1', '4') and not visited[nr][nc][d]:
-                        q.append((nr, nc, d))
-                        visited[nr][nc][d] = True
-            # 위쪽에서 온 경우
-            elif d == 1:
-                nr = r + dr[d]
-                nc = c + dc[d]
-                if 0 <= nr < R and 0 <= nc < C:
-                    if nr == z_r and nc == z_c:
-                        return True
-
-                    if board[nr][nc] in ('|', '+', '2', '3') and not visited[nr][nc][d]:
-                        q.append((nr, nc, d))
-                        visited[nr][nc][d] = True
-
-            # 오른쪽에서 온 경우
+            # 왼쪽으로 가는 경우
             elif d == 2:
-                nr = r + dr[d]
-                nc = c + dc[d]
-                if 0 <= nr < R and 0 <= nc < C:
-                    if nr == z_r and nc == z_c:
-                        return True
+                if board[nr][nc] in ('-', '+', '1', '2') and not visited[nr][nc][d]:
+                    q.append((nr, nc, d))
+                    visited[nr][nc][d] = True
 
-                    if board[nr][nc] in ('-', '+', '1', '2') and not visited[nr][nc][d]:
-                        q.append((nr, nc, d))
-                        visited[nr][nc][d] = True
-
-            # 왼쪽에서 온 경우
+            # 오른쪽으로 가는 경우
             elif d == 3:
-                nr = r + dr[d]
-                nc = c + dc[d]
-                if 0 <= nr < R and 0 <= nc < C:
-                    if nr == z_r and nc == z_c:
-                        return True
-
-                    if board[nr][nc] in ('-', '+', '3', '4') and not visited[nr][nc][d]:
-                        q.append((nr, nc, d))
-                        visited[nr][nc][d] = True
-
-        elif board[r][c] == '1':
-            # 오른쪽에서 온 경우
-            if d == 2:
-                nr = r + dr[1]
-                nc = c + dc[1]
-                if 0 <= nr < R and 0 <= nc < C:
-                    if nr == z_r and nc == z_c:
-                        return True
-
-                    if board[nr][nc] in ('|', '+', '2', '3') and not visited[nr][nc][1]:
-                        q.append((nr, nc, 1))
-                        visited[nr][nc][1] = True
-
-            # 아래쪽에서 온 경우
-            elif d == 0:
-                nr = r + dr[3]
-                nc = c + dc[3]
-                if 0 <= nr < R and 0 <= nc < C:
-                    if nr == z_r and nc == z_c:
-                        return True
-
-                    if board[nr][nc] in ('-', '+', '3', '4') and not visited[nr][nc][3]:
-                        q.append((nr, nc, 3))
-                        visited[nr][nc][3] = True
-
-        elif board[r][c] == '2':
-            # 오른쪽에서 온 경우
-            if d == 2:
-                nr = r + dr[0]
-                nc = c + dc[0]
-                if 0 <= nr < R and 0 <= nc < C:
-                    if nr == z_r and nc == z_c:
-                        return True
-
-                    if board[nr][nc] in ('|', '+', '1', '4') and not visited[nr][nc][0]:
-                        q.append((nr, nc, 0))
-                        visited[nr][nc][0] = True
-
-            # 위쪽에서 온 경우
-            elif d == 1:
-                nr = r + dr[3]
-                nc = c + dc[3]
-                if 0 <= nr < R and 0 <= nc < C:
-                    if nr == z_r and nc == z_c:
-                        return True
-
-                    if board[nr][nc] in ('-', '+', '3', '4') and not visited[nr][nc][3]:
-                        q.append((nr, nc, 3))
-                        visited[nr][nc][3] = True
-
-        elif board[r][c] == '3':
-            # 왼쪽에서 온 경우
-            if d == 3:
-                nr = r + dr[0]
-                nc = c + dc[0]
-                if 0 <= nr < R and 0 <= nc < C:
-                    if nr == z_r and nc == z_c:
-                        return True
-
-                    if board[nr][nc] in ('|', '+', '1', '4') and not visited[nr][nc][0]:
-                        q.append((nr, nc, 0))
-                        visited[nr][nc][0] = True
-
-            # 위쪽에서 온 경우
-            elif d == 1:
-                nr = r + dr[2]
-                nc = c + dc[2]
-                if 0 <= nr < R and 0 <= nc < C:
-                    if nr == z_r and nc == z_c:
-                        return True
-
-                    if board[nr][nc] in ('-', '+', '1', '2') and not visited[nr][nc][2]:
-                        q.append((nr, nc, 2))
-                        visited[nr][nc][2] = True
-
-        elif board[r][c] == '4':
-            # 왼쪽에서 온 경우
-            if d == 3:
-                nr = r + dr[1]
-                nc = c + dc[1]
-                if 0 <= nr < R and 0 <= nc < C:
-                    if nr == z_r and nc == z_c:
-                        return True
-
-                    if board[nr][nc] in ('|', '+', '2', '3') and not visited[nr][nc][1]:
-                        q.append((nr, nc, 1))
-                        visited[nr][nc][1] = True
-
-            # 아래쪽에서 온 경우
-            elif d == 0:
-                nr = r + dr[2]
-                nc = c + dc[2]
-                if 0 <= nr < R and 0 <= nc < C:
-                    if nr == z_r and nc == z_c:
-                        return True
-
-                    if board[nr][nc] in ('-', '+', '1', '2') and not visited[nr][nc][2]:
-                        q.append((nr, nc, 2))
-                        visited[nr][nc][2] = True
+                if board[nr][nc] in ('-', '+', '3', '4') and not visited[nr][nc][d]:
+                    q.append((nr, nc, d))
+                    visited[nr][nc][d] = True
 
     return False
 
@@ -255,4 +124,3 @@ for i in range(R):
                     print(i+1, j+1, pipe[k])
                     exit()
             board[i][j] = '.'
-
