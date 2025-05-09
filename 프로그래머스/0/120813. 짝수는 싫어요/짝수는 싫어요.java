bb52aeec0e3d96f2bdceed98779f9efe838b95d5
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-05-10 01:38:18 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.05 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public ArrayList solution(int n) {
+        ArrayList<Integer> answer = new ArrayList<Integer>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 != 0) {
+                answer.add(i);
+            }
+        }
+        return answer;
+    }
+}
