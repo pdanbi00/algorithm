981a45328a2ab6ae35f3725c8e823638f9ce4c10
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-05-11 23:31:54 +0900

    [Gold IV] Title: 용감한 용사 진수, Time: 180 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/14718. 용감한 용사 진수/용감한 용사 진수.py
+++ 백준/Gold/14718. 용감한 용사 진수/용감한 용사 진수.py
@@ -3,18 +3,26 @@ input = sys.stdin.readline
 N, K = map(int, input().split())
 warrior = [list(map(int, input().split())) for _ in range(N)]
 ans = 1000000 * 3
-# 모든 경우 다 살펴봄
 
+powers = []
+speeds = []
+for i in range(N):
+    powers.append(warrior[i][0])
+    speeds.append(warrior[i][1])
 
-for i in range(N): # power를 몇번째 용사의 값으로 기준을 잡을지
-    for j in range(N): # speed를 몇번째 용사의 값으로 기준을 잡을지
-        for k in range(N): # brain을 몇번째 용사의 값으로 기준을 잡을지
-            cnt = 0
+warrior.sort(key=lambda x : x[2])
 
-            for p in range(N):
-                if warrior[i][0] >= warrior[p][0] and warrior[j][1] >= warrior[p][1] and warrior[k][2] >= warrior[p][2]:
-                    cnt += 1
-                    if cnt == K:
-                        ans = min(ans, warrior[i][0] + warrior[j][1] + warrior[k][2])
+for p in powers:
+    for s in speeds:
+        b = 0
+        cnt = 0
+        for i in range(N):
+            if warrior[i][0] <= p and warrior[i][1] <= s:
+                cnt += 1
+                z = warrior[i][2]
+                if cnt == K:
+                    break
+        if cnt == K:
+            ans = min(ans, p + s + z)
 
 print(ans)
