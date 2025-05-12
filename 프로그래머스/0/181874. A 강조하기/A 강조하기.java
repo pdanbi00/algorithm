73ae6ac96f47cb3c41071692eb94dfe2385e0286
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-05-13 01:24:13 +0900

    [level 0] Title: A 강조하기, Time: 12.27 ms, Memory: 84.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        for (char c : myString.toCharArray()) {
+            if (c == 'a') {
+                answer += 'A';
+            } else if (Character.isUpperCase(c) && c != 'A') {
+                answer += Character.toLowerCase(c);
+            } else {
+                answer += c;
+            }
+        }
+        return answer;
+    }
+}
