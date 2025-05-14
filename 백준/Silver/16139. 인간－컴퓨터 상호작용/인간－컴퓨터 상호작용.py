39180976b24bba92fcdfa7f40e22c3d69de6f965
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-05-14 16:39:41 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 684 ms, Memory: 95976 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -1,18 +1,14 @@
 import sys
 input = sys.stdin.readline
 S = list(input().rstrip())
-cnt_arr = [[0] * 26 for _ in range(len(S))]
-for i in range(len(S)):
-    cnt_arr[i][ord(S[i])-97] += 1
+q = int(input().rstrip())
 
-for i in range(1, len(S)):
-    for j in range(26):
-        cnt_arr[i][j] += cnt_arr[i-1][j]
-q = int(input())
+count = [[0] * 26]
+for i in range(len(S)):
+    count.append(count[len(count)-1][:])
+    count[i+1][ord(S[i])-97] += 1
+
 for _ in range(q):
-    arr = list(input().rstrip().split())
-    target, s, e = arr[0], int(arr[1]), int(arr[2])
-    if s == 0:
-        sys.stdout.write(str(cnt_arr[e][ord(target)-97]) + '\n')
-    else:
-        sys.stdout.write(str(cnt_arr[e][ord(target)-97] - cnt_arr[s-1][ord(target)-97]) + '\n')
+    target, s, e = input().rstrip().split()
+    answer = count[int(e)+1][ord(target)-97] - count[int(s)][ord(target)-97]
+    print(answer)
