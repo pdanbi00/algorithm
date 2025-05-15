efe6c47f05a9d69bbe0264b2b1c2926b9464e8e9
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-05-15 22:31:37 +0900

    [level 2] Title: 전화번호 목록, Time: 377.47 ms, Memory: 98.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
 
 ### 성능 요약
 
-메모리: 224 MB, 시간: 303.09 ms
+메모리: 98.6 MB, 시간: 377.47 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 22일 16:44:22
+2025년 05월 15일 22:31:35
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,21 +1,14 @@
 import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
-        Map<String, Integer> map = new HashMap<>();
-            
-        for (int i = 0; i < phone_book.length; i++) {
-            map.put(phone_book[i], i);
-        }
-        // 앞번호가 뒷번호 접두어인지 확인
-        for (int i = 0; i < phone_book.length; i++){
-            for (int j = 0; j < phone_book[i].length(); j++) {
-                if (map.containsKey(phone_book[i].substring(0, j))) {
-                    return false;
-                }
-                    
+        boolean answer = true;
+        Arrays.sort(phone_book);
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            if (phone_book[i+1].startsWith(phone_book[i])) {
+                return false;
             }
         }
-        
-        return true;
+        return answer;
     }
 }
+
