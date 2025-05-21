4abd35c3fa5c282f299504afbd5209bfff26dd14
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-05-21 16:30:59 +0900

    [level 2] Title: [3차] 압축, Time: 84.41 ms, Memory: 9.35 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.py
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.py
@@ -1,48 +1,16 @@
-# 아이디어 : 종이에 적어서 풀어보면 너는 알고리즘을 구현해낼 수 있음. 오늘의 내가 해냈기 때문에
 def solution(msg):
-    answer = []
-    alpha = {
-        'A': 1,
-        'B': 2,
-        'C': 3,
-        'D': 4,
-        'E': 5,
-        'F': 6,
-        'G': 7,
-        'H': 8,
-        'I': 9,
-        'J': 10,
-        'K': 11,
-        'L': 12,
-        'M': 13,
-        'N': 14,
-        'O': 15,
-        'P': 16,
-        'Q': 17,
-        'R': 18,
-        'S': 19,
-        'T': 20,
-        'U': 21,
-        'V': 22,
-        'W': 23,
-        'X': 24,
-        'Y': 25,
-        'Z': 26
-    }
-    cnt = 27
-    idx = 0
-    word = ''
-    while idx < len(msg):
-        word += msg[idx]
-        if word in alpha.keys():
-            idx += 1
-
-        else:
-            answer.append(alpha[word[:-1]])
-            alpha[word] = cnt
-            cnt += 1
-            word = ''
-
-    answer.append(alpha[word])
-
+    dic={}
+    for i in range(1,27):
+        dic[chr(i+64)]=i
+    i=0
+    answer=[]
+    while 1:
+        if i >=len(msg):
+            break
+        for j in range(len(msg),i,-1):
+            if msg[i:j] in dic:
+                answer.append(dic[msg[i:j]])
+                dic[msg[i:j+1]] = len(dic)+1
+                i=j-1
+        i+=1
     return answer
