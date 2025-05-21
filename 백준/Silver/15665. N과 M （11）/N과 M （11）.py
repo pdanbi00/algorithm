afcae29d328eb85032c4c2882411e689f396663a
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-05-21 17:39:46 +0900

    [Silver II] Title: N과 M (11), Time: 692 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15665. N과 M （11）/N과 M （11）.py
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.py
@@ -1,23 +1,20 @@
 N, M = map(int, input().split())
 nums = list(map(int, input().split()))
-# nums.sort()
+nums.sort()
 
-answer = set()
+def func(deepth):
+    prev_num = -1
 
-def func(idx, temp):
-    if idx == M:
-        answer.add(tuple(temp))
+    if deepth == M:
+        print(*ans)
         return
 
     for i in range(N):
-        temp[idx] = nums[i]
-        func(idx+1, temp)
-        temp[idx] = -1
+        if prev_num != nums[i]:
+            ans.append(nums[i])
+            prev_num = nums[i]
+            func(deepth+1)
+            ans.pop()
 
-temp = [-1] * M
-func(0, temp)
-
-answer = list(answer)
-answer.sort()
-for i in range(len(answer)):
-    print(*answer[i])
+ans = []
+func(0)
