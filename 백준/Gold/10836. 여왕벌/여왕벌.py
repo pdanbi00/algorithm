91e3694b2841dde4f34e3a266f3c1b977393024b
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-06-07 16:09:45 +0900

    [Gold III] Title: 여왕벌, Time: 3720 ms, Memory: 116360 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/10836. 여왕벌/여왕벌.py
+++ 백준/Gold/10836. 여왕벌/여왕벌.py
@@ -1,38 +1,34 @@
+import sys
+input = sys.stdin.readline
 M, N = map(int, input().split())
 board = [[1] * M for _ in range(M)]
+glow = [0] * (2 * M - 1) # 성장하는 정도
 
 for _ in range(N):
-    cnt = list(map(int, input().split()))
-    big = [0] * cnt[0] + [1] * cnt[1] + [2] * cnt[2]
-    visited = [[0] * M for  _ in range(M)]
-    idx = 0
-    r = M-1
-    c = 0
-    while idx < len(big):
-        if r > 0:
-            visited[r][c] = big[idx]
-            idx += 1
-            r -= 1
-        else:
-            visited[r][c] = big[idx]
-            c += 1
-            idx += 1
-    dr = [0, -1, -1]
-    dc = [-1, -1, 0]
+    zero, one, two = list(map(int, input().split()))
+    for i in range(zero, zero + one):
+        glow[i] += 1
 
-    for i in range(1, M):
-        for j in range(1, M):
-            for k in range(3):
-                nr = i + dr[k]
-                nc = j + dc[k]
-                if 0 <= nr < M and 0 <= nc < M:
-                    visited[i][j] = max(visited[i][j], visited[nr][nc])
-    # for i in range(M):
-    #     print(*visited[i])
-    # print('--------------')
-    for i in range(M):
-        for j in range(M):
-            board[i][j] += visited[i][j]
+    for i in range(zero + one, 2 * M - 1):
+        glow[i] += 2
+
+
+idx = 0
+# 제일 왼쪽 열부터 채우기
+for i in range(M-1, 0, -1):
+    board[i][0] += glow[idx]
+    idx += 1
+
+# 제일 위쪽 행 채우기
+for j in range(M):
+    board[0][j] += glow[idx]
+    idx += 1
+
+# 나머지 칸 채우기
+# 항상 자기 자신 칸 위 칸이 최대 성장임
+for i in range(1, M):
+    for j in range(1, M):
+        board[i][j] = board[i-1][j]
 
 for i in range(M):
     print(*board[i])
