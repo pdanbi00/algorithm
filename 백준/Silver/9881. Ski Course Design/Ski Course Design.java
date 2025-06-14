b0db5d9459c8ec586e39e6d1a3e0c182a239dc32
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-06-14 15:05:39 +0900

    [Silver V] Title: Ski Course Design, Time: 116 ms, Memory: 14404 KB -BaekjoonHub

---
+++ 백준/Silver/9881. Ski Course Design/Ski Course Design.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int N;
+    static int[] hills;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        hills = new int[N];
+        int max = -1;
+        int min = 101;
+        for (int i = 0; i < N; i++) {
+            hills[i] = Integer.parseInt(br.readLine());
+            if (hills[i] > max) {
+                max = hills[i];
+            }
+
+            if (hills[i] < min) {
+                min = hills[i];
+            }
+        }
+
+        int answer = Integer.MAX_VALUE;
+        for (int tmp = min; tmp < max-17; tmp++) {
+            int result = 0;
+            for (int i = 0; i < N; i++) {
+                if (hills[i] < tmp) {
+                    result += Math.pow((tmp - hills[i]), 2);
+                } else if (hills[i] > tmp + 17) {
+                    result += Math.pow((hills[i] - (tmp + 17)), 2);
+                }
+            }
+            if (answer > result) {
+                answer = result;
+            }
+        }
+        if (answer == Integer.MAX_VALUE) {
+            System.out.println(0);
+        } else {
+            System.out.println(answer);
+        }
+
+
+
+    }
+}
