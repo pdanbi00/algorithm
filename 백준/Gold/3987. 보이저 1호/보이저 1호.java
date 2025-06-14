ba593420864ef7b74cd15a1054d10b9e9eda0beb
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-06-14 20:35:14 +0900

    [Gold V] Title: 보이저 1호, Time: 340 ms, Memory: 67168 KB -BaekjoonHub

--- 백준/Gold/3987. 보이저 1호/보이저 1호.java
+++ 백준/Gold/3987. 보이저 1호/보이저 1호.java
@@ -7,6 +7,9 @@ public class Main {
     static int PR, PC;
     static int[] dr = {-1, 0, 1, 0};
     static int[] dc = {0, 1, 0, -1};
+    static int[] P = {1, 0, 3, 2};
+    static int[] Q = {3, 2, 1, 0};
+    static char[] dirs = {'U', 'R', 'D', 'L'};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -37,15 +40,7 @@ public class Main {
                 idx = i;
             }
         }
-        if (idx == 0) {
-            System.out.println('U');
-        } else if (idx == 1) {
-            System.out.println('R');
-        } else if (idx == 2) {
-            System.out.println('D');
-        } else {
-            System.out.println('L');
-        }
+        System.out.println(dirs[idx]);
 
         if (max == Integer.MAX_VALUE) {
             System.out.println("Voyager");
@@ -69,9 +64,7 @@ public class Main {
     static int func(int x, int y, int d) {
         int cnt = 0;
         Queue<Point> q = new LinkedList<>();
-        boolean[][][] visited = new boolean[N][M][4];
         q.add(new Point(x, y, d));
-        visited[x][y][d] = true;
 
         while (!q.isEmpty()) {
             cnt++;
@@ -80,37 +73,17 @@ public class Main {
             int nc = cur.c + dc[cur.d];
 
             if (0 <= nr && nr < N && 0 <= nc && nc < M) {
-                if (!visited[nr][nc][cur.d]) {
-                    if (board[nr][nc] == '.') {
-                        q.add(new Point(nr, nc, cur.d));
-                        visited[nr][nc][cur.d] = true;
-                    } else if (board[nr][nc] == 'C') {
-                        return cnt;
-                    } else if (board[nr][nc] == '/') {
-                        visited[nr][nc][cur.d] = true;
-                        if (cur.d == 0) {
-                            q.add(new Point(nr, nc, 1));
-                        } else if (cur.d == 1) {
-                            q.add(new Point(nr, nc, 0));
-                        } else if (cur.d == 2) {
-                            q.add(new Point(nr, nc, 3));
-                        } else if (cur.d == 3) {
-                            q.add(new Point(nr, nc, 2));
-                        }
-                    } else if (board[nr][nc] == '\\') {
-                        visited[nr][nc][cur.d] = true;
-                        if (cur.d == 0) {
-                            q.add(new Point(nr, nc, 3));
-                        } else if (cur.d == 1) {
-                            q.add(new Point(nr, nc, 2));
-                        } else if (cur.d == 2) {
-                            q.add(new Point(nr, nc, 1));
-                        } else if (cur.d == 3) {
-                            q.add(new Point(nr, nc, 0));
-                        }
-                    }
-                } else {
+                if (nr == x && nc == y && cur.d == d) {
                     return Integer.MAX_VALUE;
+                }
+                if (board[nr][nc] == '.') {
+                    q.add(new Point(nr, nc, cur.d));
+                } else if (board[nr][nc] == 'C') {
+                    return cnt;
+                } else if (board[nr][nc] == '/') {
+                    q.add(new Point(nr, nc, P[cur.d]));
+                } else if (board[nr][nc] == '\\') {
+                    q.add(new Point(nr, nc, Q[cur.d]));
                 }
             }
         }
