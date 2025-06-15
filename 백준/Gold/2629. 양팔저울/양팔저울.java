0daba42d73c9cbc6dde899698c8af6e811ba23e0
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-06-16 01:00:06 +0900

    [Gold III] Title: 양팔저울, Time: 108 ms, Memory: 15576 KB -BaekjoonHub

--- 백준/Gold/2629. 양팔저울/양팔저울.java
+++ 백준/Gold/2629. 양팔저울/양팔저울.java
@@ -1,57 +1,59 @@
 import java.util.*;
 import java.io.*;
 public class Main {
+    static int chu_cnt;
+    static int[] chu_list;
+    static boolean[][] dp;
     public static void main(String[] args) throws IOException{
+        // dp[i][j] : i개의 추를 사용해서 j라는 무게를 만듦
+        // 즉, dp[사용한 추의 개수][양쪽 저울의 추 무게의 차이]
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[] chu = new int[N];
+        chu_cnt = Integer.parseInt(br.readLine());
+        chu_list = new int[chu_cnt];
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            chu[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < chu_cnt; i++) {
+            chu_list[i] = Integer.parseInt(st.nextToken());
         }
 
-        boolean[] dp = new boolean[15001];
-        for (int c : chu) {
-            List<Integer> arr = new ArrayList<>();
-            for (int i = 0; i < 15001; i++) {
-                if (dp[i]) {
-                    arr.add(i);
-                }
-            }
 
-            for (int i = 0; i < arr.size(); i++) {
-                if (arr.get(i) - c >= 0) {
-//                    System.out.println(c + " " + (arr.get(i) - c));
-                    dp[arr.get(i) - c] = true;
-                }
+        dp = new boolean[chu_cnt+1][40001];
 
-                if (c - arr.get(i) >= 0) {
-//                    System.out.println(c + " " + (c - arr.get(i)));
-                    dp[c - arr.get(i)] = true;
-                }
+        calc(0, 0);
 
-                if (arr.get(i) + c <= 15000) {
-//                    System.out.println(c + " " + (c + arr.get(i)));
-                    dp[arr.get(i) + c] = true;
-                }
-            }
+        int bead_cnt = Integer.parseInt(br.readLine());
 
-            dp[c] = true;
-        }
-
-        int goosle = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < goosle; i++) {
-            long num = Long.parseLong(st.nextToken());
-            if (num <= 15000) {
-                if (dp[(int) num] == true) {
-                    System.out.print("Y ");
-                } else {
-                    System.out.print("N ");
-                }
+        for (int i = 0; i < bead_cnt; i++) {
+            int bead = Integer.parseInt(st.nextToken());
+            if (dp[chu_cnt][bead]) {
+                System.out.print("Y ");
             } else {
                 System.out.print("N ");
             }
         }
+
     }
+
+    static void calc(int cnt, int weight) {
+        if (dp[cnt][weight]) {
+            return;
+        }
+
+        dp[cnt][weight] = true;
+
+        if (cnt == chu_cnt) {
+            return;
+
+        }
+
+        // 추를 저울 오른쪽에 올리는 경우
+        calc(cnt+1, weight + chu_list[cnt]);
+        // 추를 저울 왼쪽에 올리는 경우
+        calc(cnt+1, Math.abs(weight - chu_list[cnt]));
+        // 추를 안 올리는 경우
+        calc(cnt+1, weight);
+    }
+
 }
