f4fb0c7129bae80dcbc4e40891168182218d6732
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-07-01 22:59:29 +0900

    [Gold V] Title: 괄호의 값, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.py
@@ -0,0 +1,33 @@
+arr = list(input())
+stack = []
+answer = 0
+tmp = 1
+for i in range(len(arr)):
+    if arr[i] == '(':
+        stack.append('(')
+        tmp *= 2
+
+    elif arr[i] == '[':
+        stack.append('[')
+        tmp *= 3
+    elif arr[i] == ')':
+        if not stack or stack[-1] != '(':
+            answer = 0
+            break
+        if arr[i-1] == '(':
+            answer += tmp
+        stack.pop()
+        tmp //= 2
+    else:
+        if not stack or stack[-1] != '[':
+            answer = 0
+            break
+        if arr[i-1] == '[':
+            answer += tmp
+        stack.pop()
+        tmp //= 3
+
+if stack:
+    answer = 0
+
+print(answer)
