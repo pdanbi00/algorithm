daacf2d4f69787dfb792bba470857cd66041fe73
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-07-02 18:09:20 +0900

    [Gold II] Title: 뉴스 전하기, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Gold/1135. 뉴스 전하기/뉴스 전하기.py
+++ 백준/Gold/1135. 뉴스 전하기/뉴스 전하기.py
@@ -1,20 +1,26 @@
 N = int(input())
-parent_list = list(map(int, input().split()))
-child_list = [[] for _ in range(N)]
-
+relation = list(map(int, input().split()))
+graph = [[] for _ in range(N)]
 for i in range(1, N):
-    child_list[parent_list[i]].append(i)
+    graph[relation[i]].append(i)
+
+dp = [0] * N
+
 def dfs(node):
-    if not child_list[node]:
-        return 0
+    node_to_sub = []
+    for sub in graph[node]:
+        dfs(sub)
+        node_to_sub.append(dp[sub])
 
-    result = []
-    for child in child_list[node]:
-        result.append(dfs(child))
-    result.sort(reverse=True)
-    new_result = []
-    for i in range(len(child_list[node])):
-        new_result.append(result[i] + i + 1)
-    return max(new_result)
-
-print(dfs(0))
+    # 만약 부하직원이 존재한다면
+    if node_to_sub:
+        # 걸리는 시간을 내림차순으로 정렬. 오래 걸리는 곳을 먼저 주기 위해서
+        node_to_sub.sort(reverse=True)
+        choose_large_time = []
+        for i in range(len(node_to_sub)):
+            # node_to_sub[i] : 직속 부하직원에서부터 말단 직원까지 뉴스가 도달하는 데에 걸린 시간
+            # i : 뉴스를 전달받는 부하 직원 순서. 뉴스 전달 받기까지 걸리는 대기시간
+            choose_large_time.append(node_to_sub[i] + i + 1)
+        dp[node] = max(choose_large_time)
+dfs(0)
+print(dp[0])
