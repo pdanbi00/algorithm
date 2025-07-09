73ab49b415167db58a29510a24182430caa49568
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-07-09 18:56:37 +0900

    [Gold I] Title: 2048 (Easy), Time: 156 ms, Memory: 20772 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.java
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.java
@@ -18,244 +18,169 @@ public class Main {
         }
         ans = 0;
 
-        dfs(N, board, 0);
+        game(0);
         System.out.println(ans);
     }
 
-    static int[][] move_up(int N, int[][] board) {
-        int[][] new_board = new int[N][N];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                new_board[i][j] = board[i][j];
-            }
-        }
-
-        for (int i = 1; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (new_board[i][j] != 0) {
-                    int idx = i-1;
-                    while (idx >= 0) {
-                        if (new_board[idx][j] == 0) {
-                            idx--;
-                        } else {
-                            break;
-                        }
-                    }
-                    if (new_board[idx+1][j] == 0) {
-                        new_board[idx+1][j] = new_board[i][j];
-                        new_board[i][j] = 0;
-                    }
-                }
-            }
-        }
-        return new_board;
-    }
-
-    static int[][] move_down(int N, int[][] board) {
-        int[][] new_board = new int[N][N];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                new_board[i][j] = board[i][j];
-            }
-        }
-
-        for (int i = N-2; i >= 0; i--) {
-            for (int j = 0; j < N; j++) {
-                if (new_board[i][j] != 0) {
-                    int idx = i+1;
-                    while (idx < N) {
-                        if (new_board[idx][j] == 0) {
-                            idx++;
-                        } else {
-                            break;
-                        }
-                    }
-                    if (new_board[idx-1][j] == 0) {
-                        new_board[idx-1][j] = new_board[i][j];
-                        new_board[i][j] = 0;
-                    }
-                }
-            }
-        }
-        return new_board;
-    }
-
-    static int[][] move_right(int N, int[][] board) {
-        int[][] new_board = new int[N][N];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                new_board[i][j] = board[i][j];
-            }
-        }
-
-        for (int j = N-2; j >= 0; j--) {
-            for (int i = 0; i < N; i++) {
-                if (new_board[i][j] != 0) {
-                    int idx = j+1;
-                    while (idx < N) {
-                        if (new_board[i][idx] == 0) {
-                            idx++;
-                        } else {
-                            break;
-                        }
-                    }
-                    if (new_board[i][idx-1] == 0) {
-                        new_board[i][idx-1] = new_board[i][j];
-                        new_board[i][j] = 0;
-                    }
-                }
-            }
-        }
-        return new_board;
-    }
-
-    static int[][] move_left(int N, int[][] board) {
-        int[][] new_board = new int[N][N];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                new_board[i][j] = board[i][j];
-            }
-        }
-
-        for (int j = 1; j < N; j++) {
-            for (int i = 0; i < N; i++) {
-                if (new_board[i][j] != 0) {
-                    int idx = j-1;
-                    while (idx >= 0) {
-                        if (new_board[i][idx] == 0) {
-                            idx--;
-                        } else {
-                            break;
-                        }
-                    }
-                    if (new_board[i][idx+1] == 0) {
-                        new_board[i][idx+1] = new_board[i][j];
-                        new_board[i][j] = 0;
-                    }
-                }
-            }
-        }
-        return new_board;
-    }
-
-    static int[][] add_up(int N, int[][] arr) {
-        for (int j = 0; j < N; j++) {
-            int idx = 0;
-            while (idx < N-1) {
-                if (arr[idx][j] != 0 && arr[idx][j] == arr[idx+1][j]) {
-                    arr[idx][j] = arr[idx][j] * 2;
-                    arr[idx+1][j] = 0;
-                    idx += 2;
-                } else {
-                    idx += 1;
-                }
-            }
-        }
-        return arr;
-    }
-
-    static int[][] add_down(int N, int[][] arr) {
-        for (int j = 0; j < N; j++) {
-            int idx = N-1;
-            while (idx > 0) {
-                if (arr[idx][j] != 0 && arr[idx][j] == arr[idx-1][j]) {
-                    arr[idx][j] = arr[idx][j] * 2;
-                    arr[idx-1][j] = 0;
-                    idx -= 2;
-                } else {
-                    idx -= 1;
-                }
-            }
-        }
-        return arr;
-    }
-
-    static int[][] add_right(int N, int[][] arr) {
-        for (int i = 0; i < N; i++) {
-            int idx = N-1;
-            while (idx > 0) {
-                if (arr[i][idx] != 0 && arr[i][idx] == arr[i][idx-1]) {
-                    arr[i][idx] = arr[i][idx] * 2;
-                    arr[i][idx-1] = 0;
-                    idx -= 2;
-                } else {
-                    idx -= 1;
-                }
-            }
-        }
-        return arr;
-    }
-
-    static int[][] add_left(int N, int[][] arr) {
-        for (int i = 0; i < N; i++) {
-            int idx = 0;
-            while (idx < N-1) {
-                if (arr[i][idx] != 0 && arr[i][idx] == arr[i][idx+1]) {
-                    arr[i][idx] = arr[i][idx] * 2;
-                    arr[i][idx+1] = 0;
-                    idx += 2;
-                } else {
-                    idx += 1;
-                }
-            }
-        }
-        return arr;
-    }
-
-    static int[][] up(int N, int[][]board) {
-        int[][] arr = new int[N][N];
-        arr = move_up(N, board);
-        arr = add_up(N, arr);
-        arr = move_up(N, arr);
-        return arr;
-    }
-
-    static int[][] down(int N, int[][]board) {
-        int[][] arr = new int[N][N];
-        arr = move_down(N, board);
-        arr = add_down(N, arr);
-        arr = move_down(N, arr);
-        return arr;
-    }
-
-    static int[][] right(int N, int[][]board) {
-        int[][] arr = new int[N][N];
-        arr = move_right(N, board);
-        arr = add_right(N, arr);
-        arr = move_right(N, arr);
-        return arr;
-    }
-
-    static int[][] left(int N, int[][]board) {
-        int[][] arr = new int[N][N];
-        arr = move_left(N, board);
-        arr = add_left(N, arr);
-        arr = move_left(N, arr);
-        return arr;
-    }
-
-    static void dfs(int N, int[][] board, int cnt) {
+    public static void game(int cnt) {
         if (cnt == 5) {
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    ans = Math.max(ans, board[i][j]);
-                }
-            }
+            findMax();
             return;
         }
+        int[][] copy = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            copy[i] = board[i].clone();
+        }
+        for (int i = 0; i < 4; i++) {
+            move(i);
+            game(cnt+1);
+            for (int a = 0; a < N; a++) {
+                board[a] = copy[a].clone();
+            }
+        }
+    }
 
-        int[][] new_arr = new int[N][N];
+    public static void move(int dir) {
+        switch (dir) {
+            // 위로 밀기
+            case 0:
+                for (int j = 0; j < N; j++) {
+                    int idx = 0; // 값 넣을 위치
+                    int block = 0; // 최근 블록 값
+                    for (int i = 0; i < N; i++) {
+                        // 현재 블록의 값이 0이 아니라면
+                        if (board[i][j] != 0) {
+                            // 최근 블록 값이랑 현재 블록 값이 같다면
+                            if (board[i][j] == block) {
+                                // 블록 합쳐짐
+                                board[idx-1][j] = block * 2;
+                                // 블록 합쳐졌으니깐 0으로 갱신
+                                block = 0;
+                                // 값 합쳤으니깐 현재 블록 값은 0
+                                board[i][j] = 0;
 
-        new_arr = up(N, board);
-        dfs(N, new_arr, cnt+1);
+                            }
+                            // 최근 블록 값이랑 현재 블록 값이 다르면
+                            else {
+                                // 블록 변수 값을 현재 블록 값으로 갱신
+                                block = board[i][j];
+                                // 현재 블록 값을 0으로 바꿔줌.
+                                board[i][j] = 0;
+                                // 값 넣을 위치에 최근 블록 값 넣기
+                                board[idx][j] = block;
+                                // 값 넣을 위치 변경
+                                idx++;
+                            }
+                        }
+                    }
+                }
+                break;
+            // 아래로 밀기
+            case 1:
+                for (int j = 0; j < N; j++) {
+                    int idx = N-1; // 값 넣을 위치
+                    int block = 0; // 최근 블록 값
+                    for (int i = N-1; i >= 0; i--) {
+                        // 현재 블록의 값이 0이 아니라면
+                        if (board[i][j] != 0) {
+                            // 최근 블록 값이랑 현재 블록 값이 같다면
+                            if (board[i][j] == block) {
+                                // 블록 합쳐짐
+                                board[idx+1][j] = block * 2;
+                                // 블록 합쳐졌으니깐 0으로 갱신
+                                block = 0;
+                                // 값 합쳤으니깐 현재 블록 값은 0
+                                board[i][j] = 0;
 
-        new_arr = down(N, board);
-        dfs(N, new_arr, cnt+1);
+                            }
+                            // 최근 블록 값이랑 현재 블록 값이 다르면
+                            else {
+                                // 블록 변수 값을 현재 블록 값으로 갱신
+                                block = board[i][j];
+                                // 현재 블록 값을 0으로 바꿔줌.
+                                board[i][j] = 0;
+                                // 값 넣을 위치에 최근 블록 값 넣기
+                                board[idx][j] = block;
+                                // 값 넣을 위치 변경
+                                idx--;
+                            }
+                        }
+                    }
+                }
+                break;
+            // 왼쪽으로 밀기
+            case 2:
+                for (int i = 0; i < N; i++) {
+                    int idx = 0; // 값 넣을 위치
+                    int block = 0; // 최근 블록 값
+                    for (int j = 0; j < N; j++) {
+                        // 현재 블록의 값이 0이 아니라면
+                        if (board[i][j] != 0) {
+                            // 최근 블록 값이랑 현재 블록 값이 같다면
+                            if (board[i][j] == block) {
+                                // 블록 합쳐짐
+                                board[i][idx-1] = block * 2;
+                                // 블록 합쳐졌으니깐 0으로 갱신
+                                block = 0;
+                                // 값 합쳤으니깐 현재 블록 값은 0
+                                board[i][j] = 0;
 
-        new_arr = right(N, board);
-        dfs(N, new_arr, cnt+1);
+                            }
+                            // 최근 블록 값이랑 현재 블록 값이 다르면
+                            else {
+                                // 블록 변수 값을 현재 블록 값으로 갱신
+                                block = board[i][j];
+                                // 현재 블록 값을 0으로 바꿔줌.
+                                board[i][j] = 0;
+                                // 값 넣을 위치에 최근 블록 값 넣기
+                                board[i][idx] = block;
+                                // 값 넣을 위치 변경
+                                idx++;
+                            }
+                        }
+                    }
+                }
+                break;
+            // 오른쪽으로 밀기
+            case 3:
+                for (int i = 0; i < N; i++) {
+                    int idx = N-1; // 값 넣을 위치
+                    int block = 0; // 최근 블록 값
+                    for (int j = N-1; j >= 0; j--) {
+                        // 현재 블록의 값이 0이 아니라면
+                        if (board[i][j] != 0) {
+                            // 최근 블록 값이랑 현재 블록 값이 같다면
+                            if (board[i][j] == block) {
+                                // 블록 합쳐짐
+                                board[i][idx+1] = block * 2;
+                                // 블록 합쳐졌으니깐 0으로 갱신
+                                block = 0;
+                                // 값 합쳤으니깐 현재 블록 값은 0
+                                board[i][j] = 0;
 
-        new_arr = left(N, board);
-        dfs(N, new_arr, cnt+1);
+                            }
+                            // 최근 블록 값이랑 현재 블록 값이 다르면
+                            else {
+                                // 블록 변수 값을 현재 블록 값으로 갱신
+                                block = board[i][j];
+                                // 현재 블록 값을 0으로 바꿔줌.
+                                board[i][j] = 0;
+                                // 값 넣을 위치에 최근 블록 값 넣기
+                                board[i][idx] = block;
+                                // 값 넣을 위치 변경
+                                idx--;
+                            }
+                        }
+                    }
+                }
+                break;
+        }
+    }
+    public static void findMax() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                ans = Math.max(ans, board[i][j]);
+            }
+        }
     }
 }
