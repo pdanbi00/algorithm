97259684a0e2a4d46baee9fb17156bb86c60ddca
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-07-14 18:54:03 +0900

    [Gold V] Title: 경쟁적 전염, Time: 152 ms, Memory: 35508 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
@@ -1,40 +1,36 @@
-from collections import deque
+from heapq import heappush, heappop
 import sys
 input = sys.stdin.readline
 N, K = map(int, input().split())
 board = []
-virus_dict = dict()
+heap = []
+
 for i in range(N):
     arr = list(map(int, input().split()))
     for j in range(N):
         if arr[j] != 0:
-            if arr[j] in virus_dict:
-                virus_dict[arr[j]].append((i, j))
-            else:
-                virus_dict[arr[j]] = [(i, j)]
+            heappush(heap, (arr[j], i, j))
     board.append(arr)
+
+dr = [-1, 1, 0, 0]
+dc = [0, 0, -1, 1]
 
 S, X, Y = map(int, input().split())
 
-time = 0
-dr = [-1, 1, 0, 0]
-dc = [0, 0, -1, 1]
-while time < S:
-    q = deque()
-    for i in range(1, K+1):
-        if i in virus_dict:
-            for r, c in virus_dict[i]:
-                q.append((r, c, i))
-            virus_dict[i] = []
-    while q:
-        r, c, num = q.popleft()
+for _ in range(S):
+    next_virus = []
+    while heap:
+        now = heappop(heap)
+        n, r, c = now[0], now[1], now[2]
         for k in range(4):
             nr = r + dr[k]
             nc = c + dc[k]
             if 0 <= nr < N and 0 <= nc < N:
                 if board[nr][nc] == 0:
-                    board[nr][nc] = num
-                    virus_dict[num].append((nr, nc))
-    time += 1
+                    board[nr][nc] = n
+                    next_virus.append((n, nr, nc))
+
+    for vir in next_virus:
+        heappush(heap, vir)
 
 print(board[X-1][Y-1])
