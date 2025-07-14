443dfed4d279baf3801f1e9840ddc8cb1784e615
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-07-14 19:07:51 +0900

    [Gold V] Title: 경쟁적 전염, Time: 296 ms, Memory: 22636 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.java
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.java
@@ -1,12 +1,11 @@
 import java.io.*;
-import java.util.LinkedList;
 import java.util.StringTokenizer;
 import java.util.ArrayList;
-import java.util.Queue;
+import java.util.PriorityQueue;
 public class Main {
     static int N, K;
     static int[][] board;
-    static ArrayList<ArrayList> virusArr;
+    static PriorityQueue<Virus> virusQueue;
     static int[] dr = {-1, 1, 0, 0};
     static int[] dc = {0, 0, -1, 1};
 
@@ -16,18 +15,14 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
         board = new int[N][N];
-        virusArr = new ArrayList<>();
-        for (int i = 0; i <= K; i++) {
-            ArrayList<Virus> arr = new ArrayList<>();
-            virusArr.add(arr);
-        }
+        virusQueue = new PriorityQueue<>();
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
                 int n = Integer.parseInt(st.nextToken());
                 board[i][j] = n;
                 if (n != 0) {
-                    virusArr.get(n).add(new Virus(i, j, n));
+                    virusQueue.add(new Virus(n, i, j));
                 }
             }
         }
@@ -39,39 +34,43 @@ public class Main {
 
         int time = 0;
         while (time < S) {
-            Queue<Virus> q = new LinkedList<>();
-            for (int i = 1; i <= K; i++) {
-                ArrayList<Virus> vArr = virusArr.get(i);
-                for (int j = 0; j < vArr.size(); j++) {
-                    q.offer(vArr.get(j));
-                }
-                virusArr.get(i).clear();
-            }
-            while (!q.isEmpty()) {
-                Virus cur = q.poll();
+            ArrayList<Virus> arr = new ArrayList<>();
+            while (!virusQueue.isEmpty()) {
+                Virus cur = virusQueue.poll();
                 for (int k = 0; k < 4; k++) {
                     int nr = cur.r + dr[k];
                     int nc = cur.c + dc[k];
                     if (0 <= nr && nr < N && 0 <= nc && nc < N) {
                         if (board[nr][nc] == 0) {
                             board[nr][nc] = cur.num;
-                            virusArr.get(cur.num).add(new Virus(nr, nc, cur.num));
+                            arr.add(new Virus(cur.num, nr, nc));
                         }
                     }
                 }
             }
+
+            for (Virus vir : arr) {
+                virusQueue.offer(vir);
+            }
+
             time++;
         }
         System.out.println(board[X-1][Y-1]);
     }
 }
-class Virus {
+class Virus implements Comparable<Virus> {
     int r;
     int c;
     int num;
-    public Virus(int r, int c, int num) {
+    public Virus(int num, int r, int c) {
+        this.num = num;
         this.r = r;
         this.c = c;
-        this.num = num;
+
+    }
+
+    @Override
+    public int compareTo(Virus a) {
+        return this.num - a.num;
     }
 }
