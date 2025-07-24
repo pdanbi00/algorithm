b132374a863b8e1b905167efc7bd6cef99e2dff7
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-07-24 16:21:15 +0900

    [Silver III] Title: 반복하지 않는 수, Time: 2016 ms, Memory: 186788 KB -BaekjoonHub

---
+++ 백준/Silver/7696. 반복하지 않는 수/반복하지 않는 수.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+num = 1
+nums = []
+
+def duplication(num):
+    check = [False] * 10
+    while num > 0:
+        tmp = num % 10
+        if check[tmp]:
+            return True
+        check[tmp] = True
+        num //= 10
+    return False
+
+while len(nums) < 1000000:
+    if duplication(num) == False:
+        nums.append(num)
+    num += 1
+
+while True:
+    N = int(input())
+    if N == 0:
+        break
+    print(nums[N-1])
