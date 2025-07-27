70d02491b55d44770e8bf07672b8a1c347307c4f
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-07-28 01:22:06 +0900

    [Silver III] Title: 반전 요세푸스, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/20301. 반전 요세푸스/반전 요세푸스.py
+++ 백준/Silver/20301. 반전 요세푸스/반전 요세푸스.py
@@ -1,37 +1,17 @@
-from collections import deque
 N, K, M = map(int, input().split())
-q = deque()
-for i in range(1, N+1):
-    q.append(i)
+arr = [i for i in range(1, N+1)]
 
-cnt = 0
-while cnt < K-1:
-    tmp = q.popleft()
-    q.append(tmp)
-    cnt += 1
-
-cnt = 0
+idx = 0
 dir = 0
-answer = []
-while q:
-    tmp = q.popleft()
-    answer.append(tmp)
+cnt = 0
+
+while arr:
+    if dir % 2 == 0:
+        idx = (idx + K-1) % len(arr)
+    else:
+        idx = (idx + (len(arr) - K)) % len(arr)
+
+    print(arr.pop(idx))
     cnt += 1
     if cnt % M == 0:
         dir += 1
-
-    if dir % 2 == 0: # 오른쪽 방향
-        idx = 0
-        while idx < K-1 and q:
-            tmp = q.popleft()
-            q.append(tmp)
-            idx += 1
-    else: # 왼쪽 방향
-        idx = 0
-        while idx < K and q:
-            tmp = q.pop()
-            q.appendleft(tmp)
-            idx += 1
-            
-for i in range(N):
-    print(answer[i])
