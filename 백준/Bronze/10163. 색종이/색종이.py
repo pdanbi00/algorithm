f3003fb4a5402da1275a0074d45fd19853b55c9d
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-08-07 22:55:14 +0900

    [Bronze I] Title: 색종이, Time: 1288 ms, Memory: 39328 KB, Score: 53 point -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -1,18 +1,19 @@
-import sys
-input = sys.stdin.readline
 N = int(input())
 board = [[0] * 1001 for _ in range(1001)]
-
-answer = [0] * (N+1)
 
 for num in range(1, N+1):
     c, r, w, h = map(int, input().split())
     for i in range(h):
         for j in range(w):
-            if board[1000-r-i][c+j] != 0:
-                answer[board[1000-r-i][c+j]] -= 1
-            answer[num] += 1
             board[1000-r-i][c+j] = num
+
+answer = [0] * (N+1)
+for i in range(1001):
+    for j in range(1001):
+        for k in range(1, N+1):
+            if board[i][j] == k:
+                answer[k] += 1
+                break
 
 for i in range(1, N+1):
     print(answer[i])
