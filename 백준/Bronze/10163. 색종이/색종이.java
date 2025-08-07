25ec58e88a01203b713e09b3c08654963613fab9
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-08-07 22:52:11 +0900

    [Bronze I] Title: 색종이, Time: 208 ms, Memory: 20240 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.java
+++ 백준/Bronze/10163. 색종이/색종이.java
@@ -5,40 +5,39 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = null;
         int N = Integer.parseInt(br.readLine());
-        int r = 0;
-        int c = 0;
+        int x = 0;
+        int y = 0;
         int w = 0;
         int h = 0;
         int[][] board = new int[1001][1001];
 
+        int[] answer = new int[N+1];
+
         for (int n = 1; n <= N; n++) {
             st = new StringTokenizer(br.readLine());
-            c = Integer.parseInt(st.nextToken());
-            r = Integer.parseInt(st.nextToken());
+            x = Integer.parseInt(st.nextToken());
+            y = Integer.parseInt(st.nextToken());
             w = Integer.parseInt(st.nextToken());
             h = Integer.parseInt(st.nextToken());
 
-            for (int i = 0; i < h; i++) {
-                for (int j = 0; j < w; j++) {
-                    board[100-r-i][c+j] = n;
+            for (int i = x; i < x + w; i++) {
+                for (int j = y; j < y + h; j++) {
+                    board[i][j] = n;
                 }
             }
         }
-
-        int[] answer = new int[N+1];
 
         for (int i = 0; i < 1001; i++) {
             for (int j = 0; j < 1001; j++) {
-                for (int k = 1; k <= N; k++) {
-                    if (board[i][j] == k) {
-                        answer[k]++;
-                    }
-                }
+                answer[board[i][j]]++;
             }
         }
 
+        StringBuilder sb = new StringBuilder();
+
         for (int i = 1; i <= N; i++) {
-            System.out.println(answer[i]);
+            sb.append(answer[i]).append("\n");
         }
+        System.out.println(sb);
     }
 }
