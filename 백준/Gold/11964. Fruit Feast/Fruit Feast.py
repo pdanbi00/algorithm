4088a27dee90f87f136c6040fef7d36c6ec3e474
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-08-10 22:56:30 +0900

    [Gold V] Title: Fruit Feast, Time: 100 ms, Memory: 36668 KB -BaekjoonHub

---
+++ 백준/Gold/11964. Fruit Feast/Fruit Feast.py
@@ -0,0 +1,42 @@
+from collections import deque
+T, A, B = map(int, input().split())
+
+nums = set()
+q = deque()
+q.append((A, 0))
+q.append((B, 0))
+answer = 0
+while q:
+    value, used = q.popleft()
+    answer = max(answer, value)
+    tmp = value + A
+    if tmp not in nums:
+        if tmp <= T:
+            if tmp == T:
+                answer = T
+                break
+            else:
+                q.append((tmp, used))
+                nums.add(tmp)
+
+    tmp = value + B
+    if tmp not in nums:
+        if tmp <= T:
+            if tmp == T:
+                answer = T
+                break
+            else:
+                q.append((tmp, used))
+                nums.add(tmp)
+    if used == 0:
+        tmp = round(value / 2)
+        if tmp not in nums:
+            if tmp <= T:
+                if tmp == T:
+                    answer = T
+                    break
+                else:
+                    q.append((tmp, 1))
+                    nums.add(tmp)
+
+print(answer)
