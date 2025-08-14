f80fd0fd6188e03f23ec4576d5b3846d7832130f
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-08-14 23:39:08 +0900

    [Silver I] Title: 후보 추천하기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1713. 후보 추천하기/후보 추천하기.py
+++ 백준/Silver/1713. 후보 추천하기/후보 추천하기.py
@@ -1,31 +1,25 @@
-from heapq import heappush, heappop
 N = int(input())
 total = int(input())
 recommend = list(map(int, input().split()))
-now = []
 
-heap = []
+dashboard = dict()
+
 for i in range(total):
-    if recommend[i] not in now:
-        if len(heap) < N:
-            now.append(recommend[i])
-            heappush(heap, (1, i, recommend[i]))
-        else:
-            tmp = heappop(heap)
-            now.remove(tmp[2])
-            now.append(recommend[i])
-            heappush(heap, (1, i, recommend[i]))
+    if recommend[i] in dashboard.keys():
+        dashboard[recommend[i]] += 1
     else:
-        heap2 = []
-        while heap:
-            tmp = heappop(heap)
-            if tmp[2] == recommend[i]:
-                heappush(heap, (tmp[0]+1, tmp[1], tmp[2]))
-                break
-            heappush(heap2, tmp)
-        while heap2:
-            tmp = heappop(heap2)
-            heappush(heap, tmp)
+        if len(dashboard) == N:
+            min_value = total
+            for key in reversed(dashboard.keys()):
+                if min_value >= dashboard[key]:
+                    min_value = dashboard[key]
+                    min_key = key
+            # 가장 오래된 값 삭제
+            dashboard.pop(min_key)
+            dashboard[recommend[i]] = 1
+        else:
+            dashboard[recommend[i]] = 1
 
-now.sort()
-print(*now)
+sorted_keys = sorted(dashboard.keys())
+
+print(*sorted_keys)
