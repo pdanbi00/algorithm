6665f0e76ae9a7d4cb5b263ca52da0db025909ff
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-08-16 14:30:48 +0900

    [Silver IV] Title: 암기왕, Time: 1324 ms, Memory: 335376 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.java
+++ 백준/Silver/2776. 암기왕/암기왕.java
@@ -1,32 +1,44 @@
 import java.io.*;
 import java.util.StringTokenizer;
-import java.util.Map;
-import java.util.HashMap;
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
+
+        // 이분탐색
         for (int tc = 0; tc < T; tc++) {
             int N = Integer.parseInt(br.readLine());
-            Map<Integer, Integer> map = new HashMap<>();
+            int[] nums1 = new int[N];
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int i = 0; i < N; i++) {
                 int num = Integer.parseInt(st.nextToken());
-                map.put(num, 1);
+                nums1[i] = num;
             }
+            Arrays.sort(nums1);
 
             int M = Integer.parseInt(br.readLine());
             st = new StringTokenizer(br.readLine());
             for (int i = 0; i < M; i++) {
                 int num = Integer.parseInt(st.nextToken());
-                if (map.containsKey(num)) {
-                    sb.append(1).append("\n");
-                } else {
-                    sb.append(0).append("\n");
-                }
+                sb.append(binarySearch(0, N-1, nums1, num)).append("\n");
             }
         }
         System.out.println(sb);
+    }
+
+    static int binarySearch(int s, int e, int[] nums, int num) {
+        while (s <= e) {
+            int mid = (s + e) / 2;
+            if (nums[mid] == num) {
+                return 1;
+            } else if (nums[mid] < num) {
+                s = mid + 1;
+            } else {
+                e = mid - 1;
+            }
+        }
+        return 0;
     }
 }
