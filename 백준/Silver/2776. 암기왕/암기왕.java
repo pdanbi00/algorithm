48ff60d211a78c931a3604028590a84fa791555f
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-08-16 14:09:01 +0900

    [Silver IV] Title: 암기왕, Time: 1668 ms, Memory: 384916 KB -BaekjoonHub

---
+++ 백준/Silver/2776. 암기왕/암기왕.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.StringTokenizer;
+import java.util.Map;
+import java.util.HashMap;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int tc = 0; tc < T; tc++) {
+            int N = Integer.parseInt(br.readLine());
+            Map<Integer, Integer> map = new HashMap<>();
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++) {
+                int num = Integer.parseInt(st.nextToken());
+                map.put(num, 1);
+            }
+
+            int M = Integer.parseInt(br.readLine());
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < M; i++) {
+                int num = Integer.parseInt(st.nextToken());
+                if (map.containsKey(num)) {
+                    sb.append(1).append("\n");
+                } else {
+                    sb.append(0).append("\n");
+                }
+            }
+        }
+        System.out.println(sb);
+    }
+}
