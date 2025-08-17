e759c06cac352b35f66612f001a63b8d64b8fd12
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-08-17 15:26:56 +0900

    [Silver I] Title: BOJ 거리, Time: 128 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/12026. BOJ 거리/BOJ 거리.py
@@ -0,0 +1,24 @@
+N = int(input())
+blocks = list(input())
+
+dp = [1e9] * N
+dp[0] = 0
+
+for i in range(N):
+    if blocks[i] == 'B':
+        for j in range(i, N):
+            if blocks[j] == 'O':
+                dp[j] = min(dp[j], dp[i] + (j-i)**2)
+    elif blocks[i] == 'O':
+        for j in range(i, N):
+            if blocks[j] == 'J':
+                dp[j] = min(dp[j], dp[i] + (j-i)**2)
+    elif blocks[i] == 'J':
+        for j in range(i, N):
+            if blocks[j] == 'B':
+                dp[j] = min(dp[j], dp[i] + (j-i)**2)
+
+if dp[N-1] == 1e9:
+    print(-1)
+else:
+    print(dp[N-1])
