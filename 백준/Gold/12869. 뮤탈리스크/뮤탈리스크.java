2f3f5ebda02030cd4bbf809d0f1e21ecad9dbfdc
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-08-21 00:26:57 +0900

    [Gold IV] Title: 뮤탈리스크, Time: 72 ms, Memory: 12160 KB -BaekjoonHub

--- 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.java
+++ 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.java
@@ -1,10 +1,12 @@
 import java.util.StringTokenizer;
 import java.io.*;
-import java.util.Arrays;
+import java.util.Queue;
+import java.util.LinkedList;
 public class Main {
     static int N;
     static int[] SVC;
-    static int[][][] dp;
+    static int[][] attack = {{9, 3, 1}, {9, 1, 3}, {3, 9, 1}, {3, 1, 9}, {1, 9, 3}, {1, 3, 9}};
+    static boolean[][][] visited;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
@@ -13,37 +15,49 @@ public class Main {
         for (int i = 0; i < N; i++) {
             SVC[i] = Integer.parseInt(st.nextToken());
         }
-        dp = new int[61][61][61];
-        for (int i = 0; i < 61; i++) {
-            for (int j = 0; j < 61; j++) {
-                Arrays.fill(dp[i][j], -1);
+        visited = new boolean[61][61][61];
+
+        System.out.println(bfs(SVC[0], SVC[1], SVC[2]));
+    }
+
+    static int bfs(int a, int b, int c) {
+        Queue<Node> q = new LinkedList<>();
+        q.offer(new Node(a, b, c, 0));
+        visited[a][b][c] = true;
+
+        while (!q.isEmpty()) {
+            Node node = q.poll();
+            int na = node.a;
+            int nb = node.b;
+            int nc = node.c;
+            int cnt = node.cnt;
+
+            if (na <= 0 && nb <= 0 && nc <= 0) {
+                return cnt;
+            }
+
+            for (int k = 0; k < 6; k++) {
+                int nna = Math.max(na - attack[k][0], 0);
+                int nnb = Math.max(nb - attack[k][1], 0);
+                int nnc = Math.max(nc - attack[k][2], 0);
+
+                if (!visited[nna][nnb][nnc]) {
+                    visited[nna][nnb][nnc] = true;
+                    q.add(new Node(nna, nnb, nnc, cnt+1));
+                }
             }
         }
-
-        System.out.println(dfs(SVC[0], SVC[1], SVC[2]));
+        return -1;
     }
-    static int dfs(int a, int b, int c) {
-        a = Math.max(a, 0);
-        b = Math.max(b, 0);
-        c = Math.max(c, 0);
 
-        if (a == 0 && b == 0 && c == 0) {
-            return 0;
+    static class Node {
+        int a, b, c, cnt;
+
+        public Node(int a, int b, int c, int cnt) {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            this.cnt = cnt;
         }
-
-        if (dp[a][b][c] != -1) {
-            return dp[a][b][c];
-        }
-
-        int result = 987654321;
-
-        result = Math.min(result, dfs(a-9, b-3, c-1) + 1);
-        result = Math.min(result, dfs(a-9, b-1, c-3) + 1);
-        result = Math.min(result, dfs(a-3, b-9, c-1) + 1);
-        result = Math.min(result, dfs(a-3, b-1, c-9) + 1);
-        result = Math.min(result, dfs(a-1, b-9, c-3) + 1);
-        result = Math.min(result, dfs(a-1, b-3, c-9) + 1);
-
-        return dp[a][b][c] = result;
     }
 }
