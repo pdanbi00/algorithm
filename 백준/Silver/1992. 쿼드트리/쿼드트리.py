ca8bf218fec3028f901e2ec70832338ee2bcc653
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-08-27 16:45:30 +0900

    [Silver I] Title: 쿼드트리, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.py
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.py
@@ -1,33 +1,20 @@
-def compression(arr):
-    if len(arr) == 1:
-        return str(arr[0][0])
-
-    l = len(arr)
-    # 모두 같다면 그 값만 보내기
-    num = arr[0][0]
-    possible = True
-    for i in range(l):
-        for j in range(l):
-            if arr[i][j] != num:
-                possible = False
-                break
-        if not possible:
-            break
-
-    if possible:
-        return str(num)
-
-    result = ''
-
-    for k in [0, l//2]:
-        for p in [0, l//2]:
-            tmp = [[0] * (l//2) for _ in range(l//2)]
-            for i in range(l//2):
-                for j in range(l//2):
-                    tmp[i][j] = arr[k+i][p+j]
-            result += compression(tmp)
-    return '(' + result + ')'
+def compression(r, c, N):
+    global answer
+    num = board[r][c]
+    for i in range(r, r+N):
+        for j in range(c, c+N):
+            if board[i][j] != num:
+                answer += "("
+                compression(r, c, N//2)
+                compression(r, c + N//2, N//2)
+                compression(r + N // 2, c, N // 2)
+                compression(r + N // 2, c + N // 2, N // 2)
+                answer += ")"
+                return
+    answer += num
 
 N = int(input())
-board = [list(map(int, input())) for _ in range(N)]
-print(compression(board))
+board = [list(input()) for _ in range(N)]
+answer = ""
+compression(0, 0, N)
+print(answer)
