918e1047f9748475d8d99d61e4d09d2ca7d385bb
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-08-31 15:05:13 +0900

    [Silver V] Title: 논리학 교수, Time: 32 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/1813. 논리학 교수/논리학 교수.py
+++ 백준/Silver/1813. 논리학 교수/논리학 교수.py
@@ -1,11 +1,21 @@
 N = int(input())
 arr = list(map(int, input().split()))
+info = dict()
+max_k = 0
+for i in range(N):
+    max_k = max(max_k, arr[i])
+    if arr[i] in info:
+        info[arr[i]] += 1
+    else:
+        info[arr[i]] = 1
 
-answer = -1
+answer = 0
 
-for i in range(N+1):
-    cnt = arr.count(i)
-    if cnt == i:
-        answer = max(answer, i)
+for k, v in info.items():
+    if k == v and k >= answer:
+        answer = k
+
+if answer == 0 and 0 in info:
+    answer = -1
 
 print(answer)
