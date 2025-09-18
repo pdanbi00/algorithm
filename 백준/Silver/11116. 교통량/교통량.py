66817ed49b9d4936a07ac98ad0993597e9b93fde
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-09-18 23:47:36 +0900

    [Silver V] Title: 교통량, Time: 60 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11116. 교통량/교통량.py
+++ 백준/Silver/11116. 교통량/교통량.py
@@ -4,28 +4,12 @@ for _ in range(T):
     left = list(map(int, input().split()))
     right = list(map(int, input().split()))
 
-    total = []
-    for i in range(m):
-        total.append(left[i])
-        total.append(right[i])
 
-    total.sort()
     cnt = 0
-    while total:
-        tmp1 = total[0]
-        tmp2 = total[0] + 500
-        tmp3 = total[0] + 1000
-        tmp4 = total[0] + 1500
+    while left:
+        tmp = left[0]
 
-        if tmp1 in left and tmp2 in left and tmp3 in right and tmp4 in right:
+        if tmp in left and tmp+500 in left and tmp+1000 in right and tmp+1500 in right:
             cnt += 1
-            total.remove(tmp1)
-            total.remove(tmp2)
-            total.remove(tmp3)
-            total.remove(tmp4)
-        elif tmp1 in right and tmp2 in right and tmp3 in left and tmp4 in left:
-            total.remove(tmp1)
-            total.remove(tmp2)
-            total.remove(tmp3)
-            total.remove(tmp4)
+        left.pop(0)
     print(cnt)
