6a0fe0e356ddbdfc28cee749511564a9f831755a
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-09-21 21:53:25 +0900

    [Silver III] Title: 대회 장소 준비, Time: 376 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/9555. 대회 장소 준비/대회 장소 준비.py
@@ -0,0 +1,19 @@
+T = int(input())
+
+dr = [-1, -1, -1, 0, 0, 1, 1, 1]
+dc = [-1, 0, 1, -1, 1, -1, 0, 1]
+for _ in range(T):
+    N, M = map(int, input().split())
+    board = [list(map(int, input().split())) for _ in range(N)]
+    school = set()
+    for i in range(N):
+        for j in range(M):
+            if board[i][j] not in school and board[i][j] != -1:
+                for k in range(8):
+                    nr = i + dr[k]
+                    nc = j + dc[k]
+                    if 0 <= nr < N and 0 <= nc < M:
+                        if board[i][j] == board[nr][nc]:
+                            school.add(board[i][j])
+                            break
+    print(len(school))
