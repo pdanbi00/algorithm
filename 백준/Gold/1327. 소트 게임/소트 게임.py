c9dea72f9bc8359400a7abd78b1677bd2def18fb
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-09-22 17:34:28 +0900

    [Gold IV] Title: 소트 게임, Time: 276 ms, Memory: 40128 KB -BaekjoonHub

--- 백준/Gold/1327. 소트 게임/소트 게임.py
+++ 백준/Gold/1327. 소트 게임/소트 게임.py
@@ -1,10 +1,10 @@
 from collections import deque
 N, K = map(int, input().split())
 
-nums = list(input().split())
-target = sorted(nums)
+nums = tuple(map(int, (input().split())))
+target = tuple(range(1, N+1))
 
-visited = set("".join(nums))
+visited = set(nums)
 q = deque()
 q.append((nums, 0))
 
@@ -19,9 +19,8 @@ while q:
     for i in range(N - K + 1):
         tmp = now[i:i + K]
         new = now[:i] + tmp[::-1] + now[i + K:]
-        new_str = "".join(new)
-        if new_str not in visited:
+        if new not in visited:
             q.append((new, cnt+1))
-            visited.add(new_str)
+            visited.add(new)
 
 print(answer)
