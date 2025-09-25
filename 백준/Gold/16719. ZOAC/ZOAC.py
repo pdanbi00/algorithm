882e69ecc2b71ccc5cbe00bc1732c294a27c995a
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-09-25 23:34:02 +0900

    [Gold V] Title: ZOAC, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/16719. ZOAC/ZOAC.py
+++ 백준/Gold/16719. ZOAC/ZOAC.py
@@ -1,41 +1,21 @@
-# dfs임
-# 기준점부터 뒤쪽 완성 다 시키고
-# 그 다음에 기준점 앞쪽 완성시키는거 반복
-
 import sys
 sys.setrecursionlimit(10**6)
 
-line = list(input())
-N = len(line)
-word = []
-used = [False] * N
+s = input()
+order = [""] * (len(s))
 
-def func(s, e): # 시작 인덱스, 종료 인덱스, 뒤에 붙여야하는지 여부(True면 뒤에 붙이기 False면 앞에 추가하기)
-    global answer, used, word
-    if s <= e and 0 <= s < N and 0 <= e < N:
-        # 가장 작은 위치 확인하기
-        small = ord(line[s])
-        small_idx = s
+def func(arr, start_idx):
+    if arr == "":
+        return
 
-        for i in range(s+1, e+1):
-            if ord(line[i]) < small:
-                small = ord(line[i])
-                small_idx = i
-        if used[small_idx] == False:
-            word.append(small_idx)
-            used[small_idx] = True
-            word.sort()
-            tmp = ''
-            for i in word:
-                tmp += line[i]
-            print(tmp)
+    char = min(arr)
+    idx = arr.index(char)
 
-            # 뒤쪽 확인하기
-            func(small_idx+1, e)
+    order[start_idx + idx] = char
 
-            # 앞쪽 확인하기
-            func(s, small_idx-1)
+    print("".join(order))
 
-    return
+    func(arr[idx+1:], start_idx+idx+1)
+    func(arr[:idx], start_idx)
 
-func(0, N-1)
+func(s, 0)
