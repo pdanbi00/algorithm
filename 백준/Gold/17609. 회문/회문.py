66f269e32aa39bb6c55643fe04a67057ffb42616
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-09-27 01:00:11 +0900

    [Gold V] Title: 회문, Time: 144 ms, Memory: 36212 KB -BaekjoonHub

--- 백준/Gold/17609. 회문/회문.py
+++ 백준/Gold/17609. 회문/회문.py
@@ -2,48 +2,26 @@ import sys
 input = sys.stdin.readline
 
 def func(word):
-    N = len(word)
-    pseudo = False
-    front = False
-    back = False
-    front_cnt = 0
-    back_cnt = 0
-    cnt = 0
-    for i in range(N//2):
-        if not pseudo:
-            if word[i] == word[N-1-i]:
-                cnt += 1
-            else:
-                if word[i] == word[N-1-i-1]:
-                    pseudo = True
-                    back = True
-                    back_cnt = cnt + 1
-                if word[i+1] == word[N-1-i]:
-                    pseudo = True
-                    front = True
-                    front_cnt = cnt + 1
+    left = 0
+    right = len(word) - 1
 
-        else:
-            if back:
-                if word[i] == word[N - 1 - i -1]:
-                    back_cnt += 1
+    while left < right:
+        if word[left] != word[right]:
+            rm_left = word[left+1:right+1]
+            if rm_left == rm_left[::-1]:
+                return 1
 
-            if front:
-                if word[i+1] == word[N-1-i]:
-                    front_cnt += 1
+            rm_right = word[left:right]
+            if rm_right == rm_right[::-1]:
+                return 1
 
-    if cnt == N//2 and not pseudo:
-        return 0
-    elif (front or back) and (front_cnt == N//2 or back_cnt == N//2) :
-        return 1
-    return 2
+            return 2
+        left += 1
+        right -= 1
+
+    return 0
 
 T = int(input())
 for _ in range(T):
     word = list(input().rstrip())
     print(func(word))
-
-'''
-1
-abaab
-'''
