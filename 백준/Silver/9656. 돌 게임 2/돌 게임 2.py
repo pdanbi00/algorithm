37a9bfc7edd76da0f925d3c218928a6cd02f8d49
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-09-27 22:30:40 +0900

    [Silver V] Title: 돌 게임 2, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9656. 돌 게임 2/돌 게임 2.py
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.py
@@ -9,12 +9,7 @@ dp[2] = 'CY'
 dp[3] = 'SK'
 
 for i in range(4, N+1):
-    if dp[i-1] == 'SK':
-        dp[i] = 'CY'
-    else:
-        dp[i] = 'SK'
-
-    if dp[i-3] == 'SK':
+    if dp[i-1] == 'SK' or dp[i-3] == 'SK':
         dp[i] = 'CY'
     else:
         dp[i] = 'SK'
