d74072a2f4e7c9b93c18e115e80b66829593f5a9
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-10-05 17:17:32 +0900

    [Silver III] Title: 전쟁 - 땅따먹기, Time: 5732 ms, Memory: 49992 KB -BaekjoonHub

--- 백준/Silver/1270. 전쟁 － 땅따먹기/전쟁 － 땅따먹기.py
+++ 백준/Silver/1270. 전쟁 － 땅따먹기/전쟁 － 땅따먹기.py
@@ -1,22 +1,37 @@
-N = int(input())
-
-for _ in range(N):
-    arr = list(map(int, input().split()))
-    T = arr[0]
-    nums = arr[1:]
-    info = dict()
-    for i in range(T):
-        if nums[i] not in info:
-            info[nums[i]] = 1
+# 보이어-무어 과반수 투표 알고리즘
+def boyer_moore_majority(arr):
+    count = 0 # 과반수의 원소가 몇개 있는지
+    major = 0 # 과반수의 원소
+    for ele in arr:
+        if count == 0:
+            major = ele
+        if major == ele:
+            count += 1
         else:
-            info[nums[i]] += 1
+            count -= 1
 
-    find = False
-    for k, v in info.items():
-        if v > T//2:
-            find = True
-            print(k)
-            break
+    if count == 0:
+        return None, None
 
-    if not find:
+    K = len(arr)
+    m = 0 # 정말 과반수인지 체크용 변수
+
+    for ele in arr:
+        if ele == major:
+            m += 1
+
+    if m > K//2:
+        return major, m
+
+    else:
+        return None, None
+
+
+N = int(input())
+for _ in range(N):
+    war = list(map(int, input().split()))
+    major_num, major_cnt = boyer_moore_majority(war[1:])
+    if major_num == None:
         print("SYJKGW")
+    else:
+        print(major_num)
