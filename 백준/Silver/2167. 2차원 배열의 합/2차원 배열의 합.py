eabcb6a2bfd82e86013b358d4ff623a2b08ad9a0
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-10-19 22:33:18 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 112 ms, Memory: 38948 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.py
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.py
@@ -3,32 +3,13 @@ input = sys.stdin.readline
 N, M = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(N)]
 
-sum_board = [[0] * M for _ in range(N)]
-pre = 0
-for i in range(N):
-    for j in range(M):
-        sum_board[i][j] = pre + board[i][j]
-        pre = sum_board[i][j]
+dp = [[0] * (M+1) for _ in range(N+1)]
+for i in range(1, N+1):
+    for j in range(1, M+1):
+        dp[i][j] = board[i-1][j-1] + dp[i][j-1] + dp[i-1][j] - dp[i-1][j-1]
 
 K = int(input())
 for _ in range(K):
     x1, y1, x2, y2 = map(int, input().split())
-    x1 -= 1
-    y1 -= 1
-    x2 -= 1
-    y2 -= 1
-    tmp = sum_board[x2][y2]
-    # 왼쪽 빼기
-    for i in range(x1, x2+1):
-        for j in range(y1):
-            tmp -= board[i][j]
-    for i in range(x1, x2):
-        if y2 + 1 < M:
-            for j in range(y2 + 1, M):
-                tmp -= board[i][j]
-
-    # 아래쪽 빼기
-    if x1 - 1 >= 0:
-        tmp -= sum_board[x1-1][M-1]
-
+    tmp = dp[x2][y2] - dp[x2][y1-1] - dp[x1-1][y2] + dp[x1-1][y1-1]
     print(tmp)
