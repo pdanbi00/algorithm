b4d7c6225fc52e81a3b9b2c11d5a9c195b64c620
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-10-23 19:25:47 +0900

    [Silver II] Title: 아카라카, Time: 836 ms, Memory: 38272 KB -BaekjoonHub

--- 백준/Silver/23304. 아카라카/아카라카.py
+++ 백준/Silver/23304. 아카라카/아카라카.py
@@ -1,32 +1,22 @@
 line = input()
 N = len(line)
-if N == 1:
+def func(arr):
+    if len(arr) == 1:
+        return True
+    
+    N = len(arr)
+    
+    prefix = arr[:N//2]
+    if N % 2 == 0:
+        suffix = arr[N//2:]
+    else:
+        suffix = arr[N//2+1:]
+    
+    if arr == arr[::-1] and prefix == prefix[::-1] and suffix == suffix[::-1]:
+        return func(prefix) and func(suffix)
+    return False
+
+if func(line):
     print("AKARAKA")
 else:
-    akaraka = True
-
-    def func(arr):
-        if len(arr) == 1:
-            return True
-        N = len(arr)
-        for i in range(N//2):
-            if arr[i] != arr[N-1-i]:
-                return False
-
-        if N % 2 == 0:
-            new_line_f = arr[:N//2]
-            new_lien_b = arr[N//2:]
-        else:
-            new_line_f = arr[:N // 2]
-            new_lien_b = arr[N // 2 + 1:]
-
-        if func(new_line_f) and func(new_lien_b):
-            return True
-        return False
-
-    akaraka = func(line)
-
-    if akaraka:
-        print("AKARAKA")
-    else:
-        print("IPSELENTI")
+    print("IPSELENTI")
