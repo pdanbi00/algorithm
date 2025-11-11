7d93b9406eb25297a3a62305c5cb8eb91d2e4f87
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-11-11 23:47:14 +0900

    [Silver IV] Title: 캠프가는 영식, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1590. 캠프가는 영식/캠프가는 영식.py
@@ -0,0 +1,20 @@
+N, T = map(int, input().split())
+times = set()
+for _ in range(N):
+    S, I, C = map(int, input().split())
+    for i in range(C):
+        times.add(S + (i * I))
+
+times = list(times)
+times.sort()
+# print(times)
+if times[-1] < T:
+    print(-1)
+elif times[0] > T:
+    print(times[0] - T)
+else:
+    for i in range(len(times)):
+        if times[i] >= T:
+            ans = times[i]
+            break
+    print(ans-T)
