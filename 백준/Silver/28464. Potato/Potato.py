9cbbcc0b7be040c05171fff6a76f4cc308e30473
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2025-12-06 18:41:26 +0900

    [Silver V] Title: Potato, Time: 144 ms, Memory: 53288 KB -BaekjoonHub

--- 백준/Silver/28464. Potato/Potato.py
+++ 백준/Silver/28464. Potato/Potato.py
@@ -1,15 +1,19 @@
-from collections import deque
 N = int(input())
 potatoes = list(map(int, input().split()))
 
 potatoes.sort(reverse=True)
-potatoes = deque(potatoes)
+
 park = 0
 sang = 0
+s = 0
+e = N-1
 
-while potatoes:
-    park += potatoes.popleft()
-    if potatoes:
-        sang += potatoes.pop()
+while s <= e:
+    park += potatoes[s]
+    if s < e:
+        sang += potatoes[e]
+
+    s += 1
+    e -= 1
 
 print(sang, park)
