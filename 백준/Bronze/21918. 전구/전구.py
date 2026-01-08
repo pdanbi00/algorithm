7898f2f95acd3194411ad08f03757bd8a1e69f74
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2026-01-08 22:54:20 +0900

    [Bronze II] Title: 전구, Time: 1436 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/21918. 전구/전구.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+lights = list(map(int, input().split()))
+for _ in range(M):
+    a, b, c = map(int, input().split())
+    if a == 1:
+        lights[b-1] = c
+    elif a == 2:
+        for i in range(b-1, c):
+            lights[i] = (lights[i] + 1) % 2
+    elif a == 3:
+        for i in range(b-1, c):
+            lights[i] = 0
+    elif a == 4:
+        for i in range(b-1, c):
+            lights[i] = 1
+
+print(*lights)
