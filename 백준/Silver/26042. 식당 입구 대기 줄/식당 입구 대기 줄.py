9567397e98ec66e7b9c4703dfcb7a4220d00524e
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2026-01-13 23:55:00 +0900

    [Silver V] Title: 식당 입구 대기 줄, Time: 140 ms, Memory: 36632 KB -BaekjoonHub

--- 백준/Silver/26042. 식당 입구 대기 줄/식당 입구 대기 줄.py
+++ 백준/Silver/26042. 식당 입구 대기 줄/식당 입구 대기 줄.py
@@ -9,12 +9,19 @@ for _ in range(N):
     info = list(map(int, input().split()))
     if info[0] == 1:
         q.append(info[1])
+        
+    else:
         if len(q) > max_l:
             max_l = len(q)
             answer = q[-1]
         elif len(q) == max_l:
             answer = min(answer, q[-1])
-    else:
         q.popleft()
-
+        
+if len(q) > max_l:
+    max_l = len(q)
+    answer = q[-1]
+elif len(q) == max_l:
+    answer = min(answer, q[-1])
+    
 print(max_l, answer)
