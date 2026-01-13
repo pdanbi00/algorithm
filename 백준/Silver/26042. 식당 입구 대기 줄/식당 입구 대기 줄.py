de8688e578e4e3b75000b4537ff79f0b3f0555c9
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2026-01-13 23:54:10 +0900

    [Silver V] Title: 식당 입구 대기 줄, Time: 164 ms, Memory: 37364 KB -BaekjoonHub

---
+++ 백준/Silver/26042. 식당 입구 대기 줄/식당 입구 대기 줄.py
@@ -0,0 +1,20 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+N = int(input())
+q = deque()
+max_l = 0
+answer = 0
+for _ in range(N):
+    info = list(map(int, input().split()))
+    if info[0] == 1:
+        q.append(info[1])
+        if len(q) > max_l:
+            max_l = len(q)
+            answer = q[-1]
+        elif len(q) == max_l:
+            answer = min(answer, q[-1])
+    else:
+        q.popleft()
+
+print(max_l, answer)
