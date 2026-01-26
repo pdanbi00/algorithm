942cfb4574187ee793ae801fd8c0343ee6391f13
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2026-01-26 23:36:04 +0900

    [Gold V] Title: 두 개의 탑, Time: 1340 ms, Memory: 38312 KB -BaekjoonHub

---
+++ 백준/Gold/2118. 두 개의 탑/두 개의 탑.py
@@ -0,0 +1,17 @@
+N = int(input())
+dist = [int(input()) for _ in range(N)]
+
+prefix_sum = [0] * (2 * N + 1)
+for i in range(2 * N):
+    prefix_sum[i+1] = prefix_sum[i] + dist[i % N]
+
+ans = 0
+total = sum(dist)
+right = 1
+
+for left in range(2 * N):
+    # 시계 방향의 거리가 반시계방향의 거리보다 크면 거리 줄임
+    while right < 2 * N + 1 and prefix_sum[right] - prefix_sum[left] <= total - (prefix_sum[right] - prefix_sum[left]):
+        ans = max(ans, prefix_sum[right] - prefix_sum[left])
+        right += 1
+print(ans)
