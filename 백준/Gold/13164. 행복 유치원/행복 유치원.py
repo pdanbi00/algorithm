52331ab5d9e9114478dbc867bc6f854e69f424c2
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2026-02-06 20:52:23 +0900

    [Gold V] Title: 행복 유치원, Time: 204 ms, Memory: 66292 KB -BaekjoonHub

--- 백준/Gold/13164. 행복 유치원/행복 유치원.py
+++ 백준/Gold/13164. 행복 유치원/행복 유치원.py
@@ -1,16 +1,11 @@
 N, K = map(int, input().split())
 children = list(map(int, input().split()))
 diff = []
+
 for i in range(1, N):
-    diff.append(children[i] - children[i-1])
+    tmp = children[i] - children[i-1]
+    diff.append(tmp)
 
-diff.sort(reverse=True)
+diff.sort()
 
-total = sum(diff)
-answer = total - sum(diff[:K-1])
-print(answer)
-
-'''
-9 4
-1 2 3 5 7 12 15 16 17
-'''
+print(sum(diff[:N-K]))
