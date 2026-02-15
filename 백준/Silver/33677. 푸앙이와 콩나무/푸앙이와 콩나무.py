8e08f00246795a47a7b038120fc30c316fdea9a5
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2026-02-16 00:13:14 +0900

    [Silver I] Title: 푸앙이와 콩나무, Time: 1924 ms, Memory: 162624 KB -BaekjoonHub

---
+++ 백준/Silver/33677. 푸앙이와 콩나무/푸앙이와 콩나무.py
@@ -0,0 +1,31 @@
+from collections import deque
+N = int(input())
+
+q = deque()
+used = [[N, 5 * N] for _ in range(1000001)]
+used[1][0], used[1][1] = 1, 1
+
+
+q.append((1, 1, 1))
+while q:
+    length, water, day = q.popleft()
+
+    tmp = length * length
+    if tmp <= N and (used[tmp][0] > day + 1 or (used[tmp][0] == day + 1 and used[tmp][1] > water + 5)):
+        q.append((tmp, water + 5, day + 1))
+        used[tmp][0] = day+1
+        used[tmp][1] = water+5
+
+    tmp = length * 3
+    if tmp <= N and (used[tmp][0] > day + 1 or (used[tmp][0] == day + 1 and used[tmp][1] > water + 3)):
+        q.append((tmp, water + 3, day + 1))
+        used[tmp][0] = day + 1
+        used[tmp][1] = water + 3
+
+    tmp = length + 1
+    if tmp <= N and (used[tmp][0] > day + 1 or (used[tmp][0] == day + 1 and used[tmp][1] > water + 1)):
+        q.append((tmp, water + 1, day + 1))
+        used[tmp][0] = day + 1
+        used[tmp][1] = water + 1
+
+print(used[N][0], used[N][1])
