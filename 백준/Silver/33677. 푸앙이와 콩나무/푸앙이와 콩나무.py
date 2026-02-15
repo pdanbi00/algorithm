1ac7b820b5c23dcfecbaa07bb80dafc3e20dac7b
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2026-02-16 00:44:01 +0900

    [Silver I] Title: 푸앙이와 콩나무, Time: 1256 ms, Memory: 251852 KB -BaekjoonHub

--- 백준/Silver/33677. 푸앙이와 콩나무/푸앙이와 콩나무.py
+++ 백준/Silver/33677. 푸앙이와 콩나무/푸앙이와 콩나무.py
@@ -1,34 +1,72 @@
+import math
 from collections import deque
+
 N = int(input())
 
+data = dict()
+day = 0
+answer = 10 ** 8
+water = 0
+data[1] = [answer, water]
+data[N] = [0, 0]
 q = deque()
-used = [[N, 5 * N] for _ in range(1000001)]
-used[1][0], used[1][1] = 1, 1
+q.append(N)
 
-
-q.append((1, 1, 1))
 while q:
-    length, water, day = q.popleft()
-
-    if day > used[N][0]:
+    cur = q.popleft()
+    if N == 0:
+        data[1] = [-1, -1]
+        break
+    if N == 1:
+        data[1] = [0, 0]
+        break
+    if N == 2:
+        data[1] = [1, 1]
         break
 
-    tmp = length * length
-    if tmp <= N and (used[tmp][0] > day + 1 or (used[tmp][0] == day + 1 and used[tmp][1] > water + 5)):
-        q.append((tmp, water + 5, day + 1))
-        used[tmp][0] = day+1
-        used[tmp][1] = water+5
+    if cur <= 1:
+        continue
 
-    tmp = length * 3
-    if tmp <= N and (used[tmp][0] > day + 1 or (used[tmp][0] == day + 1 and used[tmp][1] > water + 3)):
-        q.append((tmp, water + 3, day + 1))
-        used[tmp][0] = day + 1
-        used[tmp][1] = water + 3
+    data1 = cur - 1
+    if data1 in data:
+        if data[data1][0] > data[cur][0] + 1:
+            data[data1][0] = data[cur][0] + 1
+            data[data1][1] = data[cur][1] + 1
+            q.append(data1)
+        elif data[data1][0] == data[cur][0] + 1 and data[data1][1] > data[cur][1] + 1:
+            data[data1][1] = data[cur][1] + 1
+            q.append(data1)
+    else:
+        data[data1] = [data[cur][0] + 1, data[cur][1] + 1]
+        q.append(data1)
 
-    tmp = length + 1
-    if tmp <= N and (used[tmp][0] > day + 1 or (used[tmp][0] == day + 1 and used[tmp][1] > water + 1)):
-        q.append((tmp, water + 1, day + 1))
-        used[tmp][0] = day + 1
-        used[tmp][1] = water + 1
+    if cur % 3 == 0:
+        data2 = cur // 3
+        if data2 in data:
+            if data[data2][0] > data[cur][0] + 1:
+                data[data2][0] = data[cur][0] + 1
+                data[data2][1] = data[cur][1] + 3
+                q.append(data2)
+            elif data[data2][0] == data[cur][0] + 1 and data[data2][1] > data[cur][1] + 3:
+                data[data2][1] = data[cur][1] + 3
+                q.append(data2)
+        else:
+            data[data2] = [data[cur][0] + 1, data[cur][1] + 3]
+            q.append(data2)
 
-print(used[N][0], used[N][1])
+    if math.sqrt(cur) == int(math.sqrt(cur)):
+        if cur != 1:
+            data3 = int(math.sqrt(cur))
+            if data3 in data:
+                if data[data3][0] > data[cur][0] + 1:
+                    data[data3][0] = data[cur][0] + 1
+                    data[data3][1] = data[cur][1] + 5
+                    q.append(data3)
+                elif data[data3][0] == data[cur][0] + 1 and data[data3][1] > data[cur][1] + 5:
+                    data[data3][1] = data[cur][1] + 5
+                    q.append(data3)
+            else:
+                data[data3] = [data[cur][0] + 1, data[cur][1] + 5]
+                q.append(data3)
+
+print(data[1][0] + 1, data[1][1] + 1)
