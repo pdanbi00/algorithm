71c8e9a71a0239f9a6bf2737a6dbcd318e94bd69
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2026-02-27 01:49:56 +0900

    [Gold V] Title: 달력, Time: 72 ms, Memory: 12000 KB -BaekjoonHub

--- 백준/Gold/20207. 달력/달력.java
+++ 백준/Gold/20207. 달력/달력.java
@@ -2,39 +2,23 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
-import java.util.List;
-import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         int N = Integer.parseInt(br.readLine());
-
-        List<int[]> schedule = new ArrayList<>();
-        int[] arr;
-
-        for (int i = 0; i < N; i++) {
-            arr = new int[2];
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 2; j++) {
-                arr[j] = Integer.parseInt(st.nextToken());
-            }
-            schedule.add(arr);
-        }
-        schedule.sort((a, b) -> {
-            if (a[0] == b[0]) {
-                return b[1] - a[1];
-            }
-            return a[0] - b[0];
-        });
-
+        int s, e;
         int[] days = new int[366];
 
-        for (int[] day : schedule) {
-            for (int i = day[0]; i <= day[1]; i++) {
-                days[i]++;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            s = Integer.parseInt(st.nextToken());
+            e = Integer.parseInt(st.nextToken());
+            for (int j = s; j <= e; j++) {
+                days[j]++;
             }
         }
+
         int answer = 0;
         int day = 0;
         int pre = -1;
