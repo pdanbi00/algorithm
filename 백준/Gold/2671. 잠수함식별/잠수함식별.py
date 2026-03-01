af01a090d54ac74caefe1870817af01fb6f20508
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2026-03-02 01:24:13 +0900

    [Gold V] Title: 잠수함식별, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/2671. 잠수함식별/잠수함식별.py
+++ 백준/Gold/2671. 잠수함식별/잠수함식별.py
@@ -1,11 +1,60 @@
-import re
+# # 정규표현식
+# import re
+#
+# word = input()
+# pattern = re.compile('(100+1+|01)+')
+#
+# res = pattern.fullmatch(word)
+#
+# if res:
+#     print("SUBMARINE")
+# else:
+#     print("NOISE")
 
 word = input()
-pattern = re.compile('(100+1+|01)+')
 
-res = pattern.fullmatch(word)
+i = 0
+N = len(word)
+possible = True
 
-if res:
+while i < N:
+    if i >= N-1:
+        possible = False
+        break
+
+    elif word[i:i+2] == '01':
+        i += 2
+    elif word[i:i+2] == '10':
+        i += 2
+        if word[i] == '1':
+            possible = False
+            break
+
+        while i < N:
+            if word[i] == '1':
+                break
+            else:
+                i += 1
+        else:
+            possible = False # 100 다음에는 1이 하나는 와야 됨.
+            break
+
+        while i < N and word[i] == '1':
+            if word[i] == '0':
+                break
+            else:
+                i += 1
+
+        if i < N-1 and word[i:i+2] == '00':
+            i -= 1
+        if word[i-1] == '0':
+            possible = False
+            break
+    else:
+        possible = False
+        break
+
+if possible:
     print("SUBMARINE")
 else:
     print("NOISE")
