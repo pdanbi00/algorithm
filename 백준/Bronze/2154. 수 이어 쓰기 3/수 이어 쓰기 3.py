8ccaa85847d47c37946d091dc36f1213556930ee
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2026-03-04 16:56:44 +0900

    [Bronze II] Title: 수 이어 쓰기 3, Time: 60 ms, Memory: 38792 KB -BaekjoonHub

--- 백준/Bronze/2154. 수 이어 쓰기 3/수 이어 쓰기 3.py
+++ 백준/Bronze/2154. 수 이어 쓰기 3/수 이어 쓰기 3.py
@@ -1,17 +1,5 @@
 N = int(input())
-line = ''
-for i in range(1, N+1):
-    line += str(i)
-N = str(N)
-l = len(N)
-for i in range(len(line)):
-    if line[i] == N[0]:
-        cnt = 0
-        for j in range(l):
-            if line[i+j] == N[j]:
-                cnt += 1
-            else:
-                break
-        if cnt == l:
-            print(i+1)
-            break
+target = str(N)
+
+S = ''.join(str(i) for i in range(1, N+1))
+print(S.find(target)+1)
