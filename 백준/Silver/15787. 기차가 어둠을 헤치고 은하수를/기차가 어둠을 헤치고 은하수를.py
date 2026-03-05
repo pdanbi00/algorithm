3269c2e98469039ae2b08fa5e72af40e8095c579
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2026-03-05 18:41:42 +0900

    [Silver II] Title: 기차가 어둠을 헤치고 은하수를, Time: 156 ms, Memory: 34204 KB -BaekjoonHub

--- 백준/Silver/15787. 기차가 어둠을 헤치고 은하수를/기차가 어둠을 헤치고 은하수를.py
+++ 백준/Silver/15787. 기차가 어둠을 헤치고 은하수를/기차가 어둠을 헤치고 은하수를.py
@@ -2,32 +2,26 @@ import sys
 input = sys.stdin.readline
 N, M = map(int, input().split())
 
-train = [[0] * 20 for _ in range(N)]
+train = [0] * N
 
 for _ in range(M):
     command = list(map(int, input().split()))
     if command[0] == 1:
-        i, x = command[1], command[2]
-        train[i-1][x-1] = 1
+        i, x = command[1]-1, command[2]-1
+        train[i] = train[i] | 1 << x
     elif command[0] == 2:
-        i, x = command[1], command[2]
-        train[i - 1][x - 1] = 0
+        i, x = command[1]-1, command[2]-1
+        train[i] = train[i] & (~(1 << x))
     elif command[0] == 3:
-        i = command[1]
-        train[i-1][19] = 0
-        for j in range(18, -1, -1):
-            train[i-1][j+1] = train[i-1][j]
-        train[i-1][0] = 0
+        i = command[1] - 1
+        train[i] = train[i] << 1
+        train[i] = train[i] % (1 << 20)
     else:
-        i = command[1]
-        train[i - 1][0] = 0
-        for j in range(19):
-            train[i - 1][j] = train[i - 1][j+1]
-        train[i - 1][19] = 0
-
+        i = command[1]-1
+        train[i] = train[i] >> 1
 milky_way = set()
 
 for i in range(N):
-    milky_way.add(tuple(train[i]))
+    milky_way.add(train[i])
 
 print(len(milky_way))
