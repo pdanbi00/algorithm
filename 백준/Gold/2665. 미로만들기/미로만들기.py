c238d9f4be17478f7fdae34e03b3b5609151c593
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2026-03-10 18:27:13 +0900

    [Gold IV] Title: 미로만들기, Time: 60 ms, Memory: 34968 KB -BaekjoonHub

--- 백준/Gold/2665. 미로만들기/미로만들기.py
+++ 백준/Gold/2665. 미로만들기/미로만들기.py
@@ -1,6 +1,9 @@
 from collections import deque
+import sys
+input = sys.stdin.readline
+
 N = int(input())
-board = [list(input()) for _ in range(N)]
+board = [list(input().rstrip()) for _ in range(N)]
 visited = [[-1] * N for _ in range(N)]
 
 q = deque()
@@ -12,21 +15,19 @@ dc = [0, 0, -1, 1]
 
 while q:
     r, c = q.popleft()
-
+    
     if r == N-1 and c == N-1:
-        continue
+        break
 
     for k in range(4):
         nr = r + dr[k]
         nc = c + dc[k]
-        if 0 <= nr < N and 0 <= nc < N:
-            if board[nr][nc] == '1' and (visited[nr][nc] == -1 or visited[nr][nc] > visited[r][c]):
-                q.append((nr, nc))
+        if 0 <= nr < N and 0 <= nc < N and visited[nr][nc] == -1:
+            if board[nr][nc] == '1':
+                q.appendleft((nr, nc))
                 visited[nr][nc] = visited[r][c]
-            elif board[nr][nc] == '0' and (visited[nr][nc] == -1 or visited[nr][nc] > visited[r][c] + 1):
+            elif board[nr][nc] == '0':
                 q.append((nr, nc))
                 visited[nr][nc] = visited[r][c] + 1
 
-# for i in range(N):
-#     print(visited[i])
 print(visited[N-1][N-1])
