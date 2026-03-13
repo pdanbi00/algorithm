506b936faa728c1fccf5739d9e74b0aa4c9b139b
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2026-03-13 17:31:27 +0900

    [Silver III] Title: 눈덩이 굴리기, Time: 68 ms, Memory: 11648 KB -BaekjoonHub

--- 백준/Silver/21735. 눈덩이 굴리기/눈덩이 굴리기.java
+++ 백준/Silver/21735. 눈덩이 굴리기/눈덩이 굴리기.java
@@ -2,10 +2,8 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-import java.util.Queue;
-import java.util.LinkedList;
-import java.util.Set;
-import java.util.HashSet;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,46 +17,28 @@ public class Main {
             yard[i] = Integer.parseInt(st.nextToken());
         }
 
-        Queue<int[]> q = new LinkedList<>();
-        Set<int[]> set = new HashSet<>();
-        q.add(new int[] {0, 0, 1});
-        set.add(new int[] {0, 0, 1});
+        int[][] dp = new int[M+1][N+1];
+        for (int i = 0; i < M+1; i++) {
+            Arrays.fill(dp[i], -1);
+        }
         int answer = 1;
+        dp[0][0] = 1;
 
-        while (!q.isEmpty()) {
-            int[] arr = q.poll();
-
-            int idx = arr[0];
-            int time = arr[1];
-            int size = arr[2];
-
-            if (time == M || idx >= N) {
-                answer = Math.max(answer, size);
-                continue;
-            }
-
-            // 굴리기
-            int nIdx = idx + 1;
-            if (nIdx <= N) {
-                int nSize = size + yard[nIdx];
-                int[] tmp = new int[] {nIdx, time+1, nSize};
-                if (!set.contains(tmp)) {
-                    q.add(tmp);
-                    set.add(tmp);
-                }
-            }
-
-            // 던지기
-            nIdx = idx + 2;
-            if (nIdx <= N) {
-                int nSize = size / 2 + yard[nIdx];
-                int[] tmp = new int[] {nIdx, time+1, nSize};
-                if (!set.contains(tmp)) {
-                    q.add(tmp);
-                    set.add(tmp);
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                if (dp[i][j] != -1) {
+                    // 굴리기
+                    dp[i+1][j+1] = Math.max(dp[i+1][j+1], dp[i][j] + yard[j+1]);
+                    answer = Math.max(answer, dp[i+1][j+1]);
+                    // 던지기
+                    if (j + 2 <= N) {
+                        dp[i+1][j+2] = Math.max(dp[i+1][j+2], dp[i][j] / 2 + yard[j+2]);
+                        answer = Math.max(answer, dp[i+1][j+2]);
+                    }
                 }
             }
         }
+
         System.out.print(answer);
     }
 }
