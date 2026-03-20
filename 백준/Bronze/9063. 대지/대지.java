7897e260e89a214105075b4f4b1511bfca34c69b
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2026-03-21 00:18:53 +0900

    [Bronze III] Title: 대지, Time: 264 ms, Memory: 30016 KB -BaekjoonHub

---
+++ 백준/Bronze/9063. 대지/대지.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.Collections;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        ArrayList<Integer> xArr = new ArrayList<>();
+        ArrayList<Integer> yArr = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            xArr.add(x);
+            yArr.add(y);
+        }
+        System.out.print((Collections.max(xArr) - Collections.min(xArr)) * (Collections.max(yArr) - Collections.min(yArr)));
+    }
+}
