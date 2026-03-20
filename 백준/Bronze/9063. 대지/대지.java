f65f5f9faed8cdf925aad35f99d6ae4a71766dc4
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2026-03-21 00:24:37 +0900

    [Bronze III] Title: 대지, Time: 212 ms, Memory: 25672 KB -BaekjoonHub

--- 백준/Bronze/9063. 대지/대지.java
+++ 백준/Bronze/9063. 대지/대지.java
@@ -2,25 +2,26 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-import java.util.ArrayList;
-import java.util.Collections;
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st;
-        ArrayList<Integer> xArr = new ArrayList<>();
-        ArrayList<Integer> yArr = new ArrayList<>();
+        int xMax = -100000;
+        int xMin = 100000;
+        int yMax = -100000;
+        int yMin = 100000;
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
-            xArr.add(x);
-            yArr.add(y);
+            xMax = Math.max(xMax, x);
+            xMin = Math.min(xMin, x);
+            yMax = Math.max(yMax, y);
+            yMin = Math.min(yMin, y);
         }
-        Collections.sort(xArr);
-        Collections.sort(yArr);
-        System.out.print((xArr.get(N-1)-xArr.get(0)) * (yArr.get(N-1) - yArr.get(0)));
+        System.out.print((xMax - xMin) * (yMax - yMin));
     }
 }
