bc27902abf80b1e0986812f724eb519de55ba339
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2026-03-25 23:56:00 +0900

    [Gold IV] Title: 안정된 집단, Time: 72 ms, Memory: 11952 KB -BaekjoonHub

---
+++ 백준/Gold/2653. 안정된 집단/안정된 집단.java
@@ -0,0 +1,68 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        int[][] board = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        boolean[] v = new boolean[n];
+        StringBuilder sb = new StringBuilder();
+        int cnt = 0;
+        boolean possible = true;
+        for (int i = 0; i < n; i++) {
+            if (v[i]) continue;
+            v[i] = true;
+            List<Integer> tmp = new ArrayList<>();
+            tmp.add(i);
+
+            for (int j = 0; j < n; j++) {
+                if (i == j || board[i][j] == 1) continue;
+                if (v[j]) {
+                    possible = false;
+                    break;
+                }
+                v[j] = true;
+                tmp.add(j);
+            }
+
+            if (tmp.size() < 2) {
+                possible = false;
+                break;
+            }
+
+            for (int cur1 : tmp) {
+                for (int cur2 : tmp) {
+                    if (board[cur1][cur2] == 1) {
+                        possible = false;
+                        break;
+                    }
+                }
+            }
+            cnt++;
+            for (int cur : tmp) {
+                sb.append(cur+1).append(' ');
+            }
+            sb.append('\n');
+        }
+
+        if (possible) {
+            System.out.println(cnt);
+            System.out.print(sb);
+        } else {
+            System.out.print(0);
+        }
+    }
+}
