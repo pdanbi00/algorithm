3136057bebac64bce413c1a7eff417786ac50380
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2026-04-03 00:32:57 +0900

    [Bronze I] Title: 유진수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1356. 유진수/유진수.py
+++ 백준/Bronze/1356. 유진수/유진수.py
@@ -1,18 +1,34 @@
-N = input()
+N = input().rstrip()
 l = len(N)
-possible = False
-for i in range(1, l):
-    n1 = 1
-    for j in range(i):
-        n1 *= int(N[j])
-    n2 = 1
-    for j in range(i, l):
-        n2 *= int(N[j])
-    if n1 == n2:
-        possible = True
-        break
-    # print(n1, n2)
-if possible:
-    print("YES")
-else:
+
+if l == 1:
     print("NO")
+else:
+    n1, n2 = 1, 1
+    
+    for i in range(l-1):
+        n1 *= int(N[i])
+    
+    n2 = int(N[l-1])
+    if n1 == n2:
+        print("YES")
+    else:
+        possible = False
+        for i in range(l-2, 0, -1):
+            if int(N[i]) == 0:
+                n1, n2 = 1, 1
+                for j in range(i):
+                    n1 *= int(N[j])
+                for j in range(i, l):
+                    n2 *= int(N[j])
+            else:
+                n2 *= int(N[i])
+                n1 //= int(N[i])
+    
+            if n1 == n2:
+                possible = True
+                break
+        if possible:
+            print("YES")
+        else:
+            print("NO")
