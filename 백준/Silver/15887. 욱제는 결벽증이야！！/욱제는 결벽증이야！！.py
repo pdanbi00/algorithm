4abf437e3d0f6fa81c0d040d29b94c0126fe01b7
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2026-04-06 00:21:07 +0900

    [Silver V] Title: 욱제는 결벽증이야!!, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15887. 욱제는 결벽증이야！！/욱제는 결벽증이야！！.py
+++ 백준/Silver/15887. 욱제는 결벽증이야！！/욱제는 결벽증이야！！.py
@@ -1,16 +1,18 @@
-n=int(input())
-a=list(map(int,input().split()))
-b=a.copy()
-b.sort()
-i=1
-op=0
-ch=[]
-while a!=b:
-    if a[i-1]!=i:
-        op+=1
-        k=a.index(i)+1
-        a[i-1:a.index(i)+1]=a[i-1:a.index(i)+1][::-1]
-        ch.append([i,k])
-    i+=1
+N = int(input())
+cards = list(map(int, input().split()))
+answer = cards.copy()
+answer.sort()
+
+i = 1
+op = 0
+ch = []
+while cards != answer:
+    if cards[i-1] != i:
+        op += 1
+        k = cards.index(i) + 1
+        cards[i-1:k] = cards[i-1:k][::-1]
+        ch.append((i, k))
+    i += 1
 print(op)
-for i in ch: print(*i)
+for a, b in ch:
+    print(a, b)
