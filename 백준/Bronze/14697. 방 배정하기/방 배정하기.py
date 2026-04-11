12567c60b78eb1ad7f767ca29eaf647770987946
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2026-04-11 22:31:59 +0900

    [Bronze II] Title: 방 배정하기, Time: 40 ms, Memory: 32412 KB, Score: 22 point -BaekjoonHub

--- 백준/Bronze/14697. 방 배정하기/방 배정하기.py
+++ 백준/Bronze/14697. 방 배정하기/방 배정하기.py
@@ -1,27 +1,29 @@
 A, B, C, N = map(int, input().split())
-
-possible = False
-for i in range(N):
-    if A * i > N:
-        break
-    for j in range(N):
-        if A * i + B * j > N:
-            break
-
-        for k in range(N):
-            tmp = A * i + B * j + C * k
-            if tmp > N:
-                break
-
-            if tmp == N:
-                possible = True
-                break
-        if possible:
-            break
-    if possible:
-        break
-
-if possible:
+if A % N == 0 or B % N == 0 or C % N == 0:
     print(1)
 else:
-    print(0)
+        possible = False
+        for i in range(N // A + 1):
+            if A * i > N:
+                break
+            for j in range(N // B + 1):
+                if A * i + B * j > N:
+                    break
+
+                for k in range(N // C + 1):
+                    tmp = A * i + B * j + C * k
+                    if tmp > N:
+                        break
+
+                    if tmp == N:
+                        possible = True
+                        break
+                if possible:
+                    break
+            if possible:
+                break
+
+        if possible:
+            print(1)
+        else:
+            print(0)
