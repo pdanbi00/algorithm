21c97bba8ad3d43f1f6dde1b39329017e7d5af4c
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2026-04-28 22:52:59 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.00 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.py
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.py
@@ -1,7 +1,17 @@
+# def solution(n):
+#     answer = 0
+#     for i in range(6, 6 * n+1):
+#         if i % n == 0 and i % 6 == 0:
+#             answer = i // 6
+#             break
+#     return answer
+def gcd(a, b):
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a % b)
+
 def solution(n):
     answer = 0
-    for i in range(6, 6 * n+1):
-        if i % n == 0 and i % 6 == 0:
-            answer = i // 6
-            break
-    return answer
+    answer = n * 6 // gcd(n, 6)
+    return answer // 6
