6fa9f592a5f3b1d30126da2ec101b4a0033df577
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2026-05-02 00:16:18 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.02 ms, Memory: 9.17 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -1,6 +1,10 @@
 def solution(s):
+    answer = ''
     arr = s.split(" ")
     for i in range(len(arr)):
-        arr[i] = arr[i][:1].upper() + arr[i][1:].lower()
-    answer = ' '.join(arr)
+        tmp = arr[i][:1].upper() + arr[i][1:].lower()
+        if i != len(arr)-1:
+            answer += tmp + " "
+        else:
+            answer += tmp
     return answer
