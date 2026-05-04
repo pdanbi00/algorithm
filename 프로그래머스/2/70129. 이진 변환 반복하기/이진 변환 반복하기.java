5b6ae245af79ef31bf2ee13897cfb7e11fb874ba
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2026-05-04 22:13:45 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 6.19 ms, Memory: 92.1 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -1,28 +1,45 @@
 class Solution {
     public int[] solution(String s) {
-        int[] answer;
-        int zero = 0;
-        int cnt = 0;
-        while (!s.equals("1")) {
-            String tmp = "";
-            int c = 0;
+//         int[] answer;
+//         int zero = 0;
+//         int cnt = 0;
+//         while (!s.equals("1")) {
+//             String tmp = "";
+//             int c = 0;
+//             for (int i = 0; i < s.length(); i++) {
+//                 if (s.charAt(i) == '0') {
+//                     zero++;
+//                 } else {
+//                     c++;
+//                 }
+//             }
+            
+//             while (c > 0) {
+//                 tmp += c % 2;
+//                 c /= 2;
+//             }
+            
+//             s = tmp;
+//             cnt++;
+//         }
+//         answer = new int[] {cnt, zero};
+//         return answer;
+        
+        int[] answer = new int[2];
+
+        while (s.length() > 1) {
+            int one = 0;
             for (int i = 0; i < s.length(); i++) {
                 if (s.charAt(i) == '0') {
-                    zero++;
+                    answer[1]++;
                 } else {
-                    c++;
+                    one++;
                 }
             }
             
-            while (c > 0) {
-                tmp += c % 2;
-                c /= 2;
-            }
-            
-            s = tmp;
-            cnt++;
+            s = Integer.toBinaryString(one);
+            answer[0]++;
         }
-        answer = new int[] {cnt, zero};
         return answer;
     }
 }
