ba50988283d5937c8f78ad7b7cd8cd712cfd5bc9
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2026-06-17 17:08:04 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.03 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/README.md
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 자연수 뒤집어 배열로 만들기 - 12932 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12932?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12932?language=java) 
 
 ### 성능 요약
 
-메모리: 11.4 MB, 시간: 0.01 ms
+메모리: 74.3 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 06월 17일 17:03:01
+2026년 06월 17일 17:08:01
 
 ### 문제 설명
 
--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
@@ -1,10 +1,23 @@
+// class Solution {
+//     public int[] solution(long n) {
+//         String nStr = Long.toString(n);
+//         int l = nStr.length();
+//         int[] answer = new int[l];
+//         for (int i = 0; i < l; i++) {
+//             answer[i] = Character.getNumericValue(nStr.charAt(l-1-i));
+//         }
+//         return answer;
+//     }
+// }
+
 class Solution {
     public int[] solution(long n) {
-        String nStr = Long.toString(n);
-        int l = nStr.length();
+        int l = Long.toString(n).length();
         int[] answer = new int[l];
         for (int i = 0; i < l; i++) {
-            answer[i] = Character.getNumericValue(nStr.charAt(l-1-i));
+            Long tmp = n % 10;
+            answer[i] = tmp.intValue();
+            n /= 10;
         }
         return answer;
     }
