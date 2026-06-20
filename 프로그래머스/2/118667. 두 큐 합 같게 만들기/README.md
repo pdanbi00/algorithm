3095a39ff95b84c419f7f90c759dd914e27913a4
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2026-06-21 00:27:02 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 529.38 ms, Memory: 15.9 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/README.md
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 두 큐 합 같게 만들기 - 118667 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118667) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118667?language=python3) 
 
 ### 성능 요약
 
-메모리: 19.2 MB, 시간: 53.41 ms
+메모리: 15.9 MB, 시간: 529.38 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 3일 0:23:44
+2026년 06월 21일 00:27:00
 
 ### 문제 설명
 
--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -1,36 +1,36 @@
-# 아이디어 : 배열 크기는 같다 했음. 고로 두개 합 다 더해서 2로 나눴을 때 홀수면 일단 제낌. 짝수일 경우에 자 두 합이 같으면? 그게 답임. 첫번째 큐의 합이 더 크면? 첫번째꺼에서 빼서 두번째꺼에 넣어줘야됨. 두번째 큐의 합이 더 크면? 두번째꺼에서 빼서 첫번째꺼에 넣어줘야됨. 자 이 반복을 언제까지 하냐? 첫번째 큐에 있던거 다 빼서 두번째꺼에 넣어서 다시 첫번째꺼가 원상복귀 되려면 총 4n번 동안 push, pop이 이뤄짐. 이해 안되면 그림 그려보셈
-
 from collections import deque
 
 def solution(queue1, queue2):
-    answer = 0 # pop, insert한 횟수
+    answer = 0
+    q1 = deque(queue1)
+    q2 = deque(queue2)
     
-    queue1 = deque(queue1)
-    queue2 = deque(queue2)
-    max_banbok = (len(queue1) + len(queue2)) * 2 # 최대 반복
-    sum1 = sum(queue1)
-    sum2 = sum(queue2)
-    total = sum1 + sum2
+    t1 = sum(queue1)
+    t2 = sum(queue2)
     
+    total = t1 + t2
     if total % 2 == 1:
         return -1
     
-    while True:
-        if sum1 == sum2:
-            break
-        elif sum1 > sum2:
-            target = queue1.popleft()
-            queue2.append(target)
-            sum1 -= target
-            sum2 += target
-            answer += 1
-        else: # queue2의 합이 queue1의 합 보다 큰 경우
-            target = queue2.popleft()
-            queue1.append(target)
-            sum1 += target
-            sum2 -= target
-            answer += 1
-        if answer == max_banbok:
+    target = total // 2
+    for t in q1:
+        if t > target:
             return -1
         
-    return answer
+    while answer <= 6000000:
+        if t1 > t2:
+            tmp = q1.popleft()
+            q2.append(tmp)
+            t1 -= tmp
+            t2 += tmp
+            answer += 1
+        elif t1 < t2:
+            tmp = q2.popleft()
+            q1.append(tmp)
+            t1 += tmp
+            t2 -= tmp
+            answer += 1
+        else:
+            return answer
+    
+    return -1
