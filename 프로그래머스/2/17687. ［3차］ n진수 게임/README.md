783c09dcc95e5aa5263b13e432c7fb648b41a3c3
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2026-07-09 16:41:10 +0900

    [level 2] Title: [3차] n진수 게임, Time: 256.31 ms, Memory: 376 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/README.md
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/README.md
@@ -1,10 +1,10 @@
 # [level 2] [3차] n진수 게임 - 17687 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17687?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17687?language=java) 
 
 ### 성능 요약
 
-메모리: 11.6 MB, 시간: 7.94 ms
+메모리: 376 MB, 시간: 256.31 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 07월 09일 16:37:39
+2026년 07월 09일 16:41:07
 
 ### 문제 설명
 
--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -1,19 +1,38 @@
+// class Solution {
+//     public String solution(int n, int t, int m, int p) {
+//         String answer = "";
+//         String tmp = "";
+//         for (int i = 0; i < (m * t); i++) {
+//             tmp += Integer.toString(i, n);
+//         }
+
+        
+//         for (int i = 0; i < t; i++) {
+//             char c = tmp.charAt(p-1 + (m*i));
+//             if (!Character.isDigit(c)) {
+//                 answer += Character.toUpperCase(c);
+//             } else {
+//                 answer += c;
+//             }
+//         }
+        
+//         return answer;
+//     }
+// }
+
 class Solution {
     public String solution(int n, int t, int m, int p) {
         String answer = "";
         String tmp = "";
-        for (int i = 0; i <= (m * t); i++) {
-            tmp += Integer.toString(i, n);
+        int num = 0;
+        while (tmp.length() < m * t) {
+            tmp += Integer.toString(num, n).toUpperCase();
+            num++;
         }
 
         
         for (int i = 0; i < t; i++) {
-            char c = tmp.charAt(p-1 + (m*i));
-            if (!Character.isDigit(c)) {
-                answer += Character.toUpperCase(c);
-            } else {
-                answer += c;
-            }
+            answer += tmp.charAt(p-1 + (m*i));
         }
         
         return answer;
