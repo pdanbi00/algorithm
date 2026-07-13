85c7111c6190631c25586e31420f675f658aa930
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2026-07-13 13:42:40 +0900

    [level 3] Title: 가장 먼 노드, Time: 18.79 ms, Memory: 21.9 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/README.md
+++ 프로그래머스/3/49189. 가장 먼 노드/README.md
@@ -1,10 +1,10 @@
 # [level 3] 가장 먼 노드 - 49189 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49189?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49189?language=python3) 
 
 ### 성능 요약
 
-메모리: 101 MB, 시간: 13.67 ms
+메모리: 21.9 MB, 시간: 18.79 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 07월 13일 13:37:39
+2026년 07월 13일 13:42:38
 
 ### 문제 설명
 
--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -1,43 +1,80 @@
+# # 한 노드에서 다른 모든 노드들 사이의 거리를 구하는 것이니깐 최단 경로 알고리즘
+# # 가중치가 양수니깐 다익스트라
+
+# from heapq import heappush, heappop
+
+# def solution(n, edge):
+#     answer = 0
+#     graph = [[] for _ in range(n+1)]
+#     distance = [1e9] * (n+1)
+#     distance[1] = 0
+#     q = []
+    
+#     for i in range(len(edge)):
+#         a, b = edge[i][0], edge[i][1]
+#         graph[a].append((b, 1))
+#         graph[b].append((a, 1))
+    
+#     heappush(q, (0, 1))
+    
+#     # dijkstra
+#     while q:
+#         dis, node = heappop(q)
+        
+#         if dis > distance[node]:
+#             continue
+        
+#         for next_node, weight in graph[node]:
+#             W = dis + weight
+#             if distance[next_node] > W:
+#                 distance[next_node] = W
+#                 heappush(q, (W, next_node))
+    
+#     # print(distance)
+#     max_v = -1
+#     cnt = 0
+#     for i in range(1, n+1):
+#         if max_v < distance[i]:
+#             max_v = distance[i]
+#             cnt = 1
+#         elif max_v == distance[i]:
+#             cnt += 1
+    
+#     return cnt
+
 # 한 노드에서 다른 모든 노드들 사이의 거리를 구하는 것이니깐 최단 경로 알고리즘
 # 가중치가 양수니깐 다익스트라
+# 아니고 가중치가 1이니깐 bfs 돌리면 됨
 
-from heapq import heappush, heappop
-
+from collections import deque
 def solution(n, edge):
     answer = 0
+    target = 0
     graph = [[] for _ in range(n+1)]
-    distance = [1e9] * (n+1)
-    distance[1] = 0
-    q = []
+    distance = [-1] * (n+1)
+    # visited = [False] * (n+1)
     
     for i in range(len(edge)):
         a, b = edge[i][0], edge[i][1]
-        graph[a].append((b, 1))
-        graph[b].append((a, 1))
+        graph[a].append(b)
+        graph[b].append(a)
     
-    heappush(q, (0, 1))
+    q = deque()
+    q.append(1)
+    distance[1] = 0
     
-    # dijkstra
     while q:
-        dis, node = heappop(q)
+        cur = q.popleft()
+        if distance[cur] > target:
+            target = distance[cur]
+            answer = 1
+        elif distance[cur] == target:
+            answer += 1
         
-        if dis > distance[node]:
-            continue
-        
-        for next_node, weight in graph[node]:
-            W = dis + weight
-            if distance[next_node] > W:
-                distance[next_node] = W
-                heappush(q, (W, next_node))
+        for a in graph[cur]:
+            if distance[a] == -1:
+                distance[a] = distance[cur] + 1
+                q.append(a)
     
-    # print(distance)
-    max_v = -1
-    cnt = 0
-    for i in range(1, n+1):
-        if max_v < distance[i]:
-            max_v = distance[i]
-            cnt = 1
-        elif max_v == distance[i]:
-            cnt += 1
     
-    return cnt
+    return answer
