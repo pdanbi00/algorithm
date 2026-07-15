b2588cb030246830f0f4b1ca893af1cd33ac7d43
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2026-07-15 15:30:16 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 535.58 ms, Memory: 11.1 MB -BaekjoonHub

--- 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.py
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.py
@@ -1,21 +1,46 @@
+# def solution(s):
+#     answer = 1
+    
+#     N = len(s)
+#     dp = [[0] * N for _ in range(N)]
+#     for i in range(N):
+#         dp[i][i] = 1
+        
+#     for i in range(N-1):
+#         if (s[i] == s[i+1]):
+#             dp[i][i+1] = 1
+#             answer = 2
+            
+#     for k in range(2, N):
+#         for i in range(N-k):
+#             j = i + k
+#             if (s[i] == s[j] and dp[i+1][j-1] == 1):
+#                 dp[i][j] = 1
+#                 answer = max(answer, j - i + 1)
+
+#     return answer
+
+
 def solution(s):
-    answer = 1
+    answer = 0
     
     N = len(s)
-    dp = [[0] * N for _ in range(N)]
-    for i in range(N):
-        dp[i][i] = 1
-        
-    for i in range(N-1):
-        if (s[i] == s[i+1]):
-            dp[i][i+1] = 1
-            answer = 2
+    
+    for k in range(N, 0, -1):
+        for i in range(N-k+1):
+            start = i
+            end = i + k - 1
+            flag = True
             
-    for k in range(2, N):
-        for i in range(N-k):
-            j = i + k
-            if (s[i] == s[j] and dp[i+1][j-1] == 1):
-                dp[i][j] = 1
-                answer = max(answer, j - i + 1)
+            while (start <= end):
+                if (s[start] != s[end]):
+                    flag = False
+                    break
+                    
+                start += 1
+                end -= 1
+            
+            if flag:
+                return k
 
     return answer
