3b34f5ed14e63fb27692a712cbbb2ed13cd3eff6
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2026-07-27 10:48:03 +0900

    [level 3] Title: 인사고과, Time: 92.66 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/152995. 인사고과/인사고과.py
@@ -0,0 +1,17 @@
+def solution(scores):
+    answer = 1
+    target = scores[0]
+    target_score = sum(scores[0])
+    
+    scores.sort(key=lambda x : (-x[0], x[1]))
+    max_v = -1
+    for score in scores:
+        if target[0] < score[0] and target[1] < score[1]:
+            return -1
+        if max_v <= score[1]:
+            if target_score < score[0] + score[1]:
+                answer += 1
+            max_v = score[1]
+    
+    
+    return answer
