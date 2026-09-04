2e9f9d58100fd22fdf2f1a26b7e979770e9ade4d
pdanbi00 <88076629+pdanbi00@users.noreply.github.com>
2026-09-04 10:04:20 +0900

    [level 1] Title: 콜라 문제, Time: 0.09 ms, Memory: 82.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+        while (n >= a) {
+            int tmp = n / a;
+            answer += tmp * b;
+            
+            n -= tmp * a;
+            n += tmp * b;
+        }
+        return answer;
+    }
+}
